239850836d8019ed95d0847368365513cef0a39c
lcg <18706423038@163.com>
2022-02-16 16:53:20 +0800

    Phoenix metadata

---
+++ dlink-metadata/dlink-metadata-phoenix/src/main/java/com/dlink/metadata/convert/PhoenixTypeConvert.java
@@ -0,0 +1,99 @@
+package com.dlink.metadata.convert;
+
+import com.dlink.metadata.constant.PhoenixEnum;
+import com.dlink.metadata.rules.DbColumnType;
+import com.dlink.metadata.rules.IColumnType;
+
+public class PhoenixTypeConvert implements ITypeConvert {
+    @Override
+    public IColumnType convert(String columnType) {
+        String t = columnType.toLowerCase();
+        if (t.contains("char") || t.contains("varchar") || t.contains("text") ||
+                t.contains("nchar") || t.contains("nvarchar") || t.contains("ntext")
+                || t.contains("uniqueidentifier") || t.contains("sql_variant")) {
+            return DbColumnType.STRING;
+        } else if (t.contains("int") || t.contains("tinyint") || t.contains("smallint") || t.contains("integer")) {
+            return DbColumnType.INTEGER;
+        } else if (t.contains("bigint")) {
+            return DbColumnType.LONG;
+        } else if (t.contains("float")) {
+            return DbColumnType.FLOAT;
+        } else if (t.contains("decimal") || t.contains("money") || t.contains("smallmoney")
+                || t.contains("numeric")) {
+            return DbColumnType.BIG_DECIMAL;
+        } else if (t.contains("double")) {
+            return DbColumnType.DOUBLE;
+        } else if (t.contains("boolean")) {
+            return DbColumnType.BOOLEAN;
+        } else if (t.contains("time")) {
+            return DbColumnType.TIME;
+        } else if (t.contains("date")) {
+            return DbColumnType.DATE;
+        } else if (t.contains("smalldatetime") || t.contains("datetime")) {
+            return DbColumnType.TIMESTAMP;
+        } else if (t.contains("timestamp") || t.contains("binary") || t.contains("varbinary") || t.contains("image")) {
+            return DbColumnType.BYTE_ARRAY;
+        }
+        return DbColumnType.STRING;
+    }
+
+    @Override
+    public String convertToDB(String columnType) {
+        try {
+            Integer typeNum = Integer.valueOf(columnType);
+            return PhoenixEnum.getDataTypeEnum(typeNum).toString();
+        } catch (Exception e) {
+            switch (columnType.toUpperCase()) {
+                case "UNSIGNED_INT":
+                    return "UNSIGNED_INT";
+                case "INT":
+                case "INTEGER":
+                    return "INTEGER";
+                case "TINYINT":
+                    return "TINYINT";
+                case "double":
+                    return "double";
+                case "BIGINT":
+                    return "BIGINT";
+                case "UNSIGNED_TINYINT":
+                    return "UNSIGNED_TINYINT";
+                case "UNSIGNED_SMALLINT":
+                    return "UNSIGNED_SMALLINT";
+                case "FLOAT":
+                    return "FLOAT";
+                case "UNSIGNED_FLOAT":
+                    return "UNSIGNED_FLOAT";
+                case "DOUBLE":
+                    return "DOUBLE";
+                case "UNSIGNED_DOUBLE":
+                    return "UNSIGNED_DOUBLE";
+                case "DECIMAL":
+                    return "DECIMAL";
+                case "BOOLEAN":
+                    return "BOOLEAN";
+                case "TIME ":
+                    return "TIME ";
+                case "DATE":
+                    return "DATE";
+                case "TIMESTAMP":
+                    return "TIMESTAMP";
+                case "UNSIGNED_TIME":
+                    return "UNSIGNED_TIME";
+                case "UNSIGNED_DATE":
+                    return "UNSIGNED_DATE";
+                case "UNSIGNED_TIMESTAMP":
+                    return "UNSIGNED_TIMESTAMP";
+                case "VARCHAR":
+                    return "VARCHAR";
+                case "CHAR":
+                    return "VARCHAR";
+                case "BINARY":
+                    return "BINARY";
+                case "VARBINARY":
+                    return "VARBINARY";
+                default:
+                    return "VARBINARY";
+            }
+        }
+    }
+}
---
+++ dlink-metadata/dlink-metadata-phoenix/src/main/java/com/dlink/metadata/driver/PhoenixDriver.java
@@ -0,0 +1,60 @@
+package com.dlink.metadata.driver;
+
+import com.dlink.metadata.constant.PhoenixConstant;
+import com.dlink.metadata.convert.ITypeConvert;
+import com.dlink.metadata.convert.PhoenixTypeConvert;
+import com.dlink.metadata.query.IDBQuery;
+import com.dlink.metadata.query.PhoenixQuery;
+import com.dlink.model.Column;
+import com.dlink.model.Table;
+import java.util.List;
+
+/**
+ *
+ * @author lcg
+ * @operate 
+ * @date 2022/2/16 16:50
+ * @return 
+ */
+public class PhoenixDriver extends AbstractJdbcDriver {
+    @Override
+    public IDBQuery getDBQuery() {
+        return new PhoenixQuery();
+    }
+
+    @Override
+    public ITypeConvert getTypeConvert() {
+        return new PhoenixTypeConvert();
+    }
+
+    @Override
+    String getDriverClass() {
+        return PhoenixConstant.PHOENIX_DRIVER;
+    }
+
+    @Override
+    public String getType() {
+        return "Phoenix";
+    }
+
+    @Override
+    public String getName() {
+        return "Phoenix";
+    }
+
+    @Override
+    public String getCreateTableSql(Table table) {
+        StringBuilder sql = new StringBuilder();
+        List<Column> columns = table.getColumns();
+        sql.append(" CREATE VIEW IF NOT EXISTS \"" + table.getName() + "\" ( ");
+        sql.append("    rowkey varchar primary key ");
+        PhoenixTypeConvert phoenixTypeConvert = new PhoenixTypeConvert();
+        if (columns != null) {
+            for (Column column : columns) {
+                sql.append(", \"" + column.getColumnFamily() + "\".\"" + column.getName() + "\"  " + phoenixTypeConvert.convertToDB(column.getType()));
+            }
+        }
+        sql.append(" ) ");
+        return sql.toString();
+    }
+}
