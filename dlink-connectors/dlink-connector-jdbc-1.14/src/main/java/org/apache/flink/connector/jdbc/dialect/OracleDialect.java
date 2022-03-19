cab8d6a42e538239ac33d885f960a5852948ccc3
wenmo <32723967+wenmo@users.noreply.github.com>
2022-03-20 00:30:50 +0800

    connector format

--- dlink-connectors/dlink-connector-jdbc-1.14/src/main/java/org/apache/flink/connector/jdbc/dialect/OracleDialect.java
+++ dlink-connectors/dlink-connector-jdbc-1.14/src/main/java/org/apache/flink/connector/jdbc/dialect/OracleDialect.java
@@ -1,7 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.connector.jdbc.dialect;
 
 import org.apache.flink.connector.jdbc.internal.converter.JdbcRowConverter;
-import org.apache.flink.connector.jdbc.internal.converter.SQLServerRowConverter;
+import org.apache.flink.connector.jdbc.internal.converter.OracleRowConverter;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
 import org.apache.flink.table.types.logical.RowType;
 
@@ -9,39 +27,43 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * SQLServerDialect
+ * JDBC dialect for Oracle.
  *
  * @author wenmo
- * @since 2021/12/9
- **/
-public class SQLServerDialect extends AbstractDialect {
+ * @since 2021/9/19 20:30
+ */
+public class OracleDialect extends AbstractDialect {
+
     private static final long serialVersionUID = 1L;
 
+    // Define MAX/MIN precision of TIMESTAMP type according to Oracle docs:
     private static final int MAX_TIMESTAMP_PRECISION = 6;
     private static final int MIN_TIMESTAMP_PRECISION = 1;
 
+    // Define MAX/MIN precision of DECIMAL type according to Mysql docs:
+    // https://dev.mysql.com/doc/refman/8.0/en/fixed-point-types.html
     private static final int MAX_DECIMAL_PRECISION = 65;
     private static final int MIN_DECIMAL_PRECISION = 1;
 
-    // jdbc:sqlserver://127.0.0.1:1433;DatabaseName=test
+    // jdbc:oracle:thin:@127.0.0.1:1521:ORCL
     @Override
     public boolean canHandle(String url) {
-        return url.startsWith("jdbc:sqlserver:");
+        return url.startsWith("jdbc:oracle:thin:");
     }
 
     @Override
     public JdbcRowConverter getRowConverter(RowType rowType) {
-        return new SQLServerRowConverter(rowType);
+        return new OracleRowConverter(rowType);
     }
 
     @Override
     public String getLimitClause(long limit) {
-        return "";
+        return "ROWNUM < " + limit;
     }
 
     @Override
     public Optional<String> defaultDriverName() {
-        return Optional.of("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return Optional.of("oracle.jdbc.driver.OracleDriver");
     }
 
     @Override
@@ -49,14 +71,6 @@ public class SQLServerDialect extends AbstractDialect {
         return identifier;
     }
 
-    /*IF EXISTS(SELECT * FROM source WHERE tid = 3)
-    BEGIN
-    UPDATE source SET tname = 'd' WHERE tid = 3
-    END
-    ELSE
-    BEGIN
-    INSERT INTO source (tid, tname) VALUES(3, 'd')
-    END*/
     @Override
     public Optional<String> getUpsertStatement(
             String tableName, String[] fieldNames, String[] uniqueKeyFields) {
@@ -69,37 +83,39 @@ public class SQLServerDialect extends AbstractDialect {
 
         String updateClause =
                 Arrays.stream(updateFieldNamesList.toArray(new String[0]))
-                        .map(f -> quoteIdentifier(f) + " = :" + quoteIdentifier(f))
+                        .map(f -> "a." + quoteIdentifier(f) + " = :" + quoteIdentifier(f))
                         .collect(Collectors.joining(", "));
         String onClause =
                 Arrays.stream(uniqueKeyFields)
-                        .map(f -> quoteIdentifier(f) + " = :" + quoteIdentifier(f))
+                        .map(f -> "a." + quoteIdentifier(f) + " = :" + quoteIdentifier(f))
                         .collect(Collectors.joining(" AND "));
         String sql =
-                "IF EXISTS ( SELECT * FROM " + tableName + " WHERE " + onClause + " ) "
-                        + " BEGIN "
-                        + " UPDATE " + tableName + " SET " + updateClause + " WHERE " + onClause
-                        + " END "
-                        + " ELSE "
-                        + " BEGIN "
-                        + getInsertStatement(tableName, fieldNames)
-                        + " END";
+                "MERGE INTO "
+                        + tableName
+                        + " a USING ( SELECT 1 FROM dual ) b ON ( "
+                        + onClause
+                        + " )"
+                        + " WHEN MATCHED THEN"
+                        + " UPDATE SET "
+                        + updateClause
+                        + " WHEN NOT MATCHED THEN "
+                        + getInsertStatement(fieldNames);
         return Optional.of(sql);
     }
 
-    private String getInsertStatement(String tableName, String[] fieldNames) {
+    private String getInsertStatement(String[] fieldNames) {
         String columns =
                 Arrays.stream(fieldNames)
                         .map(this::quoteIdentifier)
                         .collect(Collectors.joining(", "));
         String placeholders =
                 Arrays.stream(fieldNames).map(f -> ":" + f).collect(Collectors.joining(", "));
-        return "INSERT INTO " + tableName + "(" + columns + ") VALUES (" + placeholders + ")";
+        return "INSERT " + "(" + columns + ")" + " VALUES (" + placeholders + ")";
     }
 
     @Override
     public String dialectName() {
-        return "SQLServer";
+        return "Oracle";
     }
 
     @Override
@@ -125,6 +141,9 @@ public class SQLServerDialect extends AbstractDialect {
     @Override
     public List<LogicalTypeRoot> unsupportedTypes() {
 
+        // TODO: We can't convert BINARY data type to
+        //  PrimitiveArrayTypeInfo.BYTE_PRIMITIVE_ARRAY_TYPE_INFO in
+        // LegacyTypeInfoDataTypeConverter.
         return Arrays.asList(
                 LogicalTypeRoot.BINARY,
                 LogicalTypeRoot.TIMESTAMP_WITH_LOCAL_TIME_ZONE,
