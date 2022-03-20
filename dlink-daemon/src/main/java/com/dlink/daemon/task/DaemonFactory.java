3bc3deb2b019f5cb601635cee20694baf996cb13
wenmo <32723967+wenmo@users.noreply.github.com>
2022-03-20 15:01:40 +0800

    daemon format

--- dlink-daemon/src/main/java/com/dlink/daemon/task/DaemonFactory.java
+++ dlink-daemon/src/main/java/com/dlink/daemon/task/DaemonFactory.java
@@ -7,25 +7,25 @@ import java.util.List;
 
 public class DaemonFactory {
 
-    public static void start(List<DaemonTaskConfig> configList){
+    public static void start(List<DaemonTaskConfig> configList) {
         Thread thread = new Thread(() -> {
-            DefaultThreadPool defaultThreadPool =  DefaultThreadPool.getInstance();
-            for (DaemonTaskConfig config: configList) {
+            DefaultThreadPool defaultThreadPool = DefaultThreadPool.getInstance();
+            for (DaemonTaskConfig config : configList) {
                 DaemonTask daemonTask = DaemonTask.build(config);
                 defaultThreadPool.execute(daemonTask);
             }
             while (true) {
                 int taskSize = defaultThreadPool.getTaskSize();
                 try {
-                    Thread.sleep(Math.max(FlinkTaskConstant.MAX_POLLING_GAP / (taskSize + 1),  FlinkTaskConstant.MIN_POLLING_GAP));
+                    Thread.sleep(Math.max(FlinkTaskConstant.MAX_POLLING_GAP / (taskSize + 1), FlinkTaskConstant.MIN_POLLING_GAP));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
                 int num = taskSize / 100 + 1;
                 if (defaultThreadPool.getWorkCount() < num) {
-                    defaultThreadPool.addWorkers(num - defaultThreadPool.getWorkCount() );
-                }else if(defaultThreadPool.getWorkCount() > num) {
+                    defaultThreadPool.addWorkers(num - defaultThreadPool.getWorkCount());
+                } else if (defaultThreadPool.getWorkCount() > num) {
                     defaultThreadPool.removeWorker(defaultThreadPool.getWorkCount() - num);
                 }
             }
@@ -33,7 +33,7 @@ public class DaemonFactory {
         thread.start();
     }
 
-    public static void addTask(DaemonTaskConfig config){
+    public static void addTask(DaemonTaskConfig config) {
         DefaultThreadPool.getInstance().execute(DaemonTask.build(config));
     }
 }
