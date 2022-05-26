73bb31841e40aef09a3a4d13b7395710ba7067a7
wenmo <32723967+wenmo@users.noreply.github.com>
2022-05-26 22:15:49 +0800

    [Feature-254][web] Add StreamGraph JSON Export function

--- dlink-web/src/components/Studio/StudioConsole/StudioCA/index.tsx
+++ dlink-web/src/components/Studio/StudioConsole/StudioCA/index.tsx
@@ -1,11 +1,12 @@
-import { Tabs,Tooltip,Button } from "antd";
-import {SearchOutlined} from "@ant-design/icons";
+import {Tabs, Tooltip, Button, Modal, message} from "antd";
+import {SearchOutlined, SnippetsOutlined} from "@ant-design/icons";
 import {StateType} from "@/pages/DataStudio/model";
 import {connect} from "umi";
 import styles from "./index.less";
-import {getLineage} from "@/pages/DataStudio/service";
+import {getLineage, getStreamGraph} from "@/pages/DataStudio/service";
 import {useState} from "react";
 import Lineage, {getInit} from "@/components/Lineage";
+import CodeShow from "@/components/Common/CodeShow";
 
 const { TabPane } = Tabs;
 
@@ -20,21 +21,57 @@ const StudioCA = (props: any) => {
       type: 1,
     });
     res.then((result)=>{
-      setData(result.datas);
+      if(result.datas){
+        setData(result.datas);
+      }else {
+        message.error(`获取作业血缘失败，原因：\n${result.msg}`);
+      }
+    })
+  };
+
+  const handleExportStreamGraphPlan=()=>{
+    const res = getStreamGraph({
+      ...current.task,
+      configJson: JSON.stringify(current.task.config),
+      statement: current.value,
+    });
+    res.then((result)=>{
+      Modal.info({
+        title: current.task.alias + '的 StreamGraphPlan',
+        width: 1000,
+        content: (
+          <CodeShow code={JSON.stringify((result.datas?result.datas:result.msg), null, "\t")} language='json'
+                    height='500px' theme="vs-dark"/>
+        ),
+        onOk() {
+        },
+      });
     })
   };
 
   return (<>
     <Tabs defaultActiveKey="Lineage" size="small" tabPosition="top" style={{border: "1px solid #f0f0f0"}}
-          tabBarExtraContent={<Tooltip title="重新计算血缘">
-            <Button
-              type="text"
-              icon={<SearchOutlined />}
-              onClick={handleLineage}
-            >
-              计算血缘
-            </Button>
-          </Tooltip>}
+          tabBarExtraContent={
+            <>
+              <Tooltip title="重新计算血缘">
+                <Button
+                  type="text"
+                  icon={<SearchOutlined />}
+                  onClick={handleLineage}
+                >
+                  计算血缘
+                </Button>
+              </Tooltip>
+              <Tooltip title="导出 StreamGraphPlan">
+                <Button
+                  type="text"
+                  icon={<SnippetsOutlined />}
+                  onClick={handleExportStreamGraphPlan}
+                >
+                  StreamGraphPlan
+                </Button>
+              </Tooltip>
+            </>}
     >
       <TabPane tab={<span>血缘分析</span>} key="Lineage">
         <Lineage datas={data}/>
