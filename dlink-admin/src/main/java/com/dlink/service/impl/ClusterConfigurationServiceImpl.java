c83531dd7f493f4c808ee5d3cddb4137fb893272
wuhuiqiang2 <1368216733@qq.com>
2022-06-08 18:12:48 +0800

    fix cluster-id in test k8s-cluster-config is null

--- dlink-admin/src/main/java/com/dlink/service/impl/ClusterConfigurationServiceImpl.java
+++ dlink-admin/src/main/java/com/dlink/service/impl/ClusterConfigurationServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * ClusterConfigServiceImpl
@@ -68,6 +69,9 @@ public class ClusterConfigurationServiceImpl extends SuperServiceImpl<ClusterCon
             }
             if (kubernetesConfig.containsKey("kubernetes.cluster-id")) {
                 gatewayConfig.getFlinkConfig().getConfiguration().put("kubernetes.cluster-id", kubernetesConfig.get("kubernetes.cluster-id").toString());
+            }else{
+                //初始化FlinkKubeClient需要CLUSTER_ID,先用UUID代替，后面使用job名称来作为CLUSTER_ID
+                gatewayConfig.getFlinkConfig().getConfiguration().put("kubernetes.cluster-id", UUID.randomUUID().toString());
             }
             if (kubernetesConfig.containsKey("kubernetes.container.image")) {
                 gatewayConfig.getFlinkConfig().getConfiguration().put("kubernetes.container.image", kubernetesConfig.get("kubernetes.container.image").toString());
