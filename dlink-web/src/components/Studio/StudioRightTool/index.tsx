b7b1340f2d7ce62e9e5c0c477693214fbe04e071
aiwenmo <32723967+aiwenmo@users.noreply.github.com>
2022-06-28 23:32:30 +0800

    [Feature-654][web] Add task info tab (#655)
    Co-authored-by: wenmo <32723967+wenmo@users.noreply.github.com>

--- dlink-web/src/components/Studio/StudioRightTool/index.tsx
+++ dlink-web/src/components/Studio/StudioRightTool/index.tsx
@@ -1,5 +1,5 @@
-import {Tabs, Empty, Form} from "antd";
-import {SettingOutlined,ScheduleOutlined} from "@ant-design/icons";
+import {Tabs} from "antd";
+import {SettingOutlined, ScheduleOutlined, ContainerOutlined} from "@ant-design/icons";
 import {StateType} from "@/pages/DataStudio/model";
 import {connect} from "umi";
 import StudioConfig from "./StudioConfig";
@@ -11,33 +11,42 @@ import StudioSqlConfig from "./StudioSqlConfig";
 import StudioUDFInfo from "./StudioUDFInfo";
 import StudioJarSetting from "./StudioJarSetting";
 import StudioGuide from "./StudioGuide";
+import StudioTaskInfo from "./StudioTaskInfo";
 import {DIALECT, isSql} from "@/components/Studio/conf";
 
-const { TabPane } = Tabs;
+const {TabPane} = Tabs;
 
 const StudioRightTool = (props: any) => {
 
-  const {current,form,toolHeight} = props;
+  const {current, form, toolHeight} = props;
 
   const renderContent = () => {
-    if(isSql(current.task.dialect)){
+    if (isSql(current.task.dialect)) {
       return renderSqlContent();
     }
-    if(DIALECT.FLINKJAR === current.task.dialect){
+    if (DIALECT.FLINKJAR === current.task.dialect) {
       return renderJarContent();
     }
-    if(DIALECT.FLINKSQLENV === current.task.dialect){
+    if (DIALECT.FLINKSQLENV === current.task.dialect) {
       return renderEnvContent();
     }
-    if(DIALECT.JAVA === current.task.dialect){
+    if (DIALECT.JAVA === current.task.dialect) {
       return renderUDFContent();
     }
     return renderFlinkSqlContent();
   };
 
+  const renderTaskInfoContent = () => {
+    return (
+      <TabPane tab={<span><ContainerOutlined/> 作业信息</span>} key="StudioTaskInfo">
+        <StudioTaskInfo form={form}/>
+      </TabPane>
+    )
+  };
+
   const renderSqlContent = () => {
     return (<>
-      <TabPane tab={<span><ScheduleOutlined /> 执行配置</span>} key="StudioSqlConfig" >
+      <TabPane tab={<span><SettingOutlined/> 执行配置</span>} key="StudioSqlConfig">
         <StudioSqlConfig form={form}/>
       </TabPane>
     </>)
@@ -45,7 +54,7 @@ const StudioRightTool = (props: any) => {
 
   const renderJarContent = () => {
     return (<>
-      <TabPane tab={<span><SettingOutlined /> 作业配置</span>} key="StudioJarSetting" >
+      <TabPane tab={<span><SettingOutlined/> 作业配置</span>} key="StudioJarSetting">
         <StudioJarSetting form={form}/>
       </TabPane>
     </>)
@@ -53,7 +62,7 @@ const StudioRightTool = (props: any) => {
 
   const renderEnvContent = () => {
     return (<>
-      <TabPane tab={<span><SettingOutlined /> 作业配置</span>} key="StudioEnvSetting" >
+      <TabPane tab={<span><SettingOutlined/> 作业配置</span>} key="StudioEnvSetting">
         <StudioEnvSetting form={form}/>
       </TabPane>
     </>)
@@ -61,22 +70,21 @@ const StudioRightTool = (props: any) => {
 
   const renderUDFContent = () => {
     return (<>
-      <TabPane tab={<span><SettingOutlined /> UDF信息</span>} key="StudioUDFInfo" >
+      <TabPane tab={<span><SettingOutlined/> UDF信息</span>} key="StudioUDFInfo">
         <StudioUDFInfo form={form}/>
       </TabPane>
     </>)
   };
 
   const renderFlinkSqlContent = () => {
-    return (<><TabPane tab={<span><SettingOutlined /> 作业配置</span>} key="StudioSetting" >
-      <StudioSetting form={form} />
+    return (<><TabPane tab={<span><SettingOutlined/> 作业配置</span>} key="StudioSetting">
+      <StudioSetting form={form}/>
     </TabPane>
-      <TabPane tab={<span><ScheduleOutlined /> 执行配置</span>} key="StudioConfig" >
+      <TabPane tab={<span><SettingOutlined/> 执行配置</span>} key="StudioConfig">
         <StudioConfig form={form}/>
       </TabPane>
-      <TabPane tab={<span><ScheduleOutlined /> 保存点</span>} key="StudioSavePoint" >
-        <StudioSavePoint />
-      </TabPane>
+      <TabPane tab={<span><ScheduleOutlined/> 保存点</span>} key="StudioSavePoint">
+        <StudioSavePoint/>
       <TabPane tab={<span><ScheduleOutlined /> 版本历史</span>} key="StudioHistory" >
         <StudioHistory />
       </TabPane>
@@ -85,15 +93,16 @@ const StudioRightTool = (props: any) => {
 
   return (
     <>
-      { current?.task?
-        <Tabs defaultActiveKey="1" size="small" tabPosition="right"  style={{ height: toolHeight}}>
+      {current?.task ?
+        <Tabs defaultActiveKey="1" size="small" tabPosition="right" style={{height: toolHeight}}>
           {renderContent()}
-        </Tabs>:<StudioGuide toolHeight={toolHeight} />}
+          {renderTaskInfoContent()}
+        </Tabs> : <StudioGuide toolHeight={toolHeight}/>}
     </>
   );
 };
 
-export default connect(({ Studio }: { Studio: StateType }) => ({
+export default connect(({Studio}: { Studio: StateType }) => ({
   sql: Studio.sql,
   toolHeight: Studio.toolHeight,
   current: Studio.current,
