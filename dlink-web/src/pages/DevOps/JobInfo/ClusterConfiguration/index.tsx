b711b81d1da9bb1aa5e7dfc1dad2ecc39661edc7
zhu-mingye <934230207@qq.com>
2022-06-29 11:06:00 +0800

    Fixed the problem that when the job is not running, the cluster configuration is still obtained through flinkapi, resulting in an error

--- dlink-web/src/pages/DevOps/JobInfo/ClusterConfiguration/index.tsx
+++ dlink-web/src/pages/DevOps/JobInfo/ClusterConfiguration/index.tsx
@@ -3,34 +3,41 @@ import {PageContainer} from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
 import JobManagerConfiguration from "@/pages/DevOps/JobInfo/ClusterConfiguration/JobManager";
 import TaskManagerConfigurationForm from "@/pages/DevOps/JobInfo/ClusterConfiguration/TaskManager";
+import {JOB_STATUS} from "@/components/Common/JobStatus";
+import {Empty} from "antd";
 
 const ClusterConfiguration = (props: any) => {
   const {job} = props;
   const [tabKey, setTabKey] = useState<string>('jobmanager');
   return (
-    <PageContainer
-      header={{title: undefined}}
-      tabList={[
-        {
-          tab: 'Job Manager',
-          key: 'jobmanager',
-          closable: false,
-        },
-        {
-          tab: 'Task Managers',
-          key: 'taskmanager',
-          closable: false,
-        },
-      ]}
-      onTabChange={(key) => {
-        setTabKey(key);
-      }}
-    >
-      <ProCard  >
-        {tabKey === 'jobmanager' ? <JobManagerConfiguration  job={job}/> : undefined}
-        {tabKey === 'taskmanager' ? <TaskManagerConfigurationForm  job={job}/> : undefined}
-      </ProCard>
-    </PageContainer>
+    <>
+      {job?.instance?.status === JOB_STATUS.RUNNING ?
+        <PageContainer
+          header={{title: undefined}}
+          tabList={[
+            {
+              tab: 'Job Manager',
+              key: 'jobmanager',
+              closable: false,
+            },
+            {
+              tab: 'Task Managers',
+              key: 'taskmanager',
+              closable: false,
+            },
+          ]}
+          onTabChange={(key) => {
+            setTabKey(key);
+          }}
+        >
+          <ProCard>
+            {tabKey === 'jobmanager' ? <JobManagerConfiguration job={job}/> : undefined}
+            {tabKey === 'taskmanager' ? <TaskManagerConfigurationForm job={job}/> : undefined}
+          </ProCard>
+        </PageContainer> : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
+      }
+
+    </>
   );
 
 };
