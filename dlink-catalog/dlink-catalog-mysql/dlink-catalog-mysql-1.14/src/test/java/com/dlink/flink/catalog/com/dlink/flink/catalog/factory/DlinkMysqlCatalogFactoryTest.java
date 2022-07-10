5126bead20f536ffbc0f1bc08033d174d650d53e
Arnu- <Arnu-@users.noreply.github.com>
2022-07-10 12:45:00 +0800

    实现了使用mysql来持久化flink table元数据的catalog。 (#703)

---
+++ dlink-catalog/dlink-catalog-mysql/dlink-catalog-mysql-1.14/src/test/java/com/dlink/flink/catalog/com/dlink/flink/catalog/factory/DlinkMysqlCatalogFactoryTest.java
@@ -0,0 +1,74 @@
+/*
+
+#     __                        
+#    /  |  ____ ___  _          
+#   / / | / __//   // / /       
+#  /_/`_|/_/  / /_//___/        
+create @ 2022/7/9                                
+*/
+package com.dlink.flink.catalog.com.dlink.flink.catalog.factory;
+
+import com.dlink.flink.catalog.DlinkMysqlCatalog;
+import com.dlink.flink.catalog.factory.DlinkMysqlCatalogFactoryOptions;
+import org.apache.flink.table.catalog.Catalog;
+import org.apache.flink.table.catalog.CommonCatalogOptions;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DlinkMysqlCatalogFactoryTest {
+
+    protected static String url;
+    protected static DlinkMysqlCatalog catalog;
+
+    protected static final String TEST_CATALOG_NAME = "mysql-catalog";
+    protected static final String TEST_USERNAME = "flink_metastore";
+    protected static final String TEST_PWD = "flink_metastore";
+
+    @BeforeClass
+    public static void setup() throws SQLException {
+        url = "jdbc:mysql://localhost:3306/flink_metastore?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
+
+        catalog =
+                new DlinkMysqlCatalog(
+                        TEST_CATALOG_NAME,
+                        TEST_USERNAME,
+                        TEST_PWD,
+                        url);
+    }
+
+    @Test
+    public void test() {
+        final Map<String, String> options = new HashMap<>();
+        options.put(CommonCatalogOptions.CATALOG_TYPE.key(), DlinkMysqlCatalogFactoryOptions.IDENTIFIER);
+        options.put(DlinkMysqlCatalogFactoryOptions.USERNAME.key(), TEST_USERNAME);
+        options.put(DlinkMysqlCatalogFactoryOptions.PASSWORD.key(), TEST_PWD);
+        options.put(DlinkMysqlCatalogFactoryOptions.URL.key(), url);
+
+        final Catalog actualCatalog =
+                FactoryUtil.createCatalog(
+                        TEST_CATALOG_NAME,
+                        options,
+                        null,
+                        Thread.currentThread().getContextClassLoader());
+
+        checkEquals(catalog, (DlinkMysqlCatalog) actualCatalog);
+
+        assertTrue( actualCatalog instanceof DlinkMysqlCatalog);
+    }
+
+    private static void checkEquals(DlinkMysqlCatalog c1, DlinkMysqlCatalog c2) {
+        assertEquals(c1.getName(), c2.getName());
+        assertEquals(c1.getDefaultDatabase(), c2.getDefaultDatabase());
+        assertEquals(c1.getUser(), c2.getUser());
+        assertEquals(c1.getPwd(), c2.getPwd());
+        assertEquals(c1.getUrl(), c2.getUrl());
+    }
+}
