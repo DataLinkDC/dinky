8c5f6884113f3fcf02657014449b2a44d947e5b5
zhu-mingye <934230207@qq.com>
2022-08-03 17:09:23 +0800

    [Feature-812][admin,web] add FragmentVariable manager && resourcecenter page (#813)
    * add FragmentVariable manager
    
    * added resourcecenter
    
    * fix sql file , mapper.xml

---
+++ dlink-web/src/pages/FragmentVariable/index.tsx
@@ -0,0 +1,332 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+
+import {DownOutlined, PlusOutlined} from '@ant-design/icons';
+import {Button, Drawer, Modal} from 'antd';
+import React, {useRef, useState} from 'react';
+import {FooterToolbar, PageContainer} from '@ant-design/pro-layout';
+import type {ActionType, ProColumns} from '@ant-design/pro-table';
+import ProTable from '@ant-design/pro-table';
+import ProDescriptions from '@ant-design/pro-descriptions';
+import Dropdown from "antd/es/dropdown/dropdown";
+import Menu from "antd/es/menu";
+import {handleAddOrUpdate, handleRemove, queryData, updateEnabled} from "@/components/Common/crud";
+import FragmentForm from "@/pages/FragmentVariable/components/FragmentForm";
+import {FragmentVariableTableListItem} from "./data.d";
+
+const url = '/api/fragment';
+
+const FragmentTableList: React.FC<{}> = (props: any) => {
+
+  const [modalVisible, handleModalVisible] = useState<boolean>(false);
+  const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState({});
+  const actionRef = useRef<ActionType>();
+  const [row, setRow] = useState<FragmentVariableTableListItem>();
+  const [selectedRowsState, setSelectedRows] = useState<FragmentVariableTableListItem[]>([]);
+
+  const editAndDelete = (key: string | number, currentItem: FragmentVariableTableListItem) => {
+    if (key === 'edit') {
+      handleUpdateModalVisible(true);
+      setFormValues(currentItem);
+    } else if (key === 'delete') {
+      Modal.confirm({
+        title: '删除变量',
+        content: '确定删除该变量吗？',
+        okText: '确认',
+        cancelText: '取消',
+        onOk: async () => {
+          await handleRemove(url, [currentItem]);
+          actionRef.current?.reloadAndRest?.();
+        }
+      });
+    }
+  };
+
+  const MoreBtn: React.FC<{
+    item: FragmentVariableTableListItem;
+  }> = ({item}) => (
+    <Dropdown
+      overlay={
+        <Menu onClick={({key}) => editAndDelete(key, item)}>
+          <Menu.Item key="edit">编辑</Menu.Item>
+          <Menu.Item key="delete">删除</Menu.Item>
+        </Menu>
+      }
+    >
+      <a>
+        更多 <DownOutlined/>
+      </a>
+    </Dropdown>
+  );
+
+  const columns: ProColumns<FragmentVariableTableListItem>[] = [
+    {
+      title: '变量ID',
+      dataIndex: 'id',
+      hideInTable: true,
+      hideInForm: true,
+      hideInSearch: true,
+    },
+    {
+      title: '名称',
+      dataIndex: 'name',
+      tip: '名称是唯一的',
+      sorter: true,
+      render: (dom, entity) => {
+        return <a onClick={() => setRow(entity)}>{dom}</a>;
+      },
+    },
+    {
+      title: '别名',
+      dataIndex: 'alias',
+      hideInForm: false,
+      hideInSearch: false,
+      hideInTable: false,
+    },
+    {
+      title: '变量值',
+      sorter: true,
+      dataIndex: 'fragmentValue',
+      hideInForm: false,
+      hideInSearch: true,
+      hideInTable: true,
+      // ellipsis: true,
+    },
+    {
+      title: '使用变量值',
+      sorter: true,
+      dataIndex: 'fillValue',
+      hideInForm: false,
+      hideInSearch: true,
+      hideInTable: true,
+      // ellipsis: true,
+    },
+    {
+      title: '描述',
+      sorter: true,
+      dataIndex: 'note',
+      valueType: 'textarea',
+      hideInForm: false,
+      hideInSearch: false,
+      hideInTable: false,
+    },
+    {
+      title: '是否启用',
+      dataIndex: 'enabled',
+      hideInForm: false,
+      hideInSearch: true,
+      hideInTable: false,
+      filters: [
+        {
+          text: '已启用',
+          value: 1,
+        },
+        {
+          text: '已禁用',
+          value: 0,
+        },
+      ],
+      filterMultiple: false,
+      valueEnum: {
+        true: {text: '已启用', status: 'Success'},
+        false: {text: '已禁用', status: 'Error'},
+      },
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+      sorter: true,
+      valueType: 'dateTime',
+      hideInForm: true,
+      hideInTable: true,
+    },
+    {
+      title: '最近更新时间',
+      dataIndex: 'updateTime',
+      sorter: true,
+      valueType: 'dateTime',
+      hideInForm: true,
+    },
+    {
+      title: '操作',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => [
+        <a
+          onClick={() => {
+            handleUpdateModalVisible(true);
+            setFormValues(record);
+          }}
+        >
+          配置
+        </a>,
+        <MoreBtn key="more" item={record}/>,
+      ],
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <ProTable<FragmentVariableTableListItem>
+        headerTitle="全局变量列表"
+        actionRef={actionRef}
+        rowKey="id"
+        search={{
+          labelWidth: 120,
+        }}
+        toolBarRender={() => [
+          <Button type="primary" onClick={() => handleModalVisible(true)}>
+            <PlusOutlined/> 新建
+          </Button>,
+        ]}
+        request={(params, sorter, filter) => queryData(url, {...params, ...sorter, ...filter})}
+        columns={columns}
+        rowSelection={{
+          onChange: (_, selectedRows) => setSelectedRows(selectedRows),
+        }}
+      />
+      {selectedRowsState?.length > 0 && (
+        <FooterToolbar
+          extra={
+            <div>
+              已选择 <a style={{fontWeight: 600}}>{selectedRowsState.length}</a> 项&nbsp;&nbsp;
+              <span>
+                被禁用的全局变量共 {selectedRowsState.length - selectedRowsState.reduce((pre, item) => pre + (item.enabled ? 1 : 0), 0)} 个
+              </span>
+            </div>
+          }
+        >
+          <Button type="primary" danger
+                  onClick={() => {
+                    Modal.confirm({
+                      title: '删除全局变量',
+                      content: '确定删除选中的全局变量吗？',
+                      okText: '确认',
+                      cancelText: '取消',
+                      onOk: async () => {
+                        await handleRemove(url, selectedRowsState);
+                        setSelectedRows([]);
+                        actionRef.current?.reloadAndRest?.();
+                      }
+                    });
+                  }}
+          >
+            批量删除
+          </Button>
+          <Button type="primary"
+                  onClick={() => {
+                    Modal.confirm({
+                      title: '启用全局变量',
+                      content: '确定启用选中的全局变量吗？',
+                      okText: '确认',
+                      cancelText: '取消',
+                      onOk: async () => {
+                        await updateEnabled(url, selectedRowsState, true);
+                        setSelectedRows([]);
+                        actionRef.current?.reloadAndRest?.();
+                      }
+                    });
+                  }}
+          >批量启用</Button>
+          <Button danger
+                  onClick={() => {
+                    Modal.confirm({
+                      title: '禁用全局变量',
+                      content: '确定禁用选中的全局变量吗？',
+                      okText: '确认',
+                      cancelText: '取消',
+                      onOk: async () => {
+                        await updateEnabled(url, selectedRowsState, false);
+                        setSelectedRows([]);
+                        actionRef.current?.reloadAndRest?.();
+                      }
+                    });
+                  }}
+          >批量禁用</Button>
+        </FooterToolbar>
+      )}
+      <FragmentForm
+        onSubmit={async (value) => {
+          const success = await handleAddOrUpdate(url, value);
+          if (success) {
+            handleModalVisible(false);
+            setFormValues({});
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+        onCancel={() => {
+          handleModalVisible(false);
+        }}
+        modalVisible={modalVisible}
+        values={{}}
+      />
+      {
+        formValues && Object.keys(formValues).length ? (
+          <FragmentForm
+            onSubmit={async (value) => {
+              const success = await handleAddOrUpdate(url, value);
+              if (success) {
+                handleUpdateModalVisible(false);
+                setFormValues({});
+                if (actionRef.current) {
+                  actionRef.current.reload();
+                }
+              }
+            }}
+            onCancel={() => {
+              handleUpdateModalVisible(false);
+              setFormValues({});
+            }}
+            modalVisible={updateModalVisible}
+            values={formValues}
+          />
+        ) : null
+      }
+      <Drawer
+        width={600}
+        visible={!!row}
+        onClose={() => {
+          setRow(undefined);
+        }}
+        closable={false}
+      >
+        {row?.name && (
+          <ProDescriptions<FragmentVariableTableListItem>
+            column={1}
+            title={row?.name}
+            request={async () => ({
+              data: row || {},
+            })}
+            params={{
+              id: row?.id,
+            }}
+            columns={columns}
+          />
+        )}
+      </Drawer>
+    </PageContainer>
+  )
+    ;
+};
+
+export default FragmentTableList;
