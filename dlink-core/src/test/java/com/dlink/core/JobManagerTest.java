a9119cbaa79cbfc14c1cdeb65ddcfcf3c57157de
wenmo <32723967+wenmo@users.noreply.github.com>
2022-08-20 00:47:44 +0800

    [Feature-915][admin,core] Add global variables takes effect in flinksql

--- dlink-core/src/test/java/com/dlink/core/JobManagerTest.java
+++ dlink-core/src/test/java/com/dlink/core/JobManagerTest.java
@@ -20,14 +20,15 @@
 
 package com.dlink.core;
 
+import java.util.HashMap;
+
+import org.junit.Test;
+
 import com.dlink.job.JobConfig;
 import com.dlink.job.JobManager;
 import com.dlink.job.JobResult;
 import com.dlink.result.ResultPool;
 import com.dlink.result.SelectResult;
-import org.junit.Test;
-
-import java.util.HashMap;
 
 /**
  * JobManagerTest
@@ -41,20 +42,20 @@ public class JobManagerTest {
     public void cancelJobSelect() {
 
         JobConfig config = new JobConfig("session-yarn", true, true, true, true, "s1", 2,
-                null, null, null, "测试", false, false, false, 100, 0,
-                1, 0, null, new HashMap<>());
+            null, null, null, "测试", false, false, false, 100, 0,
+            1, 0, null, new HashMap<>(), new HashMap<>());
         if (config.isUseRemote()) {
             config.setAddress("192.168.123.157:8081");
         }
         JobManager jobManager = JobManager.build(config);
         String sql1 = "CREATE TABLE Orders (\n" +
-                "    order_number BIGINT,\n" +
-                "    price        DECIMAL(32,2),\n" +
-                "    order_time   TIMESTAMP(3)\n" +
-                ") WITH (\n" +
-                "  'connector' = 'datagen',\n" +
-                "  'rows-per-second' = '1'\n" +
-                ");";
+            "    order_number BIGINT,\n" +
+            "    price        DECIMAL(32,2),\n" +
+            "    order_time   TIMESTAMP(3)\n" +
+            ") WITH (\n" +
+            "  'connector' = 'datagen',\n" +
+            "  'rows-per-second' = '1'\n" +
+            ");";
         String sql3 = "select order_number,price,order_time from Orders";
         String sql = sql1 + sql3;
         JobResult result = jobManager.executeSql(sql);
