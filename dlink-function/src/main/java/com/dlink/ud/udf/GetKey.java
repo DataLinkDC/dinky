a6778238022bbc383797adf9c0cd3edbfc982221
lixiaoping <17773406760@189.cn>
2022-08-29 21:19:23 +0800

    [Fix][Optimization][Style] 修订TypeImformation原因，自定义隐函数不能在Flink 1.14.x 版本以上运行。

--- dlink-function/src/main/java/com/dlink/ud/udf/GetKey.java
+++ dlink-function/src/main/java/com/dlink/ud/udf/GetKey.java
@@ -21,25 +21,22 @@ package com.dlink.ud.udf;
 
 import org.apache.flink.table.functions.ScalarFunction;
 
-/**
- * GetKey
- *
- * @author wenmo
- * @since 2021/5/25 15:50
- **/
+import java.util.Objects;
+
 public class GetKey extends ScalarFunction {
 
-    public String eval(String map, String key, String defaultValue) {
+    public int eval(String map, String key, int defaultValue) {
         if (map == null || !map.contains(key)) {
             return defaultValue;
         }
-        String[] maps = map.replaceAll("\\{", "").replaceAll("\\}", "").split(",");
-        for (int i = 0; i < maps.length; i++) {
-            String[] items = maps[i].split("=");
-            if (items.length >= 2) {
-                if (key.equals(items[0].trim())) {
-                    return items[1];
-                }
+
+        map = map.replace("{", "").replace("}", "");
+        String[] maps = map.split(", ");
+
+        for (String s : maps) {
+            String[] items = s.split("=");
+            if (items.length == 2 && Objects.equals(key, items[0])) {
+                return Integer.parseInt(items[1]);
             }
         }
         return defaultValue;
