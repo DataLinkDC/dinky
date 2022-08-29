30306e5a3cb34eeba6513118219fa2fa9e2e897a
lixiaoping <17773406760@189.cn>
2022-08-29 21:19:23 +0800

    [Fix][Optimization][Style] 修订TypeImformation原因，自定义隐函数不能在Flink 1.14.x 版本以上运行。

--- dlink-function/src/main/java/com/dlink/ud/udtaf/RowsToMap.java
+++ dlink-function/src/main/java/com/dlink/ud/udtaf/RowsToMap.java
@@ -19,49 +19,98 @@
 
 package com.dlink.ud.udtaf;
 
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.catalog.DataTypeFactory;
 import org.apache.flink.table.functions.TableAggregateFunction;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.inference.TypeInference;
 import org.apache.flink.util.Collector;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
 
 /**
+ *
  * RowsToMap
  *
  * @author wenmo
  * @since 2021/5/25 15:50
  **/
-public class RowsToMap extends TableAggregateFunction<String, Map> {
+
+public class RowsToMap extends TableAggregateFunction<String, Map<String, Integer>> {
+
     @Override
-    public Map createAccumulator() {
-        return new HashMap();
+    public Map<String, Integer> createAccumulator() {
+        return new HashMap<>(16);
     }
 
-    public void accumulate(Map acc, String cls, Object v, String key) {
-        String[] keys = key.split(",");
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i].equals(cls)) {
+    @Override
+    public TypeInference getTypeInference(DataTypeFactory typeFactory) {
+        return TypeInference.newBuilder()
+            .accumulatorTypeStrategy(callContext -> {
+                    DataType accDataType = DataTypes.MAP(
+                        DataTypes.STRING(), DataTypes.INT());
+                    return Optional.of(accDataType);
+
+                }
+            )
+            .outputTypeStrategy(callContext -> Optional.of(DataTypes.STRING()))
+            .build();
+
+    }
+
+    public void accumulate(
+        Map<String, Integer> acc,
+        String cls,
+        Integer v) {
+        if (v == null) {
+            return;
+        }
+
+        String[] keys = cls.split(",");
+        for (String s : keys) {
+            if (s.equals(cls)) {
                 acc.put(cls, v);
             }
         }
     }
 
-    public void accumulate(Map acc, String cls, Object v) {
-        acc.put(cls, v);
+    /**
+     * Retracts the input values from the accumulator instance. The current design assumes the
+     * inputs are the values that have been previously accumulated. The method retract can be
+     * overloaded with different custom types and arguments. This function must be implemented for
+     * datastream bounded over aggregate.
+     *
+     * @param acc           the accumulator which contains the current aggregated results
+     */
+    public void retract(Map<String, Integer> acc, String cls, Integer v) {
+        if (v == null) {
+            return;
+        }
+        acc.remove(cls);
     }
 
-    public void merge(Map acc, Iterable<Map> iterable) {
-        for (Map otherAcc : iterable) {
-            Iterator iter = otherAcc.entrySet().iterator();
-            while (iter.hasNext()) {
-                Map.Entry entry = (Map.Entry) iter.next();
-                accumulate(acc, entry.getKey().toString(), entry.getValue());
+    /**
+     * Merges a group of accumulator instances into one accumulator instance. This function must be
+     * implemented for datastream session window grouping aggregate and bounded grouping aggregate.
+     *
+     * @param acc  the accumulator which will keep the merged aggregate results. It should
+     *                     be noted that the accumulator may contain the previous aggregated
+     *                     results. Therefore user should not replace or clean this instance in the
+     *                     custom merge method.
+     * @param iterable          an {@link java.lang.Iterable} pointed to a group of accumulators that will be
+     *                     merged.
+     */
+    public void merge(Map<String, Integer> acc, Iterable<Map<String, Integer>> iterable) {
+        for (Map<String, Integer> otherAcc : iterable) {
+            for (Map.Entry<String, Integer> entry : otherAcc.entrySet()) {
+                accumulate(acc, entry.getKey(), entry.getValue());
             }
         }
     }
 
-    public void emitValue(Map acc, Collector<String> out) {
+    public void emitValue(Map<String, Integer> acc, Collector<String> out) {
         out.collect(acc.toString());
     }
 }
