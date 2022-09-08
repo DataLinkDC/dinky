25ab725194bbcf036d9cd4bf5444335e76d557ab
郑文豪 <1103846001@qq.com>
2022-09-08 15:30:08 +0800

    添加第三方海豚调度,遗漏对象

---
+++ dlink-scheduler/src/main/java/com/dlink/scheduler/model/Property.java
@@ -0,0 +1,33 @@
+package com.dlink.scheduler.model;
+
+import com.dlink.scheduler.enums.DataType;
+import com.dlink.scheduler.enums.Direct;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Property implements Serializable {
+
+    private static final long serialVersionUID = -4045513703397452451L;
+    /**
+     * key
+     */
+    private String prop;
+
+    /**
+     * input/output
+     */
+    private Direct direct;
+
+    /**
+     * data type
+     */
+    private DataType type;
+
+    /**
+     * value
+     */
+    private String value;
+
+}
