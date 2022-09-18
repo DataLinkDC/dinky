9706570ac889816769a2022afe84743b4864d5e6
wenmo <32723967+wenmo@users.noreply.github.com>
2022-09-18 22:00:35 +0800

    [Feature-1027][admin,web] Add data development task information log details button

--- dlink-web/src/components/Studio/StudioConsole/StudioMsg/index.tsx
+++ dlink-web/src/components/Studio/StudioConsole/StudioMsg/index.tsx
@@ -18,23 +18,39 @@
  */
 
 
-import {Typography, Divider, Badge, Empty,Tag} from "antd";
+import {Badge, Button, Divider, Empty, Modal, Tag, Typography} from "antd";
 import {StateType} from "@/pages/DataStudio/model";
 import {connect} from "umi";
-import {FireOutlined, ScheduleOutlined} from '@ant-design/icons';
-import StudioSqlConfig from "@/components/Studio/StudioRightTool/StudioSqlConfig";
-import {DIALECT, isSql} from "@/components/Studio/conf";
+import {FireOutlined, ZoomInOutlined} from '@ant-design/icons';
+import {isSql} from "@/components/Studio/conf";
+import {useState} from "react";
+import CodeShow from "@/components/Common/CodeShow";
 
-const { Title, Paragraph, Text, Link } = Typography;
+const {Title, Paragraph, Text, Link} = Typography;
 
-const StudioMsg = (props:any) => {
+const StudioMsg = (props: any) => {
 
   const {current} = props;
+  const [sqlModalVisit, setSqlModalVisit] = useState(false);
+  const [errorModalVisit, setErrorModalVisit] = useState(false);
+
+  const handleOpenSqlModal = () => {
+    setSqlModalVisit(true);
+  };
+
+  const handleOpenErrorModal = () => {
+    setErrorModalVisit(true);
+  };
+
+  const handleCancel = () => {
+    setSqlModalVisit(false);
+    setErrorModalVisit(false);
+  };
 
   const renderCommonSqlContent = () => {
     return (<>
       <Paragraph>
-        <blockquote> <Divider type="vertical"/>{current.console.result.startTime}
+        <blockquote><Divider type="vertical"/>{current.console.result.startTime}
           <Divider type="vertical"/>{current.console.result.endTime}
           <Divider type="vertical"/>
           {!(current.console.result.success) ? <><Badge status="error"/><Text type="danger">Error</Text></> :
@@ -55,17 +71,34 @@ const StudioMsg = (props:any) => {
         </Link> <Divider type="vertical"/>{current.console.result.startTime}
           <Divider type="vertical"/>{current.console.result.endTime}
           <Divider type="vertical"/>
-          {!(current.console.result.status==='SUCCESS') ? <><Badge status="error"/><Text type="danger">Error</Text></> :
+          {!(current.console.result.status === 'SUCCESS') ? <><Badge status="error"/><Text
+              type="danger">Error</Text></> :
             <><Badge status="success"/><Text type="success">Success</Text></>}
           <Divider type="vertical"/>
-          {current.console.result.jobConfig?.jobName&&<Text code>{current.console.result.jobConfig?.jobName}</Text>}
-          {current.console.result.jobId&&
+          {current.console.result.jobConfig?.jobName && <Text code>{current.console.result.jobConfig?.jobName}</Text>}
+          {current.console.result.jobId &&
             (<>
               <Divider type="vertical"/>
               <Tag color="blue" key={current.console.result.jobId}>
-                <FireOutlined /> {current.console.result.jobId}
+                <FireOutlined/> {current.console.result.jobId}
               </Tag>
             </>)}
+          <Button
+            type="text"
+            icon={<ZoomInOutlined/>}
+            onClick={handleOpenSqlModal}
+          >
+            SQL
+          </Button>
+          {current.console.result.error ?
+            <Button
+              type="text"
+              icon={<ZoomInOutlined/>}
+              onClick={handleOpenErrorModal}
+            >
+              Error
+            </Button> : undefined
+          }
         </blockquote>
         {current.console.result.statement && (<pre style={{height: '100px'}}>{current.console.result.statement}</pre>)}
         {current.console.result.error && (<pre style={{height: '100px'}}>{current.console.result.error}</pre>)}
@@ -75,14 +108,38 @@ const StudioMsg = (props:any) => {
 
 
   return (
-    <Typography>
-      {current?.task&&current.console.result.startTime?(isSql(current.task.dialect) ? renderCommonSqlContent():
-        renderFlinkSqlContent() ):<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-      }
-    </Typography>
+    <>
+      <Typography>
+        {current?.task && current.console.result.startTime ? (isSql(current.task.dialect) ? renderCommonSqlContent() :
+          renderFlinkSqlContent()) : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
+        }
+      </Typography>
+      <Modal
+        width={'100%'}
+        visible={sqlModalVisit}
+        destroyOnClose
+        centered
+        footer={false}
+        onCancel={handleCancel}
+      >
+        {current.console.result.statement &&
+          (<CodeShow height={"80vh"} language={"sql"} code={current.console.result.statement} theme={"vs-dark"}/>)}
+      </Modal>
+      <Modal
+        width={'100%'}
+        visible={errorModalVisit}
+        destroyOnClose
+        centered
+        footer={false}
+        onCancel={handleCancel}
+      >
+        {current.console.result.error &&
+          (<CodeShow height={"80vh"} language={"java"} code={current.console.result.error} theme={"vs-dark"}/>)}
+      </Modal>
+    </>
   );
 };
 
-export default connect(({ Studio }: { Studio: StateType }) => ({
+export default connect(({Studio}: { Studio: StateType }) => ({
   current: Studio.current,
 }))(StudioMsg);
