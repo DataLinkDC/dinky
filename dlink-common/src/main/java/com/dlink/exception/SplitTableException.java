20f7991c9ddcc90ae351dea2f5c38471c4c2996d
ZackYoung <42713096+zackyoungh@users.noreply.github.com>
2022-09-20 09:38:48 +0800

    [Feature][dlink-client]CDCSOURCE Add sub database and sub table support (#994)
    * add cdcsource split table function
    
    * optimization java code style

---
+++ dlink-common/src/main/java/com/dlink/exception/SplitTableException.java
@@ -0,0 +1,16 @@
+package com.dlink.exception;
+
+/**
+ * @author ZackYoung
+ * @version 1.0
+ * @since 2022/9/2
+ */
+public class SplitTableException extends RuntimeException {
+    public SplitTableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SplitTableException(String message) {
+        super(message);
+    }
+}
