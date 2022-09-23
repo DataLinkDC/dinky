15170768931403d6087c9a5e662dde67ee2e5ff1
Zack Young <1052455797@qq.com>
2022-09-23 21:31:11 +0800

    add build udf jar

--- dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
+++ dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
@@ -461,6 +461,15 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
     }
 
     @Override
+    public List<Task> getAllUDF() {
+        List<Task> tasks = list(new QueryWrapper<Task>().eq("dialect", "Java").eq("enabled", 1).isNotNull("save_point_path"));
+        return tasks.stream().peek(task -> {
+            Assert.check(task);
+            task.setStatement(statementService.getById(task.getId()).getStatement());
+        }).collect(Collectors.toList());
+    }
+
+    @Override
     public Result releaseTask(Integer id) {
         Task task = getTaskInfoById(id);
         Assert.check(task);
