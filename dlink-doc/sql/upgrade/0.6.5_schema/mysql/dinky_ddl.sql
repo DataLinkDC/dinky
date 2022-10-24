2e37c4d154e99b579693c76b2182c9ed138371e6
zhu-mingye <934230207@qq.com>
2022-10-24 13:19:46 +0800

    [Fix][web,sql] hotfix some problems (#1136)
    * Optimize some problems
    
    * Optimize some problems
    
    * Optimize some sql comment to en-us
    
    * Optimize main sql file comment to en-us
    
    * delete some update sql
    
    * Optimize main sql file

--- dlink-doc/sql/upgrade/0.6.5_schema/mysql/dinky_ddl.sql
+++ dlink-doc/sql/upgrade/0.6.5_schema/mysql/dinky_ddl.sql
@@ -19,21 +19,21 @@
 
 -- 0.6.5 2022-06-28
 -- ----------------------------
-alter table dlink_task ADD COLUMN `version_id` INT NULL COMMENT '版本号ID';
+alter table dlink_task ADD COLUMN `version_id` INT NULL COMMENT 'version id';
 
 -- ----------------------------
 -- Table structure for dlink_task_version
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `dlink_task_version`(
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `task_id` int NOT NULL COMMENT '作业ID ',
-  `version_id` int NOT NULL COMMENT '版本ID ',
-  `statement` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'flink sql 内容',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
-  `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '别名',
-  `dialect` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '方言',
-  `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '类型',
-  `task_configure` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '作业配置',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `task_id` int NOT NULL COMMENT 'task ID ',
+  `version_id` int NOT NULL COMMENT 'version ID ',
+  `statement` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'flink sql statement',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'version name',
+  `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'alisa',
+  `dialect` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'dialect',
+  `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'type',
+  `task_configure` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'task configuration',
+  `create_time` datetime DEFAULT NULL COMMENT 'create time',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='作业历史版本';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='job history version';
--- dlink-doc/sql/upgrade/0.6.8_schema/mysql/dinky_dml.sql
+++ dlink-doc/sql/upgrade/0.6.8_schema/mysql/dinky_dml.sql
@@ -27,13 +27,13 @@ VALUES (1, 'DefaultTenant', 0, 'DefaultTenant', current_time, current_time);
 -- Records of dlink_role
 -- ----------------------------
 INSERT INTO `dlink_role`(`id`, `tenant_id`, `role_code`, `role_name`, `is_delete`, `note`, `create_time`, `update_time`)
-VALUES (1, 1, 'SuperAdmin', '超级管理员', 0, '超级管理员角色', current_time, current_time);
+VALUES (1, 1, 'SuperAdmin', 'SuperAdmin', 0, 'SuperAdmin of Role', current_time, current_time);
 
 -- ----------------------------
 -- Records of dlink_namespace
 -- ----------------------------
 INSERT INTO `dlink_namespace`(`id`, `tenant_id`, `namespace_code`, `enabled`, `note`, `create_time`, `update_time`)
-VALUES (1, 1, 'DefaultNameSpace', 1, '默认命名空间', current_time, current_time);
+VALUES (1, 1, 'DefaultNameSpace', 1, 'DefaultNameSpace', current_time, current_time);
 
 -- ----------------------------
 -- Records of dlink_role_namespace
@@ -62,3 +62,5 @@ VALUES (1, 'java_udf', 'java', 'UDF', 'package ${package};\n\nimport org.apache.
 
 INSERT INTO `dlink_udf_template` (`id`, `name`, `code_type`, `function_type`, `template_code`, `enabled`, `create_time`, `update_time`)
 VALUES (2, 'java_udtf', 'java', 'UDTF', 'package ${package};\n\nimport org.apache.flink.table.functions.ScalarFunction;\n\n@FunctionHint(output = @DataTypeHint(\"ROW<word STRING, length INT>\"))\npublic static class ${className} extends TableFunction<Row> {\n\n  public void eval(String str) {\n    for (String s : str.split(\" \")) {\n      // use collect(...) to emit a row\n      collect(Row.of(s, s.length()));\n    }\n  }\n}', NULL, '2022-10-19 09:22:58', '2022-10-19 10:01:57');
+
+
