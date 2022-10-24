3f5b8da1a0424f15e59289aa48c7988324d295e9
Licho <lecho.sun@gmail.com>
2022-10-24 10:54:58 +0800

    refactor: some trick modification (#1135)
    * refactor: some trick modification
    
    * refactor: some trick modification
    
    * fix: checkstyle
    
    Signed-off-by: licho <lecho.sun@gmail.com>
    
    Signed-off-by: licho <lecho.sun@gmail.com>

--- dlink-executor/src/main/java/com/dlink/trans/Operations.java
+++ dlink-executor/src/main/java/com/dlink/trans/Operations.java
@@ -26,6 +26,8 @@ import com.dlink.trans.ddl.SetOperation;
 import com.dlink.trans.ddl.ShowFragmentOperation;
 import com.dlink.trans.ddl.ShowFragmentsOperation;
 
+import java.util.Arrays;
+
 /**
  * Operations
  *
@@ -34,8 +36,10 @@ import com.dlink.trans.ddl.ShowFragmentsOperation;
  **/
 public class Operations {
 
-    private static Operation[] operations = {
-        new CreateAggTableOperation()
+    private Operations() {
+    }
+
+    private static final Operation[] ALL_OPERATIONS = {new CreateAggTableOperation()
             , new SetOperation()
             , new CreateCDCSourceOperation()
             , new ShowFragmentsOperation()
@@ -70,12 +74,15 @@ public class Operations {
     }
 
     public static Operation buildOperation(String statement) {
-        String sql = statement.replace("\n", " ").replaceAll("\\s{1,}", " ").trim().toUpperCase();
-        for (int i = 0; i < operations.length; i++) {
-            if (sql.startsWith(operations[i].getHandle())) {
-                return operations[i].create(statement);
-            }
-        }
-        return null;
+        String sql = statement.replace("\n", " ")
+                .replaceAll("\\s+", " ")
+                .trim()
+                .toUpperCase();
+
+        return Arrays.stream(ALL_OPERATIONS)
+                .filter(p -> sql.startsWith(p.getHandle()))
+                .findFirst()
+                .map(p -> p.create(statement))
+                .orElse(null);
     }
 }
