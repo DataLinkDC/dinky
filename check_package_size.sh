3e510d07fa6331c7f2c57dd44278ca86dbe67e61
Zack Young <1052455797@qq.com>
2022-11-02 11:31:02 +0800

    测试检查包大小

--- check_package_size.sh
+++ check_package_size.sh
@@ -2,8 +2,8 @@ cd build
 
 total_text=$(ls -ls | grep total)
 total_size=${total_text/'total '/''}
-echo $(ls -lh)
-echo $total_size
+echo `ls -lh`
+
 if [[ ${total_size} -gt 20480 ]]; then
   echo '尺寸为：${total_size}，大于200mb，编译失败'
   exit 1
