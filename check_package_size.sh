8fa6d439a94e239d4ebc5e122d96c891b4e35c81
Zack Young <1052455797@qq.com>
2022-11-02 11:43:47 +0800

    测试检查包大小，正常状态

--- check_package_size.sh
+++ check_package_size.sh
@@ -4,7 +4,7 @@ total_text=$(ls -ls | grep total)
 total_size=${total_text/'total '/''}
 echo `ls -lh`
 
-if [[ ${total_size} -gt 20480 ]]; then
+if [[ ${total_size} -gt 204800 ]]; then
   echo '尺寸为：${total_size}，大于200mb，编译失败'
   exit 1
 else
