a615e60b0e70645e7b647ddd2474f3df8d1aa443
Licho <chinabhsun@gmail.com>
2022-11-23 14:21:22 +0800

    fix: 1.14 Executor not found

--- dlink-client/dlink-client-1.14/src/main/java/com/dlink/executor/AbstractCustomTableEnvironment.java
+++ dlink-client/dlink-client-1.14/src/main/java/com/dlink/executor/AbstractCustomTableEnvironment.java
@@ -23,6 +23,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.bridge.java.internal.StreamTableEnvironmentImpl;
+import org.apache.flink.table.delegation.Executor;
 import org.apache.flink.table.delegation.Planner;
 
 /**
