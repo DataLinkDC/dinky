602bd9c0e62c987b837d2b7e988341e2309a9de5
ikiler <ikiler@126.com>
2022-11-24 21:10:07 +0800

    bug fix k8s application savepoint failed

--- dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
+++ dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
@@ -752,20 +752,18 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
         JobConfig jobConfig = new JobConfig();
         jobConfig.setAddress(cluster.getJobManagerHost());
         jobConfig.setType(cluster.getType());
+        Task task = this.getTaskInfoById(cluster.getTaskId());
 
-        if (GatewayType.KUBERNETES_APPLICATION.equalsValue(cluster.getType())) {
-            Statement statement = statementService.getById(cluster.getTaskId());
-            Map<String, Object> clusterConfiguration =
-                    clusterConfigurationService.getGatewayConfig(cluster.getClusterConfigurationId());
-            Map<String, Object> gatewayConfig = JSONUtil.toMap(statement.getStatement(), String.class, Object.class);
-            gatewayConfig.putAll(clusterConfiguration);
-            jobConfig.buildGatewayConfig(gatewayConfig);
-            jobConfig.getGatewayConfig().getClusterConfig().setAppId(cluster.getName());
-            useGateway = true;
-        }
         if (Asserts.isNotNull(cluster.getClusterConfigurationId())) {
             Map<String, Object> gatewayConfig =
                     clusterConfigurationService.getGatewayConfig(cluster.getClusterConfigurationId());
+            // 如果是k8s application 模式，则需要补齐statement 内的自定义配置
+            if (Dialect.KUBERNETES_APPLICATION.equalsVal(task.getDialect())) {
+                Statement statement = statementService.getById(cluster.getTaskId());
+                Map<String, Object> statementConfig =
+                        JSONUtil.toMap(statement.getStatement(), String.class, Object.class);
+                gatewayConfig.putAll(statementConfig);
+            }
             jobConfig.buildGatewayConfig(gatewayConfig);
             jobConfig.getGatewayConfig().getClusterConfig().setAppId(cluster.getName());
             useGateway = true;
