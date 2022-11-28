213674f8454e7b6387c2797b518743940e6dee02
xiaowu <1766128467@qq.com>
2022-11-28 18:26:08 +0800

    Improve and clean up unused code in AbstractCDCBuilder Fix checkstyle in client module

--- dlink-client/dlink-client-1.12/src/main/java/com/dlink/cdc/mysql/MysqlCDCBuilder.java
+++ dlink-client/dlink-client-1.12/src/main/java/com/dlink/cdc/mysql/MysqlCDCBuilder.java
@@ -29,8 +29,6 @@ import com.dlink.model.FlinkCDCConfig;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -76,10 +74,10 @@ public class MysqlCDCBuilder extends AbstractCDCBuilder implements CDCBuilder {
             }
         }
         MySQLSource.Builder<String> sourceBuilder = MySQLSource.<String>builder()
-            .hostname(config.getHostname())
-            .port(config.getPort())
-            .username(config.getUsername())
-            .password(config.getPassword());
+                .hostname(config.getHostname())
+                .port(config.getPort())
+                .username(config.getUsername())
+                .password(config.getPassword());
         String database = config.getDatabase();
         if (Asserts.isNotNullString(database)) {
             String[] databases = database.split(FlinkParamConstant.SPLIT);
@@ -111,26 +109,6 @@ public class MysqlCDCBuilder extends AbstractCDCBuilder implements CDCBuilder {
         return env.addSource(sourceBuilder.build(), "MySQL CDC Source");
     }
 
-    public List<String> getSchemaList() {
-        List<String> schemaList = new ArrayList<>();
-        String schema = config.getDatabase();
-        if (Asserts.isNotNullString(schema)) {
-            String[] schemas = schema.split(FlinkParamConstant.SPLIT);
-            Collections.addAll(schemaList, schemas);
-        }
-        List<String> tableList = getTableList();
-        for (String tableName : tableList) {
-            tableName = tableName.trim();
-            if (Asserts.isNotNullString(tableName) && tableName.contains(".")) {
-                String[] names = tableName.split("\\\\.");
-                if (!schemaList.contains(names[0])) {
-                    schemaList.add(names[0]);
-                }
-            }
-        }
-        return schemaList;
-    }
-
     public Map<String, Map<String, String>> parseMetaDataConfigs() {
         Map<String, Map<String, String>> allConfigMap = new HashMap<>();
         List<String> schemaList = getSchemaList();
@@ -150,5 +128,10 @@ public class MysqlCDCBuilder extends AbstractCDCBuilder implements CDCBuilder {
             allConfigMap.put(schema, configMap);
         }
         return allConfigMap;
+    }
+
+    @Override
+    public String getSchema() {
+        return config.getDatabase();
     }
 }
--- dlink-client/dlink-client-1.14/src/main/java/com/dlink/cdc/oracle/OracleCDCBuilder.java
+++ dlink-client/dlink-client-1.14/src/main/java/com/dlink/cdc/oracle/OracleCDCBuilder.java
@@ -75,11 +75,11 @@ public class OracleCDCBuilder extends AbstractCDCBuilder implements CDCBuilder {
             }
         }
         OracleSource.Builder<String> sourceBuilder = OracleSource.<String>builder()
-            .hostname(config.getHostname())
-            .port(config.getPort())
-            .username(config.getUsername())
-            .password(config.getPassword())
-            .database(config.getDatabase());
+                .hostname(config.getHostname())
+                .port(config.getPort())
+                .username(config.getUsername())
+                .password(config.getPassword())
+                .database(config.getDatabase());
         String schema = config.getSchema();
         if (Asserts.isNotNullString(schema)) {
             String[] schemas = schema.split(FlinkParamConstant.SPLIT);
@@ -130,5 +130,10 @@ public class OracleCDCBuilder extends AbstractCDCBuilder implements CDCBuilder {
             allConfigList.put(schema, configMap);
         }
         return allConfigList;
+    }
+
+    @Override
+    public String getSchema() {
+        return config.getSchema();
     }
 }
