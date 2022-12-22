0e81d3c21e567bde8d6d4ef72a1f75b47940e6bf
Licho <chinabhsun@gmail.com>
2022-12-22 10:45:05 +0800

    refactor: remote unused code
    Signed-off-by: Licho <chinabhsun@gmail.com>

--- dlink-web/src/components/Studio/StudioEdit/index.tsx
+++ dlink-web/src/components/Studio/StudioEdit/index.tsx
@@ -112,8 +112,6 @@ const FlinkSqlEditor = (props:any) => {
           suggestions:buildSuggestions(),
         };
       },
-      // quickSuggestions: false,
-      // triggerCharacters: ['$', '.', '='],
     });
   };
 
