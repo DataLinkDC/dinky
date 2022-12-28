d997339c24d7c2b8d29d78e76cdf93544aaff825
ZackYoung <zackyoungh@163.com>
2022-12-28 10:28:42 +0800

    add github action(auto build Dockerfile image)

--- check_package_size.sh
+++ check_package_size.sh
@@ -4,8 +4,8 @@ total_text=$(ls -ls | grep total)
 total_size=${total_text/'total '/''}
 echo `ls -lh`
 
-if [[ ${total_size} -gt 204800 ]]; then
-  echo "尺寸为：$total_size，大于200mb，编译失败"
+if [[ ${total_size} -gt 256000 ]]; then
+  echo "尺寸为：$total_size，大于250mb，编译失败"
   exit 1
 else
   exit 0
