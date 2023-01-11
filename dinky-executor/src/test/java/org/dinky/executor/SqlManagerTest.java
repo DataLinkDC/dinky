2ce57def8f6bbd42a51700bf895d46a9cd4be57b
licho <lecho.sun@gmail.com>
2023-01-11 21:57:46 +0800

    feat: add junit test
    Signed-off-by: licho <lecho.sun@gmail.com>

---
+++ dinky-executor/src/test/java/org/dinky/executor/SqlManagerTest.java
@@ -0,0 +1,16 @@
+package org.dinky.executor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+class SqlManagerTest {
+
+    @Test
+    void getSqlFragment() {
+        SqlManager sqlManager = new SqlManager();
+    }
+}
