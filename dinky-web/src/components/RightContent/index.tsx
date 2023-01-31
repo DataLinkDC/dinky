9672be5b9ef2e1def35e3a854194bfa2e6b59356
wenqiang.zhang <13701701451@163.com>
2023-01-31 15:49:45 +0800

    add dinky-web module

---
+++ dinky-web/src/components/RightContent/index.tsx
@@ -0,0 +1,57 @@
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { SelectLang, useModel } from '@umijs/max';
+import React from 'react';
+import Avatar from './AvatarDropdown';
+
+export type SiderTheme = 'light' | 'dark';
+
+const GlobalHeaderRight: React.FC = () => {
+  const className = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      height: '48px',
+      marginLeft: 'auto',
+      overflow: 'hidden',
+      gap: 8,
+    };
+  });
+
+  const actionClassName = useEmotionCss(({ token }) => {
+    return {
+      display: 'flex',
+      float: 'right',
+      height: '48px',
+      marginLeft: 'auto',
+      overflow: 'hidden',
+      cursor: 'pointer',
+      padding: '0 12px',
+      borderRadius: token.borderRadius,
+      '&:hover': {
+        backgroundColor: token.colorBgTextHover,
+      },
+    };
+  });
+
+  const { initialState } = useModel('@@initialState');
+
+  if (!initialState || !initialState.settings) {
+    return null;
+  }
+
+  return (
+    <div className={className}>
+      <span
+        className={actionClassName}
+        onClick={() => {
+          window.open('https://pro.ant.design/docs/getting-started');
+        }}
+      >
+        <QuestionCircleOutlined />
+      </span>
+      <Avatar />
+      <SelectLang className={actionClassName} />
+    </div>
+  );
+};
+export default GlobalHeaderRight;
