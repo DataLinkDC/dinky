da2cc29bf87047bfa75c883640710756292ae82f
Licho <chinabhsun@gmail.com>
2023-02-01 11:03:57 +0800

    style: spotless
    Signed-off-by: Licho <chinabhsun@gmail.com>

--- dinky-gateway/src/test/java/org/dinky/gateway/config/ActionTypeTest.java
+++ dinky-gateway/src/test/java/org/dinky/gateway/config/ActionTypeTest.java
@@ -1,21 +1,35 @@
-package org.dinky.gateway.config;
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+package org.dinky.gateway.config;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-/**
- *
- */
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/** */
 class ActionTypeTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "savepoint, SAVEPOINT",
-            "cancel, CANCEL"
-    })
+    @CsvSource({"savepoint, SAVEPOINT", "cancel, CANCEL"})
     void get(String value, ActionType actionType) {
         assertThat(ActionType.get(value), equalTo(actionType));
     }
