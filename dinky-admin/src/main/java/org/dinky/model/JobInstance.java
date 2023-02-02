a2ebfab7da27b47874ed36a702b493dced983bfa
zhu-mingye <934230207@qq.com>
2023-02-02 18:29:32 +0800

    format code

--- dinky-admin/src/main/java/org/dinky/model/JobInstance.java
+++ dinky-admin/src/main/java/org/dinky/model/JobInstance.java
@@ -22,6 +22,11 @@ package org.dinky.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
