bd777ad946ca0f791a5b29f55ac29ad6913589cc
ZackYoung <zackyoungh@163.com>
2023-02-06 16:52:58 +0800

    refactor flink ClusterConfiguration code

---
+++ dinky-common/src/main/java/org/dinky/model/FlinkClusterConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.dinky.model;
+
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author ZackYoung
+ * @since
+ */
+@Getter
+@Setter
+public class FlinkClusterConfiguration {
+    Type type;
+    String hadoopConfigPath;
+    String flinkConfigPath;
+    String flinkLibPath;
+    String userJarPath;
+
+    Map<String, String> flinkConfig;
+    Map<String, Object> kubernetesConfig;
+
+    public static enum Type {
+        //
+        Yarn,
+        Kubernetes;
+    }
+}
