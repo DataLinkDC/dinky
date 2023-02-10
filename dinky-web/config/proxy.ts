79bef4a398457055304505bc95ba758da92ba966
wenqiang.zhang <13701701451@163.com>
2023-02-10 14:45:58 +0800

    update dev api config

--- dinky-web/config/proxy.ts
+++ dinky-web/config/proxy.ts
@@ -29,14 +29,10 @@
 export default {
   // 如果需要自定义本地开发服务器  请取消注释按需调整
   dev: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-    '/api/': {
-      // 要代理的地址
-      target: 'http://www.dlink.top:8888/',
-      // target: 'https://preview.pro.ant.design',
-      // 配置了这个可以从 http 代理到 https
-      // 依赖 origin 的功能可能需要这个，比如 cookie
+    '/api': {
+      target: 'http://127.0.0.1:8888',
       changeOrigin: true,
+      pathRewrite: { '^': '' },
     },
   },
 
