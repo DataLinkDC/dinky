9f85e0d18b6fde031b634880fa29849aeabec3d1
Licho <lecho.sun@gmail.com>
2023-03-17 11:39:13 +0800

    fix: rename
    Signed-off-by: Licho <lecho.sun@gmail.com>

---
+++ dinky-connectors/flink-connector-printnet/src/main/java/org/apache/flink/connector/printnet/table/sink/PrintNetDynamicTableSinkFactory.java
@@ -0,0 +1,76 @@
+package org.apache.flink.connector.printnet.table.sink;
+
+import org.apache.commons.compress.utils.Sets;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.connector.format.EncodingFormat;
+import org.apache.flink.table.connector.sink.DynamicTableSink;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.factories.DynamicTableSinkFactory;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.factories.SerializationFormatFactory;
+
+import java.util.Set;
+
+import static org.apache.flink.configuration.ConfigOptions.key;
+
+public class PrintNetDynamicTableSinkFactory implements DynamicTableSinkFactory {
+    public static final String IDENTIFIER = "print_net";
+
+    public static final ConfigOption<String> HOSTNAME =
+            ConfigOptions.key("hostName").stringType().defaultValue("127.0.0.1");
+
+    public static final ConfigOption<Integer> PORT =
+            ConfigOptions.key("port").intType().noDefaultValue();
+
+    public static final ConfigOption<String> PRINT_IDENTIFIER =
+            key("print-identifier")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "Message that identify print and is prefixed to the output of the value.");
+
+    @Override
+    public DynamicTableSink createDynamicTableSink(Context context) {
+        final FactoryUtil.TableFactoryHelper helper =
+                FactoryUtil.createTableFactoryHelper(this, context);
+
+        helper.validate();
+
+        final ReadableConfig options = helper.getOptions();
+
+        EncodingFormat<SerializationSchema<RowData>> serializingFormat = null;
+
+        try {
+            // TODO: 2023/3/17 maybe not right
+            serializingFormat = helper.discoverEncodingFormat(SerializationFormatFactory.class, FactoryUtil.FORMAT);
+        } catch (Exception ignored) {}
+
+        return new PrintNetDynamicTableSink(
+                context.getCatalogTable().getResolvedSchema().toPhysicalRowDataType(),
+                context.getCatalogTable().getPartitionKeys(),
+                serializingFormat,
+                options.get(HOSTNAME),
+                options.get(PORT),
+                options.get(FactoryUtil.SINK_PARALLELISM),
+                options.get(PRINT_IDENTIFIER)
+        );
+    }
+
+    @Override
+    public String factoryIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        return Sets.newHashSet(HOSTNAME, PORT);
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        return Sets.newHashSet(PRINT_IDENTIFIER, FactoryUtil.SINK_PARALLELISM, FactoryUtil.FORMAT);
+    }
+}
---
+++ dinky-connectors/flink-connector-printnet/src/main/java/org/apache/flink/connector/printnet/table/sink/PrintNetSinkFunction.java
@@ -0,0 +1,67 @@
+package org.apache.flink.connector.printnet.table.sink;
+
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.InputTypeConfigurable;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.operators.StreamingRuntimeContext;
+import org.apache.flink.table.connector.sink.DynamicTableSink;
+import org.apache.flink.table.data.RowData;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class PrintNetSinkFunction extends RichSinkFunction<RowData> {
+    private final String hostname;
+    private final int port;
+    private final SerializationSchema<RowData> serializer;
+    private DynamicTableSink.DataStructureConverter converter;
+    private volatile boolean running = true;
+    private DatagramSocket socket;
+    private final InetAddress target;
+
+
+    public PrintNetSinkFunction(String hostname, int port, SerializationSchema<RowData> serializer,
+                                DynamicTableSink.DataStructureConverter converter) {
+        this.hostname = hostname;
+        this.port = port;
+        this.serializer = serializer;
+        this.converter = converter;
+        try {
+            this.target = InetAddress.getByName(hostname);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        if (serializer != null) {
+            serializer.open(null);
+        }
+
+        StreamingRuntimeContext context = (StreamingRuntimeContext) getRuntimeContext();
+        socket = new DatagramSocket();
+
+    }
+
+    @Override
+    public void invoke(RowData value, Context context) throws IOException {
+        try {
+            byte[] buf = serializer != null ?
+                    serializer.serialize(value)
+                    : converter.toExternal(value).toString().getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, target, port);
+            socket.send(packet);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
