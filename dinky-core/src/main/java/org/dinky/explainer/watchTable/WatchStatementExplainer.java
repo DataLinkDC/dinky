d99effca77b13260203ab931b5bd9826be8442bb
Licho <lecho.sun@gmail.com>
2023-03-29 22:18:20 +0800

    fix watch sql key not support local webui statementSet (#1802)
    * fix: watch statement not support local webUI statementSet model
    
    * Spotless Apply
    
    ---------
    
    Co-authored-by: leechor <leechor@users.noreply.github.com>

--- dinky-core/src/main/java/org/dinky/explainer/watchTable/WatchStatementExplainer.java
+++ dinky-core/src/main/java/org/dinky/explainer/watchTable/WatchStatementExplainer.java
@@ -17,13 +17,7 @@
  *
  */
 
-package org.dinky.trans.ddl;
-
-import org.dinky.executor.Executor;
-import org.dinky.trans.AbstractOperation;
-import org.dinky.trans.Operation;
-
-import org.apache.flink.table.api.TableResult;
+package org.dinky.explainer.watchTable;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -32,9 +26,7 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class WatchTableOperation extends AbstractOperation implements Operation {
-
-    private static final String KEY_WORD = "WATCH";
+public class WatchStatementExplainer {
 
     public static final String PATTERN_STR = "WATCH (.+)";
     public static final Pattern PATTERN = Pattern.compile(PATTERN_STR, Pattern.CASE_INSENSITIVE);
@@ -46,39 +38,29 @@ public class WatchTableOperation extends AbstractOperation implements Operation 
     public static final String INSERT_SQL_TEMPLATE = "insert into print_{0} select * from {0};";
     public static final int PORT = 7125;
 
-    public WatchTableOperation() {}
+    private final String statement;
 
-    public WatchTableOperation(String statement) {
-        super(statement);
+    public WatchStatementExplainer(String statement) {
+        this.statement = statement;
     }
 
-    @Override
-    public String getHandle() {
-        return KEY_WORD;
-    }
-
-    @Override
-    public Operation create(String statement) {
-        return new WatchTableOperation(statement);
-    }
-
-    @Override
-    public TableResult build(Executor executor) {
+    public String getTableName() {
         Matcher matcher = PATTERN.matcher(statement);
         matcher.find();
-        String tableName = matcher.group(1);
-
-        Optional<InetAddress> address = getSystemLocalIp();
-        String ip = address.isPresent() ? address.get().getHostAddress() : "127.0.0.1";
-
-        String printCreateSql = MessageFormat.format(CREATE_SQL_TEMPLATE, tableName, ip, PORT, 1);
-        executor.getCustomTableEnvironment().executeSql(printCreateSql);
-
-        String printInsertSql = MessageFormat.format(INSERT_SQL_TEMPLATE, tableName);
-        return executor.getCustomTableEnvironment().executeSql(printInsertSql);
+        return matcher.group(1);
     }
 
-    private Optional<InetAddress> getSystemLocalIp() {
+    public String getCreateStatement(String tableName) {
+        Optional<InetAddress> address = getSystemLocalIp();
+        String ip = address.isPresent() ? address.get().getHostAddress() : "127.0.0.1";
+        return MessageFormat.format(CREATE_SQL_TEMPLATE, tableName, ip, PORT, 1);
+    }
+
+    public String getInsertStatement(String tableName) {
+        return MessageFormat.format(INSERT_SQL_TEMPLATE, tableName);
+    }
+
+    private static Optional<InetAddress> getSystemLocalIp() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
             return Optional.of(ip);
