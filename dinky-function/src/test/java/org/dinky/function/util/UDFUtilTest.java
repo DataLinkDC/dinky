5e4e28f7ffeabc3f6cd2e5caf9d2b7f080219b90
leechor <leechor@users.noreply.github.com>
2023-04-19 09:07:34 +0000

    Spotless Apply

--- dinky-function/src/test/java/org/dinky/function/util/UDFUtilTest.java
+++ dinky-function/src/test/java/org/dinky/function/util/UDFUtilTest.java
@@ -1,12 +1,31 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.dinky.function.util;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 class UDFUtilTest {
 
@@ -20,11 +39,19 @@ class UDFUtilTest {
         assertTrue(c.apply("create  function  a  as  'abc'  language python"));
         assertTrue(c.apply("create  function  a  as  'abc'  using jar 'path'"));
         assertTrue(c.apply("create  function  a  as  'abc'  using jar 'path', jar 'path/2'"));
-        assertTrue(c.apply("create tempOrary function  a  as  'abc'  using jar 'path', jar 'path/2'"));
-        assertTrue(c.apply("create tempOrary system function  a  as  'abc'  using jar 'path', jar 'path/2'"));
-        assertTrue(c.apply("create tempOrary system function  a  as  'abc'  using jar 'path',jar 'path/2'"));
-        assertFalse(c.apply(" create tempOrary system function  a  as  'abc'  using jar 'path', jar 'path/2'"));
-        assertFalse(c.apply("create tempOrary system function  a  as  abc  using jar 'path', jar 'path/2'"));
-
+        assertTrue(
+                c.apply("create tempOrary function  a  as  'abc'  using jar 'path', jar 'path/2'"));
+        assertTrue(
+                c.apply(
+                        "create tempOrary system function  a  as  'abc'  using jar 'path', jar 'path/2'"));
+        assertTrue(
+                c.apply(
+                        "create tempOrary system function  a  as  'abc'  using jar 'path',jar 'path/2'"));
+        assertFalse(
+                c.apply(
+                        " create tempOrary system function  a  as  'abc'  using jar 'path', jar 'path/2'"));
+        assertFalse(
+                c.apply(
+                        "create tempOrary system function  a  as  abc  using jar 'path', jar 'path/2'"));
     }
 }
