8250a3ba7d25bfeb2114b0612228bede6d074fd5
zhu-mingye <934230207@qq.com>
2023-05-15 14:23:29 +0800

    fix i18n

--- dinky-web/src/locales/en-US/pages.ts
+++ dinky-web/src/locales/en-US/pages.ts
@@ -796,8 +796,8 @@ export default {
   'sys.process.viewInfoLog':'View Info Level Log',
   'sys.process.viewErrorLog':'View Error Level Log',
   'sys.process.subStep.status':'Step Status',
-  'sys.process.subStep.info':'Step Status',
-  'sys.process.subStep.error':'Step Status',
+  'sys.process.subStep.info':'Step Info',
+  'sys.process.subStep.error':'Step Error',
   'sys.info.logList.tips': 'Click left log file to viewing',
 
   'devops.jobinfo.alert.title': 'Title',
