36b955bb02a3a29581d75df6f0542623eae0e3d9
ZackYoung <zackyoungh@163.com>
2023-06-09 17:58:16 +0800

    fix_pg & sms_alert & SystemConfiguration

--- dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLPrepareInterceptor.java
+++ dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLPrepareInterceptor.java
@@ -21,6 +21,11 @@ package org.dinky.interceptor;
 
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
--- dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLQueryInterceptor.java
+++ dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLQueryInterceptor.java
@@ -23,6 +23,11 @@ import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
