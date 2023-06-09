7e907f2f05539b9c9dd2c10e31dbd61d0e03e556
ZackYoung <zackyoungh@163.com>
2023-06-09 22:48:54 +0800

    fix_pg & sms_alert & SystemConfiguration

--- dinky-admin/src/main/java/org/dinky/data/annotation/ConditionalOnListProperty.java
+++ dinky-admin/src/main/java/org/dinky/data/annotation/ConditionalOnListProperty.java
@@ -19,6 +19,11 @@
 
 package org.dinky.data.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Condition;
