b0bce280ac6364a42d2a89c2c2c3ddf2b8242371
zhu-mingye <934230207@qq.com>
2023-06-10 23:22:52 +0800

    fix metrics function bug

--- dinky-web/src/pages/Metrics/Server/function.tsx
+++ dinky-web/src/pages/Metrics/Server/function.tsx
@@ -18,59 +18,49 @@
  */
 
 
-const getSubMinTime = (currentTime : Date ,minutes: number) => {
+/**
+ * 获取当前时间的前几分钟 | get current time before several minutes
+ * @param {Date} currentTime | current time
+ * @param {number} minutes | sub minutes
+ * @returns {Date} | return the time before several minutes
+ */
+export const getSubMinTime = (currentTime : Date ,minutes: number) => {
     const pastTime = new Date(currentTime.getTime() - minutes * 60000);
     return pastTime;
 };
 
-const getSubMinutesEndTime = (currentTime : Date ,minutes: number) => {
-    const pastTime = new Date(
-        currentTime.getFullYear(),
-        currentTime.getMonth(),
-        currentTime.getDate(),
-        currentTime.getHours(),
-        59,
-        59,59,
-    );
-    return pastTime;
-};
 
-const getSubDateTime = (currentTime : Date ,day: number) => {
+/**
+ * 获取当前时间的前几天 | get current time before several days
+ * @param {Date} currentTime | current time
+ * @param {number} day sub days
+ * @param {boolean} isStart | is start time , if true , the time is 00:00:00.000    if false , the time is 23:59:59.999
+ * @returns {Date} | return the time before several days
+ */
+export const getSubDateTime = (currentTime : Date , day: number,isStart: boolean) => {
 
     const pastTime = new Date(
         currentTime.getFullYear(),
         currentTime.getMonth(),
         currentTime.getDate() - day,
-        0, 0, 0, 0
-    );
-    return pastTime;
-};
-const getSubDateEndTime = (currentTime : Date ,day: number) => {
-    const pastTime = new Date(
-        currentTime.getFullYear(),
-        currentTime.getMonth(),
-        currentTime.getDate() - day,
-        23, 59, 59, 59
+        (isStart? 0: 23), (isStart? 0: 59), (isStart? 0: 59), (isStart? 0: 999)
     );
     return pastTime;
 };
 
-const getSubMon = (currentTime : Date ,month: number) => {
-    const pastTime = new Date(
-        currentTime.getFullYear(),
-        currentTime.getMonth() - month,
-        1,
-        0, 0, 0, 0
-    );
-    return pastTime;
-};
 
-const getSubMonEnd = (currentTime : Date ,month: number) => {
+/**
+ * 获取当前月的第一天/最后一天 | get the first day / last day of current month
+ * @param {Date} currentTime | current time
+ * @param {boolean} isFirst | is first day of month, if true , the time is 00:00:00.000    if false , the time is 23:59:59.999
+ * @returns {Date} | return the first day / last day of current month
+ */
+export const getDayOfMonth = (currentTime : Date , isFirst: boolean) => {
     const pastTime = new Date(
         currentTime.getFullYear(),
-        currentTime.getMonth() - month,
-        currentTime.getDay(),
-        23, 59, 59, 59
+        currentTime.getMonth() + (isFirst ?  0 : 1),
+        (isFirst? 1: 0),
+        (isFirst? 0: 23), (isFirst? 0: 59), (isFirst? 0: 59), (isFirst? 0: 999)
     );
     return pastTime;
-};
+}
