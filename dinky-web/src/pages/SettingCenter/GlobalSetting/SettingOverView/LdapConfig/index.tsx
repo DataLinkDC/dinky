be97177d24890416b498b2b5f72d741288ec6e93
ikiler <ikiler@126.com>
2023-06-12 23:15:14 +0800

    Perfect ldap test login and test connection function with front end pages

--- dinky-web/src/pages/SettingCenter/GlobalSetting/SettingOverView/LdapConfig/index.tsx
+++ dinky-web/src/pages/SettingCenter/GlobalSetting/SettingOverView/LdapConfig/index.tsx
@@ -17,11 +17,19 @@
 
 
 import {BaseConfigProperties} from '@/types/SettingCenter/data';
-import {ProCard} from '@ant-design/pro-components';
+import {ModalForm, ProCard, ProFormText} from '@ant-design/pro-components';
 import GeneralConfig from '@/pages/SettingCenter/GlobalSetting/SettingOverView/GeneralConfig';
 import {l} from '@/utils/intl';
-import {Button, Space, Tag} from 'antd';
+import {Space, Tag} from 'antd';
 import React from 'react';
+import {
+  ApiFilled,
+  LoginOutlined,
+  UserSwitchOutlined,
+} from "@ant-design/icons";
+import {handleOption, queryDataByParams} from "@/services/BusinessCrud";
+import {API_CONSTANTS} from "@/services/constants";
+import {SuccessMessage} from "@/utils/messages";
 
 interface LdapConfigProps {
   data: BaseConfigProperties[];
@@ -32,6 +40,22 @@ export const LdapConfig = ({data, onSave}: LdapConfigProps) => {
 
   const [loading, setLoading] = React.useState(false);
 
+  const testConnection = async () => {
+    setLoading(true);
+    const datas = await queryDataByParams(API_CONSTANTS.LDAP_TEST);
+    if (datas) {
+      SuccessMessage(l("sys.ldap.settings.testConnect.success","",{count:datas}))
+    }
+    setLoading(false);
+  }
+
+  const testLogin = async (value: any) => {
+    setLoading(true);
+    await handleOption(API_CONSTANTS.LDAP_TEST_LOGIN, l("sys.ldap.settings.testLogin"), value);
+    setLoading(false);
+  }
+
+
   const onSaveHandler = (data: BaseConfigProperties) => {
     setLoading(true);
     onSave(data);
@@ -39,6 +63,39 @@ export const LdapConfig = ({data, onSave}: LdapConfigProps) => {
       setLoading(false);
     }, 1000);
   };
+
+  /**
+   * render ldap test login form
+   */
+  const renderTestLoginForm = () => {
+    return (
+      <ModalForm
+        width={400} onFinish={testLogin}
+        trigger={<Tag icon={<LoginOutlined/>} color="#108ee9">{l("sys.ldap.settings.testLogin")}</Tag>}
+      >
+        <ProFormText name="username" label={l("login.username.placeholder")}/>
+        <ProFormText name="password" label={l("login.password.placeholder")}/>
+      </ModalForm>
+    )
+  }
+
+  /**
+   * render ldap test case toolbar
+   */
+  const renderToolBar = () => {
+    return [
+      <Space key={"ldapToolBar"}>
+        <Tag icon={<ApiFilled/>} color="#87d068" onClick={() => testConnection()}>
+          {l("sys.ldap.settings.testConnect")}
+        </Tag>
+        {renderTestLoginForm()}
+        <Tag icon={<UserSwitchOutlined/>} color="#f50" onClick={() => {}}>
+          {l("sys.ldap.settings.loadUser")}
+        </Tag>
+      </Space>
+    ];
+  };
+
   return <>
     <ProCard
       title={l('sys.setting.ldap')}
@@ -52,6 +109,7 @@ export const LdapConfig = ({data, onSave}: LdapConfigProps) => {
         onSave={onSaveHandler}
         tag={<><Tag color={'default'}>{l('sys.setting.tag.integration')}</Tag></>}
         data={data}
+        toolBarRender={renderToolBar}
       />
     </ProCard>
   </>;
