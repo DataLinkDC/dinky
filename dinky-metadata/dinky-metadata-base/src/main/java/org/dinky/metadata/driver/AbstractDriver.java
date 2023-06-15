80960b8868dc1e883acb0a68a2f751a71a3454b1
Licho <lecho.sun@gmail.com>
2023-06-15 09:02:49 +0800

    fix: \r\n problem
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- dinky-metadata/dinky-metadata-base/src/main/java/org/dinky/metadata/driver/AbstractDriver.java
+++ dinky-metadata/dinky-metadata-base/src/main/java/org/dinky/metadata/driver/AbstractDriver.java
@@ -106,19 +106,19 @@ public abstract class AbstractDriver implements Driver {
                     columnComment = columnComment.replaceAll("[\"']", "");
                 }
                 sb.append(
-                        String.format("`%s`  --  %s %n", columns.get(i).getName(), columnComment));
+                        String.format("`%s`  --  %s \n", columns.get(i).getName(), columnComment));
             } else {
-                sb.append(String.format("`%s` %%n", columns.get(i).getName()));
+                sb.append(String.format("`%s` %\n", columns.get(i).getName()));
             }
         }
 
         if (Asserts.isNotNullString(table.getComment())) {
             sb.append(
                     String.format(
-                            " FROM %s.%s; -- %s%n",
+                            " FROM %s.%s; -- %s\n",
                             table.getSchema(), table.getName(), table.getComment()));
         } else {
-            sb.append(String.format(" FROM %s.%s;%n", table.getSchema(), table.getName()));
+            sb.append(String.format(" FROM %s.%s;\n", table.getSchema(), table.getName()));
         }
         return sb.toString();
     }
