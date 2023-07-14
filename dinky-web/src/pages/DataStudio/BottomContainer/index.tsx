ca911634cbb2290fb05250ba27f9bc7a372aa888
zhu-mingye <934230207@qq.com>
2023-07-15 00:04:10 +0800

    modify CODE

--- dinky-web/src/pages/DataStudio/BottomContainer/index.tsx
+++ dinky-web/src/pages/DataStudio/BottomContainer/index.tsx
@@ -2,111 +2,106 @@ import React from "react";
 import MovableSidebar from "@/components/Sidebar/MovableSidebar";
 import {l} from "@/utils/intl";
 import {StateType, VIEW} from "@/pages/DataStudio/model";
-import {ConfigProvider, Space, Tabs} from "antd";
+import {Space, Tabs} from "antd";
 import {LeftBottomSide} from "@/pages/DataStudio/route";
 import {connect} from "@@/exports";
 import {Title} from "@/components/StyledComponents";
 
 export type BottomContainerProps = {
-  size:number
+    size: number
 }
-const BottomContainer:React.FC<BottomContainerProps> = (props:any) => {
-  const {dispatch,size} = props;
+const BottomContainer: React.FC<BottomContainerProps> = (props: any) => {
+    const {dispatch, size,bottomContainer} = props;
 
 
-  const {bottomContainer} = props
+    /**
+     * 侧边栏最小化
+     */
+    const handleMinimize = () => {
+        dispatch({
+            type: 'Studio/updateSelectBottomKey',
+            payload: "",
+        })
+    }
+
+    /**
+     * 更新底部高度
+     * @param {number} height
+     */
+    const updateBottomHeight = (height: number) => {
+        dispatch({
+            type: "Studio/updateBottomHeight",
+            payload: height,
+        })
+    }
+
+    /**
+     * 更新中间内容高度
+     * @param {number} height
+     */
+    const updateCenterContentHeight = (height: number) => {
+        dispatch({
+            type: "Studio/updateCenterContentHeight",
+            payload: height,
+        })
+    }
+
+    /**
+     * 更新工具栏内容高度
+     * @param {number} height
+     */
+    const updateToolContentHeight = (height: number) => {
+        dispatch({
+            type: "Studio/updateToolContentHeight",
+            payload: height,
+        })
+    }
 
 
-  /**
-   * 侧边栏最小化
-   */
-  const handleMinimize = () => {
-    dispatch({
-      type: 'Studio/updateSelectBottomKey',
-      payload: "",
-    })
-  }
-  const updateBottomHeight= (height: number) =>{
-    dispatch({
-    type: "Studio/updateBottomHeight",
-    payload: height,
-  })
-  }
-  const updateCenterContentHeight= (height: number) =>{
-    dispatch({
-    type: "Studio/updateCenterContentHeight",
-    payload: height,
-  })
-  }
-  const updateToolContentHeight= (height: number) =>{
-    dispatch({
-    type: "Studio/updateToolContentHeight",
-    payload: height,
-  })
-  }
-
-
-
-  return(
-    <MovableSidebar
-      title={
-      <ConfigProvider
-        theme={{
-          components:{
-            Tabs:{
-              horizontalMargin:"0"
+    return (
+        <MovableSidebar
+            title={
+                <Space align={'baseline'} size={'small'}>
+                    <Title>{(bottomContainer.selectKey === '' ? "" : l(bottomContainer.selectKey))}</Title>
+                    <Tabs
+                        type="editable-card" hideAdd
+                        defaultActiveKey="StudioMsg" size="small" tabPosition="top"
+                        items={[
+                            {
+                                key: "test",
+                                label: "123456"
+                            }, {
+                                key: "tes2",
+                                label: "123"
+                            }
+                        ]}
+                    />
+                </Space>
             }
-          }
-        }}
-      >
-
-      <Space>
-     <Title >{(bottomContainer.selectKey===''?"":l(bottomContainer.selectKey))}</Title>
-      <Tabs
-        style={{height:"43px",display:"-webkit-box"}}
-        items={[{
-          key: "test",
-          label: "123"
-        },{
-          key: "tes2",
-          label: "123"
-        }]}
-        type="editable-card"
-        hideAdd
-        defaultActiveKey="StudioMsg" size="small" tabPosition="top"  />
-      </Space>
-      </ConfigProvider>
-      }
-      visible={props.bottomContainer.selectKey !== ""}
-      style={{
-        border: "solid 1px #ddd",
-        background: "#f0f0f0",
-        zIndex: 999
-      }}
-      defaultSize={{
-        width: "100%",
-        height: props.bottomContainer.height
-      }}
-      minHeight={VIEW.midMargin+10}
-      maxHeight={size.contentHeight - 40}
-      onResize={(event: any, direction: any, elementRef: { offsetHeight: any; }) => {
-        updateBottomHeight(elementRef.offsetHeight)
-        const centerContentHeight = document.documentElement.clientHeight - VIEW.headerHeight - VIEW.headerNavHeight - VIEW.footerHeight - VIEW.otherHeight - props.bottomContainer.height;
-        updateCenterContentHeight(centerContentHeight)
-        updateToolContentHeight(centerContentHeight-VIEW.midMargin)
-      }}
-      enable={{top: true}}
-      handlerMinimize={handleMinimize}
-    >
-      <Tabs activeKey={bottomContainer.selectKey} items={LeftBottomSide} tabBarStyle={{display: "none"}}/>
-
-
-    </MovableSidebar>
-  )
+            visible={bottomContainer.selectKey !== ""}
+            style={{zIndex: 999}}
+            defaultSize={{
+                width: "100%",
+                height: bottomContainer.height
+            }}
+            minHeight={VIEW.midMargin + 10}
+            maxHeight={size.contentHeight - 40}
+            onResize={(event: any, direction: any, elementRef: { offsetHeight: any; }) => {
+                updateBottomHeight(elementRef.offsetHeight)
+                const centerContentHeight = document.documentElement.clientHeight - VIEW.headerHeight - VIEW.headerNavHeight - VIEW.footerHeight - VIEW.otherHeight - bottomContainer.height;
+                updateCenterContentHeight(centerContentHeight)
+                updateToolContentHeight(centerContentHeight - VIEW.midMargin)
+            }}
+            enable={{top: true}}
+            handlerMinimize={handleMinimize}
+        >
+            <Tabs activeKey={bottomContainer.selectKey} items={LeftBottomSide} tabBarStyle={{display: "none"}}/>
+        </MovableSidebar>
+    )
 }
 
 export default connect(({Studio}: { Studio: StateType }) => ({
-  leftContainer: Studio.leftContainer,
-  bottomContainer: Studio.bottomContainer,
-  centerContentHeight: Studio.centerContentHeight,
+    leftContainer: Studio.leftContainer,
+    bottomContainer: Studio.bottomContainer,
+    centerContentHeight: Studio.centerContentHeight,
 }))(BottomContainer);
