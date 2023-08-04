7dde1481008579b28f59398395d152ca4c3b6f19
zhu-mingye <zhu-mingye@users.noreply.github.com>
2023-08-04 08:54:03 +0000

    Spotless Apply

--- dinky-admin/src/main/java/org/dinky/controller/MenuController.java
+++ dinky-admin/src/main/java/org/dinky/controller/MenuController.java
@@ -33,7 +33,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
