e36fce93f19effd23bf58276e4f703711e31334a
zhu-mingye <934230207@qq.com>
2023-08-18 14:05:11 +0800

    fix sql bug (#2239)

--- script/sql/dinky-pg.sql
+++ script/sql/dinky-pg.sql
@@ -2210,3 +2210,38 @@ COMMENT on column public.dinky_sys_menu.note is 'note';
 -- Primary Key structure for table dinky_sys_menu
 -- ----------------------------
 ALTER TABLE "public"."dinky_sys_menu" ADD CONSTRAINT "dinky_sys_menu_pkey" PRIMARY KEY ("id");
+
+
+INSERT INTO "public"."dinky_sys_menu" VALUES (1, -1, '首页', '/home', './Home', null, 'HomeOutlined', 'C ', 0, 100000, '2023-08-11 14:06:52.000000', '2023-08-17 15:09:52.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (2, -1, '运维中心', '/devops', null, null, 'ControlOutlined', 'M ', 0, 300000, '2023-08-11 14:06:52.000000', '2023-08-17 15:10:10.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (3, -1, '注册中心', '/registration', null, null, 'AppstoreOutlined', 'M ', 0, 400000, '2023-08-11 14:06:52.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (4, -1, '认证中心', '/auth', null, null, 'SafetyCertificateOutlined', 'M ', 0, 500000, '2023-08-11 14:06:52.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (5, -1, '数据开发', '/datastudio', './DataStudio', null, 'CodeOutlined', 'C ', 0, 200000, '2023-08-11 14:06:52.000000', '2023-08-17 15:10:00.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (6, -1, '配置中心', '/settings', null, null, 'SettingOutlined', 'M ', 0, 600000, '2023-08-11 14:06:53.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (7, -1, '关于', '/about', './Other/About', null, 'SmileOutlined', 'C ', 0, 800000, '2023-08-11 14:06:53.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (8, -1, '监控', '/metrics', './Metrics', null, 'DashboardOutlined', 'C ', 0, 700000, '2023-08-11 14:06:53.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (9, 3, 'cluster', '/registration/cluster', null, null, 'GoldOutlined', 'M ', 0, 410000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (10, 3, 'database', '/registration/database', './RegCenter/DataSource', null, 'DatabaseOutlined', 'M ', 0, 420000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (11, -1, 'center', '/account/center', './Other/PersonCenter', null, 'UserOutlined', 'C ', 0, 900000, '2023-08-11 14:06:54.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (12, 3, 'alert', '/registration/alert', null, null, 'AlertOutlined', 'M ', 0, 430000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (13, 3, 'document', '/registration/document', './RegCenter/Document', null, 'BookOutlined', 'C ', 0, 440000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (14, 3, 'fragment', '/registration/fragment', './RegCenter/GlobalVar', null, 'RocketOutlined', 'C ', 0, 450000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (15, 3, 'gitprojects', '/registration/gitprojects', './RegCenter/GitProject', null, 'GithubOutlined', 'C ', 0, 460000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (16, 3, 'udf', '/registration/udf', './RegCenter/UDF', null, 'ToolOutlined', 'C ', 0, 470000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (17, 2, 'job-detail', '/devops/job-detail', './DevOps/JobDetail', null, null, 'C ', 0, 320000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (18, 2, 'job', '/devops/joblist', './DevOps', null, null, 'C ', 0, 310000, '2023-08-11 14:06:54.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (19, 3, 'resource', '/registration/resource', './RegCenter/Resource', null, 'FileZipOutlined', 'C ', 0, 480000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (20, 4, 'role', '/auth/role', './AuthCenter/Role', null, 'TeamOutlined', 'C ', 0, 520000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (21, 4, 'user', '/auth/user', './AuthCenter/User', null, 'UserOutlined', 'C ', 0, 510000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (22, 4, '菜单', '/auth/menu', './AuthCenter/Menu', null, 'MenuOutlined', 'C ', 0, 530000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (23, 4, 'tenant', '/auth/tenant', './AuthCenter/Tenant', null, 'SecurityScanOutlined', 'C ', 0, 540000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (24, 6, 'globalsetting', '/settings/globalsetting', './SettingCenter/GlobalSetting', null, 'SettingOutlined', 'C ', 0, 610000, '2023-08-11 14:06:54.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (25, 6, 'systemlog', '/settings/systemlog', './SettingCenter/SystemLogs', null, 'InfoCircleOutlined', 'C ', 0, 620000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (26, 6, 'process', '/settings/process', './SettingCenter/Process', null, 'ReconciliationOutlined', 'C ', 0, 630000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (27, 4, 'rowpermissions', '/auth/rowpermissions', './AuthCenter/RowPermissions', null, 'SafetyCertificateOutlined', 'C ', 0, 550000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (28, 9, 'cluster-instance', '/registration/cluster/instance', './RegCenter/Cluster/Instance', null, null, 'C ', 0, 411000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (29, 12, 'group', '/registration/alert/group', './RegCenter/Alert/AlertGroup', null, null, 'C ', 0, 432000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (30, 9, 'cluster-config', '/registration/cluster/config', './RegCenter/Cluster/Configuration', null, null, 'C ', 0, 412000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:51.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (31, 12, 'instance', '/registration/alert/instance', './RegCenter/Alert/AlertInstance', null, null, 'C ', 0, 431000, '2023-08-11 14:06:55.000000', '2023-08-17 15:12:50.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (32, 1, '作业监控', '/home/jobOverView', 'Row', 'show', 'AntCloudOutlined', 'F ', 0, 110000, '2023-08-15 16:52:59.000000', '2023-08-17 15:11:40.000000');
+INSERT INTO "public"."dinky_sys_menu" VALUES (33, 1, '数据开发', '/home/devOverView', 'DevOverView', 'show', 'AimOutlined', 'F ', 0, 120000, '2023-08-15 16:54:47.000000', '2023-08-17 15:11:40.000000');
