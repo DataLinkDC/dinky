848c27b4d898a84e63c3b238daefe0d81bf4e75e
Licho <lecho.sun@gmail.com>
2023-08-21 13:20:02 +0800

    fix: app.tsx import constant (#2252)
    * fix: app.tsx import constant
    
    Signed-off-by: Licho <lecho.sun@gmail.com>
    
    * refactor: remove comment-on-issue
    
    Signed-off-by: Licho <lecho.sun@gmail.com>
    
    * remove submodule
    
    ---------
    
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- dinky-web/src/app.tsx
+++ dinky-web/src/app.tsx
@@ -34,6 +34,7 @@ import { default as defaultSettings, default as Settings } from '../config/defau
 import { errorConfig } from './requestErrorConfig';
 import { getDataByParamsReturnResult } from './services/BusinessCrud';
 import { API } from './services/data';
+import {API_CONSTANTS} from "@/services/endpoints";
 
 // const isDev = process.env.NODE_ENV === "development";
 const loginPath = API_CONSTANTS.LOGIN_PATH;
