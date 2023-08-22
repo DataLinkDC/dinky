a6d5bdcdeaceac48bdba26490accbe7fad58b261
Licho <lecho.sun@gmail.com>
2023-08-22 19:39:29 +0800

    refactor:

--- dinky-admin/src/main/java/org/dinky/controller/WatchTableController.java
+++ dinky-admin/src/main/java/org/dinky/controller/WatchTableController.java
@@ -23,6 +23,7 @@ import org.dinky.data.result.Result;
 import org.dinky.service.WatchTableService;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import io.swagger.annotations.Api;
