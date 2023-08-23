23abd5b5ed6258d592178f3584036470d6f49753
Licho <lecho.sun@gmail.com>
2023-08-23 11:31:17 +0800

    fix: task data update problem

--- dinky-web/src/pages/DataStudio/index.tsx
+++ dinky-web/src/pages/DataStudio/index.tsx
@@ -170,11 +170,18 @@ const DataStudio = (props: any) => {
     const params = currentTab.params;
     const res = await getTaskDetails(params.taskId);
 
-    const changed = Object.keys(res).some(
-      (key) =>
-        res[key] !== params.taskData[key] ||
-        JSON.stringify(res[key]) !== JSON.stringify(params.taskData[key])
-    );
+    const changed = Object.keys(params.taskData).some(
+        (key) => {
+          // ignore this property
+          if (['updateTime', 'createTime', 'jobInstanceId'].includes(key)) {
+            return false;
+          }
+
+          if(JSON.stringify(res[key]) !== JSON.stringify(params.taskData[key])){
+            console.log('key', key, res[key], params.taskData[key])
+            return true;
+          }
+        });
 
     if (changed) {
       setIsModalUpdateTabContentOpen(true);
