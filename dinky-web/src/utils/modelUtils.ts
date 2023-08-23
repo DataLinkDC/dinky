cc6ef8ccde00ee1a5ffcc162887b2cb4b3682389
zhu-mingye <zhu-mingye@users.noreply.github.com>
2023-08-23 14:52:37 +0000

    Spotless Apply

--- dinky-web/src/utils/modelUtils.ts
+++ dinky-web/src/utils/modelUtils.ts
@@ -1,27 +1,24 @@
-
-
-
 interface BaseModelType {
-    ['reducers']: {};
-    ['effects']: {};
-    ['namespace']: string;
+  ['reducers']: {};
+  ['effects']: {};
+  ['namespace']: string;
 }
 type MemberType<T extends BaseModelType, U> = U extends 'effects' ? T['effects'] : T['reducers'];
 
 function getModelTypes<T extends BaseModelType>(
-    target: T,
-    type: 'effects' | 'reducers'
+  target: T,
+  type: 'effects' | 'reducers'
 ): { [K in keyof MemberType<T, typeof type>]: string } {
-    const reducers = Object.keys(target[type]).map((obj: string) => [
-        obj,
-        `${target.namespace}/${obj}`
-    ]);
-    // @ts-ignore
-    return Object.fromEntries(new Map<keyof (typeof target)[type], string>(reducers));
+  const reducers = Object.keys(target[type]).map((obj: string) => [
+    obj,
+    `${target.namespace}/${obj}`
+  ]);
+  // @ts-ignore
+  return Object.fromEntries(new Map<keyof (typeof target)[type], string>(reducers));
 }
 
 export const createModelTypes = <T extends BaseModelType>(
-    target: T
+  target: T
 ): [{ [K in keyof T['reducers']]: string }, { [K in keyof T['effects']]: string }] => {
-    return [getModelTypes(target, 'reducers'), getModelTypes(target, 'effects')];
+  return [getModelTypes(target, 'reducers'), getModelTypes(target, 'effects')];
 };
