ae696053e7b37f7d9dd23cd8353e89407cbcdb85
Licho <lecho.sun@gmail.com>
2023-08-23 13:54:06 +0800

    refactor: refactorModelUtil
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- dinky-web/src/utils/modals.tsx
+++ dinky-web/src/utils/modals.tsx
@@ -47,27 +47,3 @@ export const ErrorModelWithCode = (
   );
 };
 
-interface BaseModeType {
-  ['reducers']: {};
-  ['effects']: {};
-  ['namespace']: string;
-}
-type MemberType<T extends BaseModeType, U> = U extends 'effects' ? T['effects'] : T['reducers'];
-
-function getModelTypes<T extends BaseModeType>(
-  target: T,
-  type: 'effects' | 'reducers'
-): { [K in keyof MemberType<T, typeof type>]: string } {
-  const reducers = Object.keys(target[type]).map((obj: string) => [
-    obj,
-    `${target.namespace}/${obj}`
-  ]);
-  // @ts-ignore
-  return Object.fromEntries(new Map<keyof (typeof target)[type], string>(reducers));
-}
-
-export const createModelTypes = <T extends BaseModeType>(
-  target: T
-): [{ [K in keyof T['reducers']]: string }, { [K in keyof T['effects']]: string }] => {
-  return [getModelTypes(target, 'reducers'), getModelTypes(target, 'effects')];
-};
