1ffce3d1d432b82c528bf9ca0164779cab7d2ccb
leechor <leechor@users.noreply.github.com>
2023-08-27 11:03:34 +0000

    Spotless Apply

--- dinky-common/src/main/java/org/dinky/utils/I18n.java
+++ dinky-common/src/main/java/org/dinky/utils/I18n.java
@@ -1,20 +1,35 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.dinky.utils;
 
 import java.text.MessageFormat;
-
 import java.util.Arrays;
-
 import java.util.Locale;
-
 import java.util.ResourceBundle;
 
 public final class I18n {
 
-    private final static String MESSAGES_BASE = "message";
+    private static final String MESSAGES_BASE = "message";
     private static ResourceBundle bundle;
 
-    private I18n() {
-    }
+    private I18n() {}
 
     public static Locale getLocale() {
         Locale defaultLocale = Locale.getDefault();
@@ -31,13 +46,13 @@ public final class I18n {
     }
 
     public static String getMessage(String key) {
-        if(bundle == null) {
+        if (bundle == null) {
             bundle = ResourceBundle.getBundle(MESSAGES_BASE);
         }
         return bundle.getString(key);
     }
 
-    public static String getMessage(String key, Object ... arguments) {
+    public static String getMessage(String key, Object... arguments) {
         return MessageFormat.format(getMessage(key), arguments);
     }
 }
