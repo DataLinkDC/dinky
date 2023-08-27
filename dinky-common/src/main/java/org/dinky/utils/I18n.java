9a5f86353626630e8a2858878bd8e728c792a6c0
Licho <lecho.sun@gmail.com>
2023-08-27 20:48:12 +0800

    feat: chinese encode translate
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- dinky-common/src/main/java/org/dinky/utils/I18n.java
+++ dinky-common/src/main/java/org/dinky/utils/I18n.java
@@ -1,5 +1,11 @@
 package org.dinky.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 
 import java.util.Arrays;
@@ -9,6 +15,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public final class I18n {
+    private static final Logger logger = LoggerFactory.getLogger(I18n.class);
 
     private static final String MESSAGES_BASE = "message";
     private static ResourceBundle bundle;
@@ -33,10 +40,16 @@ public final class I18n {
         if(bundle == null) {
             bundle = ResourceBundle.getBundle(MESSAGES_BASE);
         }
-        return bundle.getString(key);
+
+        String message = bundle.getString(key);
+        if (getLocale() == Locale.SIMPLIFIED_CHINESE) {
+            message = new String(message.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        }
+
+        return message;
     }
 
-    public static String getMessage(String key, Object ... arguments) {
+    public static String getMessage(String key, Object... arguments) {
         return MessageFormat.format(getMessage(key), arguments);
     }
 }
