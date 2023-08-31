1d4fc926de3655723c48c9001686363ec15cb8ba
Licho <lecho.sun@gmail.com>
2023-08-31 16:37:41 +0800

    feat: fix encode

---
+++ dinky-common/src/main/java/org/dinky/utils/AppConstant.java
@@ -0,0 +1,11 @@
+package org.dinky.utils;
+
+public final class AppConstant {
+    public static final String JDK_VERSION = System.getProperty("java.specification.version");
+    public static final boolean JDK_ABOVE_1_8 = JDK_VERSION.compareTo("1.8") > 0;
+
+    private AppConstant() {
+    }
+
+
+}
