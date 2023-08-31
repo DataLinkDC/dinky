8a16245e2f2d5b532b6f173adc7bb298d96f496d
Licho <lecho.sun@gmail.com>
2023-08-31 12:14:04 +0800

    feat: fix encode

--- dinky-common/src/main/java/org/dinky/utils/I18n.java
+++ dinky-common/src/main/java/org/dinky/utils/I18n.java
@@ -52,7 +52,7 @@ public final class I18n {
     public static String getMessage(String key) {
         bundle = ResourceBundle.getBundle(MESSAGES_BASE);
         String message = bundle.getString(key);
-        message = new String(message.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+//        message = new String(message.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
         return message;
     }
 
