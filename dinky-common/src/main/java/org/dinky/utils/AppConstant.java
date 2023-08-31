5a6f768b6a6f8ba5fdb568312355e792bf9defe5
Licho <lecho.sun@gmail.com>
2023-08-31 21:09:53 +0800

    Fix encode (#2283)
    * feat: fix encode
    
    * Spotless Apply
    
    * feat: fix encode
    
    * feat: fix fix encode
    
    * Spotless Apply
    
    ---------
    
    Co-authored-by: leechor <leechor@users.noreply.github.com>

---
+++ dinky-common/src/main/java/org/dinky/utils/AppConstant.java
@@ -0,0 +1,27 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.dinky.utils;
+
+public final class AppConstant {
+    public static final String JDK_VERSION = System.getProperty("java.specification.version");
+    public static final boolean JDK_ABOVE_1_8 = JDK_VERSION.compareTo("1.8") > 0;
+
+    private AppConstant() {}
+}
