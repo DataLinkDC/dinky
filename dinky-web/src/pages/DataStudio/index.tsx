8f1b5830aa1c96d8c33ebcc7c6d5e096ebf1e8ce
Licho <lecho.sun@gmail.com>
2023-09-06 09:27:41 +0800

    fx
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- dinky-web/src/pages/DataStudio/index.tsx
+++ dinky-web/src/pages/DataStudio/index.tsx
@@ -291,8 +291,13 @@ const DataStudio = (props: any) => {
             <Content style={{ display: 'flex', flexDirection: 'column' }}>
               <div style={{ display: 'flex' }}>
                 <LeftContainer size={size} />
-                <Content style={{flexGrow: 1,}}>
-                  <MiddleContainer style={{flexGrow: 1,}} />
+                <Content
+                  style={{
+                    width:
+                      size.width - 2 * VIEW.sideWidth - leftContainer.width - rightContainer.width
+                  }}
+                >
+                  <MiddleContainer />
                 </Content>
                 <RightContainer size={size} bottomHeight={bottomHeight} />
               </div>
