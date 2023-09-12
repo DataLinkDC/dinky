c9fdc6727413e8f23ba6ea6d7ac46e63940b1a07
Licho <lecho.sun@gmail.com>
2023-09-12 21:07:49 +0800

    refactor: flink web with iframe css

--- dinky-web/src/pages/Home/JobOverView/JobOperatorGraph/index.tsx
+++ dinky-web/src/pages/Home/JobOverView/JobOperatorGraph/index.tsx
@@ -1,14 +1,32 @@
 import { JobProps } from '@/pages/DevOps/JobDetail/data';
 
+
 const JobOperatorGraph = (props: JobProps) => {
-  const { jobDetail } = props;
+    const {jobDetail} = props;
+    const onLoad = () => {
+        const iframe = document.getElementById("iframe-id") as HTMLIFrameElement;
+        if (!iframe) return;
+        const innerDoc = iframe.contentWindow?.document;
+
+        const flinkJob = innerDoc?.querySelector('flink-root > nz-layout > nz-layout > nz-content');
+        if (!flinkJob) return;
+        const style = flinkJob.style;
+        style.position = 'relative';
+        style.overflow = 'hidden';
+    }
+
   return (
-    <iframe
-      width='100%'
-      height={window.innerHeight - 200}
-      style={{ border: 0 }}
-      src={`/api/flink/web/${jobDetail?.history?.jobManagerAddress}/#/job/running/${jobDetail?.instance?.jid}/overview`}
-    />
+      <div style={{border: '0px solid', overflow: 'hidden', marginLeft: '100px', width: '100%', height: '100%'}}>
+      <iframe
+          id='iframe-id'
+          scrolling="no"
+          //       src={`/api/flink/web/${jobDetail?.history?.jobManagerAddress}/#/job/running/${jobDetail?.instance?.jid}/overview`}
+          src={`http://127.0.0.1:8000/api/flink/${jobDetail?.history?.jobManagerAddress}/#/job/running/${jobDetail?.instance?.jid}/overview`}
+          onLoad={() => onLoad()}
+          sandbox='allow-same-origin allow-scripts allow-popups allow-forms'
+          style={{overflow:'hidden', border: '0px none', width: '100%', height: window.innerHeight, marginLeft: '-260px', marginRight: '-220px'}}
+      />
+      </div>
   );
 };
 
