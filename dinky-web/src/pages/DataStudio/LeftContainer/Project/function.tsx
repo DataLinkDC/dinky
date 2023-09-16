b45541fd331346edd5a61531337fc3f88ea450e3
aiwenmo <32723967+aiwenmo@users.noreply.github.com>
2023-09-16 23:23:05 +0800

    [Fix-2320] [web] Fix expanding all but only the root directory (#2321)
    Co-authored-by: wenmo <32723967+wenmo@users.noreply.github.com>

--- dinky-web/src/pages/DataStudio/LeftContainer/Project/function.tsx
+++ dinky-web/src/pages/DataStudio/LeftContainer/Project/function.tsx
@@ -1,18 +1,19 @@
-import { getTabIcon } from '@/pages/DataStudio/MiddleContainer/function';
-import { Catalogue } from '@/types/Studio/data.d';
-import { searchTreeNode } from '@/utils/function';
+import {getTabIcon} from '@/pages/DataStudio/MiddleContainer/function';
+import {Catalogue} from '@/types/Studio/data.d';
+import {searchTreeNode} from '@/utils/function';
 
 export const generateList = (data: any, list: any[]) => {
   for (const element of data) {
     const node = element;
-    const { name, id, parentId, level } = node;
-    list.push({ name, id, key: id, title: name, parentId, level });
+    const {name, id, parentId, level} = node;
+    list.push({name, id, key: id, title: name, parentId, level});
     if (node.children) {
       generateList(node.children, list);
     }
   }
   return list;
 };
+
 export const getParentKey = (key: number | string, tree: any): any => {
   let parentKey;
   for (const element of tree) {
@@ -28,6 +29,19 @@ export const getParentKey = (key: number | string, tree: any): any => {
   return parentKey;
 };
 
+export const getLeafKeyList = (tree: any): any => {
+  let leafKeyList = [];
+  for (const node of tree) {
+    if (node.isLeaf) {
+      leafKeyList.push(node.id);
+      continue;
+    }
+    if (node.children) {
+      leafKeyList = leafKeyList.concat(getLeafKeyList(node.children))
+    }
+  }
+  return leafKeyList;
+};
 /**
  * build Catalogue tree
  * @param {Catalogue[]} data
@@ -43,29 +57,30 @@ export const buildProjectTree = (
 ): any =>
   data
     ? data.map((item: Catalogue) => {
-        const currentPath = path ? [...path, item.name] : [item.name];
-        return {
-          // isLeaf: (item.type && item.children.length === 0) ,
-          isLeaf: item.isLeaf,
-          name: item.name,
-          parentId: item.parentId,
-          label: searchTreeNode(item.name, searchValue),
-          icon: item.type && item.children.length === 0 && getTabIcon(item.type, 20),
-          value: item.id,
-          path: currentPath,
-          type: item.type,
-          title: <>{searchTreeNode(item.name, searchValue)}</>,
-          fullInfo: item,
-          key: item.id,
-          id: item.id,
-          taskId: item.taskId,
-          children: buildProjectTree(item.children, searchValue, currentPath)
-        };
-      })
+      const currentPath = path ? [...path, item.name] : [item.name];
+      return {
+        // isLeaf: (item.type && item.children.length === 0) ,
+        isLeaf: item.isLeaf,
+        name: item.name,
+        parentId: item.parentId,
+        label: searchTreeNode(item.name, searchValue),
+        icon: item.type && item.children.length === 0 && getTabIcon(item.type, 20),
+        value: item.id,
+        path: currentPath,
+        type: item.type,
+        title: <>{searchTreeNode(item.name, searchValue)}</>,
+        fullInfo: item,
+        key: item.id,
+        id: item.id,
+        taskId: item.taskId,
+        children: buildProjectTree(item.children, searchValue, currentPath)
+      };
+    })
     : [];
 
 export const isUDF = (jobType: string) => {
   return jobType === 'Scala' || jobType === 'Python' || jobType === 'Java';
 };
 
-export const buildUDFTree = (data: []) => {};
+export const buildUDFTree = (data: []) => {
+};
