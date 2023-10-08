5b5b6d2168c78619619e88685777629f2dfc3ffa
zhu-mingye <934230207@qq.com>
2023-10-08 10:24:17 +0800

    fix import

--- dinky-admin/src/main/java/org/dinky/job/handler/JobRefreshHandler.java
+++ dinky-admin/src/main/java/org/dinky/job/handler/JobRefreshHandler.java
@@ -50,7 +50,7 @@ import java.time.LocalDateTime;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import cn.hutool.json.JSONObject;
