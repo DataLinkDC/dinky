6e15c71ce0f3f85a2178b6673470f44fcedc2894
WintleChoung <cwtao@live.cn>
2023-10-09 20:12:37 +0800

    [Fix] flink task query use enum value. (#2364)
    Co-authored-by: 常文陶 <changwt@olymtech.com>

--- dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
+++ dlink-admin/src/main/java/com/dlink/service/impl/TaskServiceImpl.java
@@ -507,7 +507,7 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
 
     @Override
     public List<Task> listFlinkSQLEnv() {
-        return this.list(new QueryWrapper<Task>().eq("dialect", Dialect.FLINKSQLENV).eq("enabled", 1));
+        return this.list(new QueryWrapper<Task>().eq("dialect", Dialect.FLINKSQLENV.getValue()).eq("enabled", 1));
     }
 
     @Override
@@ -621,7 +621,7 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
     @Override
     public Task getUDFByClassName(String className) {
         Task task = getOne(
-                new QueryWrapper<Task>().in("dialect", Dialect.JAVA, Dialect.SCALA, Dialect.PYTHON).eq("enabled", 1)
+                new QueryWrapper<Task>().in("dialect", Dialect.JAVA.getValue(), Dialect.SCALA.getValue(), Dialect.PYTHON.getValue()).eq("enabled", 1)
                         .eq("save_point_path", className));
         Asserts.checkNull(task, StrUtil.format("class: {} ,not exists!", className));
         task.setStatement(statementService.getById(task.getId()).getStatement());
@@ -630,7 +630,7 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
 
     @Override
     public List<Task> getAllUDF() {
-        List<Task> tasks = list(new QueryWrapper<Task>().in("dialect", Dialect.JAVA, Dialect.SCALA, Dialect.PYTHON)
+        List<Task> tasks = list(new QueryWrapper<Task>().in("dialect", Dialect.JAVA.getValue(), Dialect.SCALA.getValue(), Dialect.PYTHON.getValue())
                 .eq("enabled", 1).isNotNull("save_point_path"));
         return tasks.stream().peek(task -> {
             Assert.check(task);
