c3effadc8e2a77c618503dd84459ec54cc7dfc05
zhu-mingye <934230207@qq.com>
2023-10-28 08:55:59 -0500

    [Feature][Web]support custom editor language height light (#2451)
    * Spotless Apply
    
    * support custom editor language height light
    
    * Spotless Apply
    
    ---------
    
    Co-authored-by: zhu-mingye <zhu-mingye@users.noreply.github.com>

--- dinky-web/src/components/CustomEditor/languages/javalog/index.tsx
+++ dinky-web/src/components/CustomEditor/languages/javalog/index.tsx
@@ -1,20 +1,29 @@
-import {editor} from "monaco-editor";
-import {languages} from "monaco-editor";
-
 // todo 自定义语言不生效
-export  const JavaLog = () => {
-    // Register a new language
-    languages.register({ id: "java-log" });
-
-// Register a tokens provider for the language
-    languages.setMonarchTokensProvider("java-log", {
-        tokenizer: {
-            root: [
-                [/\[error.*/, "custom-error"],
-                [/\[notice.*/, "custom-notice"],
-                [/.*/, "custom-info"],
-                [/\[[a-zA-Z 0-9:]+\]/, "custom-date"],
-            ],
-        },
-    });
+export function LogLanguage(monaco: any) {
+  // Register a new language
+  monaco?.languages.register({
+    id: 'javalog',
+    extensions: ['.log'],
+    aliases: ['javalog', 'Javalog', 'Javalog', 'jl', 'log']
+  });
+  monaco?.languages.setMonarchTokensProvider('javalog', {
+    // todo: 如果需要分段展示不同的颜色 则需要在这里添加规则, 且正则必须是严格模式, 否则会匹配一整行,无法达到预期效果
+    tokenizer: {
+      root: [
+        // 默认不区分大小写 //
+        [/\[(\w*-\d*)+\]/, 'custom-thread'],
+        [/\s+(\w.+):$/, 'custom-class'],
+        [/error/, 'custom-error'],
+        [/warring/, 'custom-warning'],
+        [/warn/, 'custom-warning'],
+        [/info/, 'custom-info'],
+        [
+          /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d\.\d{3}/,
+          'custom-date'
+        ]
+      ]
+    },
+    ignoreCase: true,
+    unicode: true
+  });
 }
