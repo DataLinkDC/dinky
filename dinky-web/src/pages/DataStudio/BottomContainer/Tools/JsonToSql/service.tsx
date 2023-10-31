0cbbaaec44a9425f6adeb3867bebe5660a828101
ZackYoung <zackyoungh@163.com>
2023-10-31 15:44:10 +0800

    add_json_to_sql_tool

--- dinky-web/src/pages/DataStudio/BottomContainer/Tools/JsonToSql/service.tsx
+++ dinky-web/src/pages/DataStudio/BottomContainer/Tools/JsonToSql/service.tsx
@@ -2,5 +2,5 @@ import {postAll} from "@/services/api";
 
 
 export async function jsonToSql(params: any) {
-    return (await postAll('/api/tools/jsonToFlinkSql', params)).datas;
+    return (await postAll('/api/tools/jsonToFlinkSql', params)).data;
 }
