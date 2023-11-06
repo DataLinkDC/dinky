ec2bd42f3d0cc5373a92e63a082ad3263d203a5e
zhu-mingye <934230207@qq.com>
2023-11-06 15:36:46 +0800

    modify git build model layout

--- dinky-web/src/pages/RegCenter/GitProject/components/ProjectProTable/index.tsx
+++ dinky-web/src/pages/RegCenter/GitProject/components/ProjectProTable/index.tsx
@@ -1,19 +1,19 @@
 /*
  *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements.  See the NOTICE file distributed with
- *   this work for additional information regarding copyright ownership.
- *   The ASF licenses this file to You under the Apache License, Version 2.0
- *   (the "License"); you may not use this file except in compliance with
- *   the License.  You may obtain a copy of the License at
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
 
@@ -353,6 +353,15 @@ const ProjectProTable: React.FC = () => {
   };
 
   /**
+   * re try build
+   * @param value
+   */
+  const handleReTryBuild = async (value: Partial<GitProject>) => {
+    handleCancel();
+    await handleBuild(value);
+  };
+
+  /**
    * render
    */
   return (
@@ -401,7 +410,9 @@ const ProjectProTable: React.FC = () => {
       {gitProjectStatus.buildOpen && (
         <BuildSteps
           title={l('rc.gp.build')}
-          onCancel={handleCancel}
+          onOk={handleCancel}
+          onRebuild={() => handleReTryBuild(gitProjectStatus.value)}
+          onReTry={() => handleReTryBuild(gitProjectStatus.value)}
           values={gitProjectStatus.value}
         />
       )}
@@ -410,7 +421,9 @@ const ProjectProTable: React.FC = () => {
       {gitProjectStatus.logOpen && (
         <BuildSteps
           title={l('rc.gp.log')}
-          onCancel={handleCancel}
+          onOk={handleCancel}
+          showLog={gitProjectStatus.logOpen}
+          onRebuild={() => handleReTryBuild(gitProjectStatus.value)}
           values={gitProjectStatus.value}
         />
       )}
--- dinky-web/src/pages/RegCenter/GitProject/data.d.tsx
+++ dinky-web/src/pages/RegCenter/GitProject/data.d.tsx
@@ -1,24 +1,26 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 
 export type BuildStepsState = {
   key: number;
   title: string;
-  status: string;
+  status: string; // 0-notStart(未开始) 1-process(进行中/构建中) 2-failed(构建失败) 3-success(构建成功)
   description: string;
   disabled: boolean;
   onClick: () => void;
