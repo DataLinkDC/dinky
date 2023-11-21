4e6fe01de8fb4e3f6d56c1045d93e33c2d6fca7e
licho <lecho.sun@gmail.com>
2023-11-21 12:25:47 +0800

    fix: SSE init problem
    Signed-off-by: licho <lecho.sun@gmail.com>

--- dinky-web/src/models/Sse.tsx
+++ dinky-web/src/models/Sse.tsx
@@ -20,7 +20,7 @@
 import { postAll } from '@/services/api';
 import { ErrorMessage } from '@/utils/messages';
 import { useEffect, useRef, useState } from 'react';
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 
 export type SseData = {
