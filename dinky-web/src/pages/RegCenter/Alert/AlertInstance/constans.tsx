b8d64620ec8e0fbd2c1d954bfb32d4ce9803742f
Zzm0809 <Zzm0809@users.noreply.github.com>
2023-12-13 06:17:00 +0000

    Spotless Apply

--- dinky-web/src/pages/RegCenter/Alert/AlertInstance/constans.tsx
+++ dinky-web/src/pages/RegCenter/Alert/AlertInstance/constans.tsx
@@ -17,10 +17,6 @@
  *
  */
 
-import { ALERT_TYPE } from '@/types/RegCenter/data.d';
-import { l } from '@/utils/intl';
-import { DefaultOptionType } from 'rc-select/es/Select';
-import {Space} from "antd";
 import {
   AliYunSmsSvg,
   DingTalkSvg,
@@ -29,27 +25,51 @@ import {
   SmsSvg,
   TencentSmsSvg,
   WeChatSvg
-} from "@/components/Icons/AlertIcon";
+} from '@/components/Icons/AlertIcon';
+import { ALERT_TYPE } from '@/types/RegCenter/data.d';
+import { l } from '@/utils/intl';
+import { Space } from 'antd';
+import { DefaultOptionType } from 'rc-select/es/Select';
 
 export const ALERT_TYPE_LIST_OPTIONS: DefaultOptionType[] = [
   {
-    label: <Space align={'baseline'} size={5}><DingTalkSvg size={16} /> {l('rc.ai.dingTalk')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <DingTalkSvg size={16} /> {l('rc.ai.dingTalk')}
+      </Space>
+    ),
     value: ALERT_TYPE.DINGTALK
   },
   {
-    label: <Space align={'baseline'} size={5}><WeChatSvg size={16} /> {l('rc.ai.wechat')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <WeChatSvg size={16} /> {l('rc.ai.wechat')}
+      </Space>
+    ),
     value: ALERT_TYPE.WECHAT
   },
   {
-    label: <Space align={'baseline'} size={5}><FeiShuSvg size={16} /> {l('rc.ai.feishu')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <FeiShuSvg size={16} /> {l('rc.ai.feishu')}
+      </Space>
+    ),
     value: ALERT_TYPE.FEISHU
   },
   {
-    label: <Space align={'baseline'} size={5}><EmailSvg size={16} /> {l('rc.ai.email')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <EmailSvg size={16} /> {l('rc.ai.email')}
+      </Space>
+    ),
     value: ALERT_TYPE.EMAIL
   },
   {
-    label: <Space align={'baseline'} size={5}><SmsSvg size={16} /> {l('rc.ai.sms')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <SmsSvg size={16} /> {l('rc.ai.sms')}
+      </Space>
+    ),
     value: ALERT_TYPE.SMS
   }
 ];
@@ -70,12 +90,20 @@ export enum SMS_TYPE {
 
 export const MANU_FRACTURES = [
   {
-    label: <Space align={'baseline'} size={5}><AliYunSmsSvg size={16} /> {l('rc.ai.mf.alibaba')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <AliYunSmsSvg size={16} /> {l('rc.ai.mf.alibaba')}
+      </Space>
+    ),
     value: SMS_TYPE.ALIBABA,
     key: l('rc.ai.mf.alibaba')
   },
   {
-    label: <Space align={'baseline'} size={5}><TencentSmsSvg size={16} /> {l('rc.ai.mf.tencent')}</Space>,
+    label: (
+      <Space align={'baseline'} size={5}>
+        <TencentSmsSvg size={16} /> {l('rc.ai.mf.tencent')}
+      </Space>
+    ),
     value: SMS_TYPE.TENCENT,
     key: l('rc.ai.mf.tencent')
   }
