9d2d57e709e977151ca228de4f8d82c66fc30597
licho <lecho.sun@gmail.com>
2023-12-15 09:51:28 +0800

    feat: listen all network
    Signed-off-by: licho <lecho.sun@gmail.com>

--- dinky-admin/src/main/java/org/dinky/service/impl/PrintTableServiceImpl.java
+++ dinky-admin/src/main/java/org/dinky/service/impl/PrintTableServiceImpl.java
@@ -114,7 +114,7 @@ public class PrintTableServiceImpl implements PrintTableService {
         private static DatagramSocket getDatagramSocket(int port) {
             InetAddress host = null;
             try {
-                host = InetAddress.getByName("127.0.0.1");
+                host = InetAddress.getByName("0.0.0.0");
                 return new DatagramSocket(port, host);
             } catch (SocketException | UnknownHostException e) {
                 log.error(
