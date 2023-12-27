f5936d737628929f33b8131f2086e5d9d7896cdd
Zzm0809 <934230207@qq.com>
2023-12-27 21:20:45 +0800

    added devops docs

--- dinky-admin/src/main/java/org/dinky/service/impl/TaskServiceImpl.java
+++ dinky-admin/src/main/java/org/dinky/service/impl/TaskServiceImpl.java
@@ -512,13 +512,20 @@ public class TaskServiceImpl extends SuperServiceImpl<TaskMapper, Task> implemen
         if (saved && Asserts.isNotNull(task.getJobInstanceId())) {
             JobInstance jobInstance = jobInstanceService.getById(task.getJobInstanceId());
             jobInstance.setStep(lifeCycle.getValue());
-            jobInstanceService.updateById(jobInstance);
-            log.info("jobInstance [{}] step change to {}", jobInstance.getJid(), lifeCycle.getValue());
+            boolean updatedJobInstance = jobInstanceService.updateById(jobInstance);
+            if (updatedJobInstance) {
+                jobInstanceService.refreshJobInfoDetail(jobInstance.getId(), true);
+                log.warn(
+                        "JobInstance [{}] step change to [{}] ,Trigger Force Refresh",
+                        jobInstance.getName(),
+                        lifeCycle.getValue());
+            }
         }
         return saved;
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public boolean saveOrUpdateTask(Task task) {
         Task byId = getById(task.getId());
         if (byId != null && JobLifeCycle.PUBLISH.equalsValue(byId.getStep())) {
