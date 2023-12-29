0e6a80cd91bf8fb12e8aae12402f2f22ab1a9b63
Zzm0809 <934230207@qq.com>
2023-12-29 18:25:46 +0800

    [Fix-2823] fix jobconfig cannot render yarnprejob cluster (#2851)
    * fix-jobconfig-cannot-render-yarnprejob-cluster
    
    * Spotless Apply
    
    ---------
    
    Co-authored-by: Zzm0809 <Zzm0809@users.noreply.github.com>

--- dinky-web/src/locales/zh-CN/pages.ts
+++ dinky-web/src/locales/zh-CN/pages.ts
@@ -407,7 +407,8 @@ export default {
   'pages.datastudio.label.jobConfig.clusterConfig': 'Flink集群配置',
   'pages.datastudio.label.jobConfig.clusterConfig.tip1':
     '选择Flink集群配置进行【{type}】模式的远程提交任务',
-  'pages.datastudio.label.jobConfig.clusterConfig.tip2': '选择Flink集群配置',
+  'pages.datastudio.label.jobConfig.clusterConfig.tip2':
+    '如下拉框无数据,请先配置/新建集群实例,或者检查集群配置/集群实例是否为健康可用状态',
   'pages.datastudio.label.jobConfig.execmode.tip': '指定 Flink 任务的执行模式，默认为 Local',
   'pages.datastudio.label.jobConfig.flinksql.env': 'FlinkSQL 环境',
   'pages.datastudio.label.jobConfig.flinksql.env.tip1':
