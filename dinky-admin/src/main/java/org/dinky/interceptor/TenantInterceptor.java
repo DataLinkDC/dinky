0f263826b92f43b4595f02eb870334b1b1ac653a
Zzm0809 <934230207@qq.com>
2024-01-30 12:19:31 +0800

    fix-token-error

--- dinky-admin/src/main/java/org/dinky/interceptor/TenantInterceptor.java
+++ dinky-admin/src/main/java/org/dinky/interceptor/TenantInterceptor.java
@@ -19,31 +19,30 @@
 
 package org.dinky.interceptor;
 
+import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.dao.SaTokenDao;
+import cn.dev33.satoken.stp.StpLogic;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.lang.Opt;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.dinky.assertion.Asserts;
 import org.dinky.context.TenantContextHolder;
 import org.dinky.context.UserInfoContextHolder;
 import org.dinky.data.dto.UserDTO;
 import org.dinky.data.model.rbac.Tenant;
-
-import org.apache.commons.collections4.CollectionUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
-import cn.dev33.satoken.SaManager;
-import cn.dev33.satoken.stp.StpUtil;
-import cn.hutool.core.lang.Opt;
-import lombok.extern.slf4j.Slf4j;
-
-/** tenant interceptor */
+/**
+ * tenant interceptor
+ */
 @Slf4j
 public class TenantInterceptor implements AsyncHandlerInterceptor {
 
@@ -56,13 +55,18 @@ public class TenantInterceptor implements AsyncHandlerInterceptor {
         if (Asserts.isNotNull(cookies)) {
             for (Cookie cookie : cookies) {
                 switch (cookie.getName()) {
-                    case "satoken":
+                    case "token":
                         token = Opt.ofBlankAble(cookie.getValue());
-                        if (SaManager.getSaTokenDao().get("satoken:login:token:" + token.get()) != null) {
+                        SaTokenDao saTokenDao = SaManager.getSaTokenDao();
+                        String keyTokenValue = StpUtil.getStpLogic().splicingKeyTokenValue(token.get());
+                        if (saTokenDao.get(keyTokenValue) != null) {
                             isPass = true;
                         }
                         break;
                     case "tenantId":
+                        if (!StpUtil.isLogin()){
+                            return false;
+                        }
                         UserDTO userInfo = UserInfoContextHolder.get(StpUtil.getLoginIdAsInt());
                         if (Asserts.isNull(userInfo)) {
                             StpUtil.logout(StpUtil.getLoginIdAsInt());
