000c7a815e94b38a38875cc6e2a15c4a36300c7d
ikiler <ikiler@126.com>
2024-02-24 20:21:27 +0800

    Fix the nginx proxy on the web page cannot access static resources

--- dinky-web/config/config.ts
+++ dinky-web/config/config.ts
@@ -37,6 +37,8 @@ export default defineConfig({
       hasFixedHeader: true, // Whether there is a fixed header
    },*/
 
+  base: './',
+  publicPath: './',
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
@@ -166,7 +168,7 @@ export default defineConfig({
    */
   headScripts: [
     // 解决首次加载时白屏的问题
-    { src: '/scripts/loading.js', async: true }
+    { src: './scripts/loading.js', async: true }
   ],
   //================ pro 插件配置 =================
   presets: ['umi-presets-pro'],
