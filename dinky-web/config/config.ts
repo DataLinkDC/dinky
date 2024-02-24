2e606b3f3fa833fc86b0b022786ec35186378c5a
ikiler <ikiler@126.com>
2024-02-24 20:39:31 +0800

    Fix the nginx proxy on the web page cannot access static resources

--- dinky-web/config/config.ts
+++ dinky-web/config/config.ts
@@ -38,7 +38,8 @@ export default defineConfig({
    },*/
 
   base: './',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
