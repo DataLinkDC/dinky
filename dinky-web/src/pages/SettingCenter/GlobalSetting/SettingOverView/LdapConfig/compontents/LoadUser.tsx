0d23109bcb3752b8ff65d337865314cd719e274b
Zzm0809 <Zzm0809@users.noreply.github.com>
2024-02-28 03:20:08 +0000

    Spotless Apply

--- dinky-web/src/pages/SettingCenter/GlobalSetting/SettingOverView/LdapConfig/compontents/LoadUser.tsx
+++ dinky-web/src/pages/SettingCenter/GlobalSetting/SettingOverView/LdapConfig/compontents/LoadUser.tsx
@@ -24,7 +24,7 @@ import { UserBaseInfo } from '@/types/AuthCenter/data.d';
 import { l } from '@/utils/intl';
 import { UserSwitchOutlined } from '@ant-design/icons';
 import { ModalForm } from '@ant-design/pro-components';
-import {Input, Table, Tag} from 'antd';
+import { Input, Table, Tag } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import { useState } from 'react';
 
@@ -74,8 +74,8 @@ export const LoadUser = () => {
         title={l('sys.ldap.settings.loadUser')}
         width={'50%'}
         submitter={{
-          submitButtonProps:{
-            disabled : selectedUsers.length === 0
+          submitButtonProps: {
+            disabled: selectedUsers.length === 0
           }
         }}
         onFinish={() => importUser()}
@@ -85,7 +85,13 @@ export const LoadUser = () => {
           </Tag>
         }
       >
-        <Input.Search placeholder={l('sys.ldap.settings.keyword')} style={{ marginBottom: 8 }} enterButton loading={loading} onSearch={(value) => setKeyword(value)} />
+        <Input.Search
+          placeholder={l('sys.ldap.settings.keyword')}
+          style={{ marginBottom: 8 }}
+          enterButton
+          loading={loading}
+          onSearch={(value) => setKeyword(value)}
+        />
         <Table<UserBaseInfo.User>
           loading={loading}
           size={'small'}
@@ -94,8 +100,10 @@ export const LoadUser = () => {
             onChange: (_, rows) => setSelectedUsers(rows),
             getCheckboxProps: (record) => ({ disabled: !record.enabled })
           }}
-          dataSource={users.filter(user => user.username.indexOf(keyword) !== -1 || user.nickname?.indexOf(keyword) !== -1)
-          }
+          dataSource={users.filter(
+            (user) =>
+              user.username.indexOf(keyword) !== -1 || user.nickname?.indexOf(keyword) !== -1
+          )}
           rowKey={'username'}
         />
       </ModalForm>
