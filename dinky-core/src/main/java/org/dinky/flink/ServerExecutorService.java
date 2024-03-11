0a0cb846c858761be9ac4111a57e100379bd2772
leechor <leechor@users.noreply.github.com>
2024-03-11 01:36:42 +0000

    Spotless Apply

--- dinky-core/src/main/java/org/dinky/flink/ServerExecutorService.java
+++ dinky-core/src/main/java/org/dinky/flink/ServerExecutorService.java
@@ -19,10 +19,6 @@
 
 package org.dinky.flink;
 
-import org.dinky.executor.ExecutorConfig;
-
 import java.rmi.Remote;
 
-public interface ServerExecutorService extends Remote, DinkyExecutor {
-
-}
+public interface ServerExecutorService extends Remote, DinkyExecutor {}
