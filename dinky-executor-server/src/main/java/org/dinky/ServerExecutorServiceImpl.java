069ed9ee555bfaf8fd45157cded12aba1ad87a40
Lichao <lecho.sun@gmail.com>
2024-03-11 21:16:24 +0800

    feat: patch ServerExecutorServiceImpl

--- dinky-executor-server/src/main/java/org/dinky/ServerExecutorServiceImpl.java
+++ dinky-executor-server/src/main/java/org/dinky/ServerExecutorServiceImpl.java
@@ -3,15 +3,21 @@ package org.dinky;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import org.dinky.executor.Executor;
 import org.dinky.executor.ExecutorConfig;
+import org.dinky.executor.ExecutorFactory;
 import org.dinky.flink.ServerExecutorService;
 
 public class ServerExecutorServiceImpl extends UnicastRemoteObject implements ServerExecutorService {
+    private Executor executor;
+
     public ServerExecutorServiceImpl() throws RemoteException {
     }
 
     @Override
     public void init(ExecutorConfig executorConfig) {
+        executor = ExecutorFactory.buildExecutor(executorConfig);
 
     }
+
 }
