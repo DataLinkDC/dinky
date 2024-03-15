505b32245d3ef5895c0341653f4539bd52f3ef07
Zzm0809 <934230207@qq.com>
2024-03-15 16:15:17 +0800

    op udf manage tips

--- dinky-web/src/pages/RegCenter/UDF/components/UDFRegister/index.tsx
+++ dinky-web/src/pages/RegCenter/UDF/components/UDFRegister/index.tsx
@@ -30,7 +30,6 @@ import { ProColumns } from '@ant-design/pro-components';
 import ProTable, { ActionType } from '@ant-design/pro-table';
 import React, { Key, useEffect, useRef, useState } from 'react';
 import { add, update } from './service';
-import {Alert} from "antd";
 
 type UDFRegisterProps = {
   showEdit: boolean;
@@ -173,9 +172,6 @@ const UDFRegister: React.FC<UDFRegisterProps> = (props) => {
     };
 
     return (
-    <>
-      {/* TODO: 等待UDF和数据开发联动功能开发完成之后,删除该提示语*/}
-      <Alert message={'该功能目前没有和数据开发进行联动,目前仅为展示该 jar 中的相关 UDF,如你在数据开发中使用 UDF 时,你仍然需要按照 Flink 中的创建 UDF 的方式'} type="info" showIcon />
       <ProTable
         rowKey={'id'}
         columns={columns}
@@ -195,7 +191,6 @@ const UDFRegister: React.FC<UDFRegisterProps> = (props) => {
           actionRender: (_, _2, defaultDom) => [defaultDom.save, defaultDom.cancel]
         }}
       />
-    </>
     );
   };
 
