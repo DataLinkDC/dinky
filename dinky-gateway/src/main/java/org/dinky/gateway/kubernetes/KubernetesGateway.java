ec581d32362e6687a8f12fce9571e3b71a891fd1
gaoyan <ikiler@126.com>
2024-04-03 23:42:33 +0800

    [Bug] Fix k8s pod template not decorate  bug (#3350)
    Co-authored-by: gaoyan1998 <gaoyan1998@users.noreply.github.com>

--- dinky-gateway/src/main/java/org/dinky/gateway/kubernetes/KubernetesGateway.java
+++ dinky-gateway/src/main/java/org/dinky/gateway/kubernetes/KubernetesGateway.java
@@ -28,7 +28,9 @@ import org.dinky.gateway.exception.GatewayException;
 import org.dinky.gateway.kubernetes.utils.K8sClientHelper;
 import org.dinky.gateway.result.SavePointResult;
 import org.dinky.gateway.result.TestResult;
+import org.dinky.utils.TextUtil;
 
+import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.CoreOptions;
 import org.apache.flink.configuration.DeploymentOptions;
 import org.apache.flink.configuration.GlobalConfiguration;
@@ -44,8 +46,11 @@ import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.UUID;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.text.StrFormatter;
 import cn.hutool.core.util.ReflectUtil;
+import io.fabric8.kubernetes.api.model.Pod;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
@@ -62,7 +67,12 @@ public abstract class KubernetesGateway extends AbstractGateway {
     protected FlinkConfig flinkConfig;
     protected K8sConfig k8sConfig;
 
+    private Pod jmPodTemplate;
+    private Pod tmPodTemplate;
+    private Pod defaultPodTemplate;
+
     private K8sClientHelper k8sClientHelper;
+    private String tmpConfDir = String.format("%s/tmp/kubernets/%s", System.getProperty("user.dir"), UUID.randomUUID());
 
     public KubernetesGateway() {}
 
@@ -97,7 +107,35 @@ public abstract class KubernetesGateway extends AbstractGateway {
             resetCheckpointInApplicationMode(flinkConfig.getJobName());
         }
 
-        k8sClientHelper = new K8sClientHelper(configuration, k8sConfig);
+        preparPodTemplate(k8sConfig.getKubeConfig(), KubernetesConfigOptions.KUBE_CONFIG_FILE);
+        k8sClientHelper = new K8sClientHelper(configuration, k8sConfig.getKubeConfig());
+
+        String sql = config.getSql();
+        defaultPodTemplate = k8sClientHelper.decoratePodTemplate(sql, k8sConfig.getPodTemplate());
+        preparPodTemplate(
+                k8sClientHelper.dumpPod2Str(defaultPodTemplate), KubernetesConfigOptions.KUBERNETES_POD_TEMPLATE);
+
+        if (!TextUtil.isEmpty(k8sConfig.getJmPodTemplate())) {
+            jmPodTemplate = k8sClientHelper.decoratePodTemplate(sql, k8sConfig.getJmPodTemplate());
+            preparPodTemplate(
+                    k8sClientHelper.dumpPod2Str(jmPodTemplate), KubernetesConfigOptions.JOB_MANAGER_POD_TEMPLATE);
+        }
+        if (!TextUtil.isEmpty(k8sConfig.getTmPodTemplate())) {
+            tmPodTemplate = k8sClientHelper.decoratePodTemplate(sql, k8sConfig.getJmPodTemplate());
+            preparPodTemplate(
+                    k8sClientHelper.dumpPod2Str(tmPodTemplate), KubernetesConfigOptions.JOB_MANAGER_POD_TEMPLATE);
+        }
+    }
+
+    protected void preparPodTemplate(String podTemplate, ConfigOption<String> option) {
+        if (!TextUtil.isEmpty(podTemplate)) {
+            String filePath = String.format("%s/%s.yaml", tmpConfDir, option.key());
+            if (FileUtil.exist(filePath)) {
+                Assert.isTrue(FileUtil.del(filePath));
+            }
+            FileUtil.writeUtf8String(podTemplate, filePath);
+            addConfigParas(option, filePath);
+        }
     }
 
     public SavePointResult savepointCluster(String savePoint) {
@@ -184,6 +222,11 @@ public abstract class KubernetesGateway extends AbstractGateway {
     }
 
     public boolean close() {
+        try {
+            FileUtil.del(tmpConfDir);
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+        }
         if (k8sClientHelper != null) {
             return k8sClientHelper.close();
         }
