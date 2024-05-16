abd92612b03176166a91f71a7e85a2b07f07c694
Zzm0809 <934230207@qq.com>
2024-05-16 17:04:44 +0800

    Add Tenant Automatically Add Admin User Association
    Signed-off-by: Zzm0809 <934230207@qq.com>

--- dinky-admin/src/main/java/org/dinky/service/impl/TenantServiceImpl.java
+++ dinky-admin/src/main/java/org/dinky/service/impl/TenantServiceImpl.java
@@ -19,6 +19,8 @@
 
 package org.dinky.service.impl;
 
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.dinky.assertion.Asserts;
 import org.dinky.context.TenantContextHolder;
 import org.dinky.data.constant.BaseConstant;
@@ -52,6 +54,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TenantServiceImpl extends SuperServiceImpl<TenantMapper, Tenant> implements TenantService {
 
     @Resource
@@ -71,6 +74,17 @@ public class TenantServiceImpl extends SuperServiceImpl<TenantMapper, Tenant> im
             }
             tenant.setIsDelete(false);
             if (save(tenant)) {
+               // When adding a new tenant, the admin user will be added to the tenant by default
+                Long count = new LambdaQueryChainWrapper<>(userTenantService.getBaseMapper())
+                        .eq(UserTenant::getUserId, BaseConstant.ADMIN_ID).eq(UserTenant::getTenantId, tenant.getId()).count();
+                if (count == 0) {
+                    UserTenant userTenant = new UserTenant();
+                    userTenant.setUserId(BaseConstant.ADMIN_ID);
+                    userTenant.setTenantId(tenant.getId());
+                    userTenant.setTenantAdminFlag(true); // set an admin flag to true,because the admin user is the tenant admin
+                    userTenantService.save(userTenant);
+                    log.info("You have added tenant {}, and the system will automatically add the admin user to that tenant.", tenant.getTenantCode());
+                }
                 TenantContextHolder.set(tenant.getId());
                 return Result.succeed(Status.ADDED_SUCCESS);
             }
