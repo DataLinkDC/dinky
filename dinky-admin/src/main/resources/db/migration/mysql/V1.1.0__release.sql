3ac287c9b1cea7fb4e9ea40483a61d8d9c3d1789
Zzm0809 <934230207@qq.com>
2024-05-24 09:19:48 +0800

    Optimizing MySQL stored procedures
    Signed-off-by: Zzm0809 <934230207@qq.com>

--- dinky-admin/src/main/resources/db/migration/mysql/V1.1.0__release.sql
+++ dinky-admin/src/main/resources/db/migration/mysql/V1.1.0__release.sql
@@ -5,9 +5,17 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 -- 创建存储过程 用于添加表字段时判断字段是否存在, 如果字段不存在则添加字段, 如果字段存在则不执行任何操作,避免添加重复字段时抛出异常,从而终止Flyway执行, 在 Flyway 执行时, 如果你需要增加字段,必须使用该存储过程
 -- Create a stored procedure to determine whether a field exists when adding table fields. If the field does not exist, add it. If the field exists, do not perform any operations to avoid throwing exceptions when adding duplicate fields. When executing in Flyway, if you need to add a field, you must use this stored procedure
+-- Parameter Description:
+-- tableName: Table name
+-- columnName: Field name
+-- columnDefinitionType: Field type
+-- columnDefinitionDefaultValue Value: Field default value
+-- columnDefinitionComment: Field comment
+-- afterColumnName: Field position, default value is empty. If it is not empty, it means adding a field after the afterColumnName field
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS add_column_if_not_exists$$
-CREATE PROCEDURE if not exists add_column_if_not_exists(IN tableName VARCHAR(64), IN columnName VARCHAR(64), IN columnDefinitionType VARCHAR(64), IN columnDefinitionDefaultValue VARCHAR(128), IN columnDefinitionComment VARCHAR(255))
+CREATE PROCEDURE if not exists add_column_if_not_exists(IN tableName VARCHAR(64), IN columnName VARCHAR(64), IN columnDefinitionType VARCHAR(64), IN columnDefinitionDefaultValue VARCHAR(128), IN columnDefinitionComment VARCHAR(255), in afterColumnName VARCHAR(64))
 BEGIN
     IF NOT EXISTS (
         SELECT *
@@ -16,7 +24,12 @@ BEGIN
           AND table_name = tableName
           AND column_name = columnName
     ) THEN
-        SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', columnName, ' ', columnDefinitionType , ' DEFAULT ', columnDefinitionDefaultValue, ' COMMENT ', columnDefinitionComment);
+        -- 判断 afterColumnName 入参是否 有值, 如果有值则拼接 afterColumnName 和 columnName 之间的关系
+        IF (afterColumnName IS NOT NULL OR afterColumnName <> '') THEN
+            SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', columnName, ' ', columnDefinitionType, ' DEFAULT ', columnDefinitionDefaultValue, ' COMMENT ', columnDefinitionComment, ' AFTER ', afterColumnName);
+        ELSE
+            SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', columnName, ' ', columnDefinitionType, ' DEFAULT ', columnDefinitionDefaultValue, ' COMMENT ', columnDefinitionComment);
+        END IF;
         PREPARE stmt FROM @sql;
         EXECUTE stmt;
     END IF;
@@ -53,8 +66,8 @@ where `id` = 119;
 ALTER TABLE dinky_udf_manage CHANGE COLUMN class_name class_name VARCHAR(100) null DEFAULT null COMMENT 'Complete class name';
 
 
-CALL add_column_if_not_exists('dinky_task', 'first_level_owner', 'int', 'NULL', 'primary responsible person id');
-CALL add_column_if_not_exists('dinky_task', 'second_level_owners', 'varchar(128)', 'NULL', 'list of secondary responsible persons ids');
+CALL add_column_if_not_exists('dinky_task', 'first_level_owner', 'int', 'NULL', 'primary responsible person id' ,'');
+CALL add_column_if_not_exists('dinky_task', 'second_level_owners', 'varchar(128)', 'NULL', 'list of secondary responsible persons ids' , '');
 
 
 update dinky_task set first_level_owner = creator;
@@ -70,10 +83,6 @@ ALTER TABLE dinky_task_version CHANGE COLUMN `statement` `statement` mediumtext 
 
 ALTER TABLE dinky_resources CHANGE COLUMN `file_name` `file_name` text DEFAULT NULL COMMENT 'file name';
 
-
-
-alter table dinky_udf_manage add column `language` VARCHAR(10) DEFAULT null comment 'udf language' after class_name;
-
-CALL add_column_if_not_exists('dinky_udf_manage', 'language', 'varchar(10)', 'NULL', 'udf language');
+CALL add_column_if_not_exists('dinky_udf_manage', 'language', 'varchar(10)', 'NULL', 'udf language' , 'class_name');
 
 SET FOREIGN_KEY_CHECKS = 1;
