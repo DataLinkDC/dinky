71f9487d23f6613f8ea068b94b65f2039924ad3b
Zane <1435865327@qq.com>
2024-06-18 15:42:28 +0800

    style(数据开发-任务保存): 添加接口参数校验
    任务保存接口参数校验，便于排查参数确缺失异常问题

--- dinky-admin/src/main/java/org/dinky/controller/TaskController.java
+++ dinky-admin/src/main/java/org/dinky/controller/TaskController.java
@@ -40,10 +40,12 @@ import org.dinky.data.result.SqlExplainResult;
 import org.dinky.gateway.enums.SavePointType;
 import org.dinky.gateway.result.SavePointResult;
 import org.dinky.job.JobResult;
+import org.dinky.mybatis.annotation.Save;
 import org.dinky.service.TaskService;
 
 import java.util.List;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -190,7 +192,7 @@ public class TaskController {
             paramType = "body",
             dataTypeClass = TaskSaveDTO.class)
     @CheckTaskOwner(serviceType = TaskService.class)
-    public Result<Void> saveOrUpdateTask(@RequestBody TaskSaveDTO task) {
+    public Result<Void> saveOrUpdateTask(@Validated({Save.class}) @RequestBody TaskSaveDTO task) {
         if (taskService.saveOrUpdateTask(task.toTaskEntity())) {
             return Result.succeed(Status.SAVE_SUCCESS);
         } else {
