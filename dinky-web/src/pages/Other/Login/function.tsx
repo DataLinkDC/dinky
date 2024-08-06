3900843813a046367cef8be8cc7c67dd20506ba7
Zzm0809 <934230207@qq.com>
2024-08-06 15:22:40 +0800

    delete notes
    Signed-off-by: Zzm0809 <934230207@qq.com>

--- dinky-web/src/pages/Other/Login/function.tsx
+++ dinky-web/src/pages/Other/Login/function.tsx
@@ -47,14 +47,11 @@ export const redirectToLogin = (tipMsg: string) => {
 export const initSomeThing = () => {
   //  initialize setting theme
   setLocalThemeToStorage();
-  // 设置服务版本到localstorage
-
   queryDataByParams<string>(API_CONSTANTS.GET_SERVICE_VERSION).then((result) => {
     if (result && result != getValueFromLocalStorage(SERVER_VERSION)) {
       console.log('current version:', getValueFromLocalStorage(SERVER_VERSION));
       console.log('update server version:', result);
       setKeyToLocalStorage(SERVER_VERSION, result)
-      // 清理 缓存
       console.log('clean dva cache');
       window.localStorage.removeItem('persist:root');
     }
