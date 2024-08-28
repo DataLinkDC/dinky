7daedeba21b038b309f97e5571208128af8a19bc
Zzm0809 <934230207@qq.com>
2024-08-28 17:04:30 +0800

    [Fix][admin] Fix the SQL auto initialization issue of PG (#3756)
    Signed-off-by: Zzm0809 <934230207@qq.com>

--- dinky-admin/src/main/resources/db/migration/pgsql/V20240827.1.1.1__release.sql
+++ dinky-admin/src/main/resources/db/migration/pgsql/V20240827.1.1.1__release.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE FUNCTION public.drop_index_if_exists(
 DECLARE
     index_exists int;
 BEGIN
-    -- 检查索引是否存在
+    -- Check if the index exists
     SELECT count(*)
     INTO index_exists
     FROM pg_indexes
@@ -24,3 +24,29 @@ $$ LANGUAGE plpgsql;
 
 
 SELECT public.drop_index_if_exists('public', 'cluster_un_idx1', 'dinky_cluster');
+
+
+CREATE TABLE IF NOT EXISTS public.dinky_dashboard
+(
+    id               SERIAL PRIMARY KEY          NOT NULL,
+    name             VARCHAR(255),
+    remark TEXT,
+    chart_theme      VARCHAR(255),
+    layouts          TEXT,
+    create_time      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON COLUMN public.dinky_dashboard.id IS 'ID';
+COMMENT ON COLUMN public.dinky_dashboard.name IS 'name';
+COMMENT ON COLUMN public.dinky_dashboard.remark IS 'remark';
+COMMENT ON COLUMN public.dinky_dashboard.chart_theme IS 'chart theme';
+COMMENT ON COLUMN public.dinky_dashboard.layouts IS 'layouts';
+COMMENT ON COLUMN public.dinky_dashboard.create_time IS 'create time';
+COMMENT ON COLUMN public.dinky_dashboard.update_time IS 'update time';
+
+CREATE OR REPLACE TRIGGER update_dinky_dashboard_update_time
+              BEFORE UPDATE
+                         ON public.dinky_dashboard
+                         FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
