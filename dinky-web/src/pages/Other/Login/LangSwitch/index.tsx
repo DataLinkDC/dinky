44fe2d159651b8e083931aab47e6370339a84ca6
zackyoungh <zackyoungh@users.noreply.github.com>
2024-08-29 06:35:10 +0000

    Spotless Apply

--- dinky-web/src/pages/Other/Login/LangSwitch/index.tsx
+++ dinky-web/src/pages/Other/Login/LangSwitch/index.tsx
@@ -39,7 +39,7 @@ const LangSwitch = () => {
 
   const langClassName = useEmotionCss(({ token }) => {
     return {
-      color:"#fff",
+      color: '#fff',
       width: 42,
       lineHeight: '42px',
       position: 'absolute',
