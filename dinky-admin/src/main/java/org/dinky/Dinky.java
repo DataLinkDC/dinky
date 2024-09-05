8043982c664a307d094ad1975f1abb76631fa83d
zackyoungh <zackyoungh@users.noreply.github.com>
2024-09-05 07:16:58 +0000

    Spotless Apply

--- dinky-admin/src/main/java/org/dinky/Dinky.java
+++ dinky-admin/src/main/java/org/dinky/Dinky.java
@@ -19,13 +19,15 @@
 
 package org.dinky;
 
-import com.alibaba.druid.proxy.DruidDriver;
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alibaba.druid.proxy.DruidDriver;
+
+import lombok.SneakyThrows;
 
 /**
  * Dinky Starter
@@ -43,7 +45,8 @@ public class Dinky {
 
     @SneakyThrows
     public static void main(String[] args) {
-        // Initialize the JDBC Driver, because the number of packages is very large, so it needs to be executed asynchronously and loaded in advance
+        // Initialize the JDBC Driver, because the number of packages is very large, so it needs to be executed
+        // asynchronously and loaded in advance
         // chinese: 初始化JDBC Driver，因为包的数量特别庞大，所以这里需要异步执行，并提前加载Driver
         new Thread(DruidDriver::getInstance).start();
 
