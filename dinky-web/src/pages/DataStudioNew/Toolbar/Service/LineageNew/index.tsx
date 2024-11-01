9bf8f59a67b0d854884cebdcf17c465393170b48
zackyoungh <zackyoungh@users.noreply.github.com>
2024-11-01 02:39:04 +0000

    Spotless Apply

--- dinky-web/src/pages/DataStudioNew/Toolbar/Service/LineageNew/index.tsx
+++ dinky-web/src/pages/DataStudioNew/Toolbar/Service/LineageNew/index.tsx
@@ -1,15 +1,34 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 import { Circle, Path } from '@antv/g';
 import { CircleCombo, ExtensionCategory, Graph, register } from '@antv/g6';
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from 'react';
 import { Graph as G6Graph } from '@antv/g6';
-import {useAsyncEffect} from "ahooks";
+import { useAsyncEffect } from 'ahooks';
 const collapse = (x, y, r) => {
   return [
     ['M', x - r, y],
     ['a', r, r, 0, 1, 0, r * 2, 0],
     ['a', r, r, 0, 1, 0, -r * 2, 0],
     ['M', x - r + 4, y],
-    ['L', x + r - 4, y],
+    ['L', x + r - 4, y]
   ];
 };
 
@@ -21,7 +40,7 @@ const expand = (x, y, r) => {
     ['M', x - r + 4, y],
     ['L', x - r + 2 * r - 4, y],
     ['M', x - r + r, y - r + 4],
-    ['L', x, y + r - 4],
+    ['L', x, y + r - 4]
   ];
 };
 
@@ -38,7 +57,12 @@ class CircleComboWithExtraButton extends CircleCombo {
     const y = height / 2 + btnR;
     const d = collapsed ? expand(0, y, btnR) : collapse(0, y, btnR);
 
-    const hitArea = this.upsert('hit-area', Circle, { cy: y, r: 10, fill: '#fff', cursor: 'pointer' }, this);
+    const hitArea = this.upsert(
+      'hit-area',
+      Circle,
+      { cy: y, r: 10, fill: '#fff', cursor: 'pointer' },
+      this
+    );
     this.upsert('button', Path, { stroke: '#3d81f7', d, cursor: 'pointer' }, hitArea);
   }
 
@@ -53,14 +77,11 @@ class CircleComboWithExtraButton extends CircleCombo {
   }
 }
 
-
-
-
 export const LineageNew = () => {
   const graphRef = useRef<G6Graph>();
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect( () => {
+  useEffect(() => {
     register(ExtensionCategory.COMBO, 'circle-combo-with-extra-button', CircleComboWithExtraButton);
 
     const graph = new G6Graph({
@@ -69,21 +90,21 @@ export const LineageNew = () => {
         nodes: [
           { id: 'node-0', combo: 'combo-0', style: { x: 100, y: 100 } },
           { id: 'node-1', combo: 'combo-0', style: { x: 150, y: 100 } },
-          { id: 'node-2', style: { x: 250, y: 100 } },
+          { id: 'node-2', style: { x: 250, y: 100 } }
         ],
         edges: [{ source: 'node-1', target: 'node-2' }],
-        combos: [{ id: 'combo-0' }],
+        combos: [{ id: 'combo-0' }]
       },
       combo: {
-        type: 'circle-combo-with-extra-button',
+        type: 'circle-combo-with-extra-button'
       },
-      behaviors: ['drag-element'],
+      behaviors: ['drag-element']
     });
 
     graphRef.current = graph;
 
-     graph.render().then();
-    return  () => {
+    graph.render().then();
+    return () => {
       const graph = graphRef.current;
       if (graph) {
         graph.destroy();
--- dinky-web/src/pages/DataStudioNew/Toolbar/Service/index.tsx
+++ dinky-web/src/pages/DataStudioNew/Toolbar/Service/index.tsx
@@ -17,14 +17,14 @@
  *
  */
 
-import {connect} from '@@/exports';
-import {CenterTab, DataStudioState} from '@/pages/DataStudioNew/model';
-import {mapDispatchToProps} from '@/pages/DataStudioNew/DvaFunction';
-import {Flex, Tabs, TabsProps, TreeDataNode} from 'antd';
-import {Panel, PanelGroup} from 'react-resizable-panels';
+import { connect } from '@@/exports';
+import { CenterTab, DataStudioState } from '@/pages/DataStudioNew/model';
+import { mapDispatchToProps } from '@/pages/DataStudioNew/DvaFunction';
+import { Flex, Tabs, TabsProps, TreeDataNode } from 'antd';
+import { Panel, PanelGroup } from 'react-resizable-panels';
 import DirectoryTree from 'antd/es/tree/DirectoryTree';
 import './index.less';
-import React, {Key, useEffect, useMemo, useState} from 'react';
+import React, { Key, useEffect, useMemo, useState } from 'react';
 import {
   ApartmentOutlined,
   ArrowsAltOutlined,
@@ -40,24 +40,24 @@ import RunToolBarButton from '@/pages/DataStudioNew/components/RunToolBarButton'
 import CusPanelResizeHandle from '@/pages/DataStudioNew/components/CusPanelResizeHandle';
 import Output from '@/pages/DataStudioNew/Toolbar/Service/Output';
 import ExecutionHistory from '@/pages/DataStudioNew/Toolbar/Service/ExecutionHistory';
-import {KeepAlive} from 'react-activation';
-import {DataStudioActionType} from '@/pages/DataStudioNew/data.d';
+import { KeepAlive } from 'react-activation';
+import { DataStudioActionType } from '@/pages/DataStudioNew/data.d';
 import Explain from '@/pages/DataStudioNew/Toolbar/Service/Explain';
 import FlinkGraph from '@/pages/DataStudioNew/Toolbar/Service/FlinkGraph';
 import Lineage from '@/pages/DataStudioNew/Toolbar/Service/Lineage';
 import Result from '@/pages/DataStudioNew/Toolbar/Service/Result';
-import {getTabIcon} from '@/pages/DataStudioNew/function';
-import {assert} from '@/pages/DataStudio/function';
-import {DIALECT} from '@/services/constants';
-import {TableData} from '@/pages/DataStudioNew/Toolbar/Service/TableData';
-import {isSql} from '@/pages/DataStudioNew/utils';
-import {LineageNew} from "@/pages/DataStudioNew/Toolbar/Service/LineageNew";
+import { getTabIcon } from '@/pages/DataStudioNew/function';
+import { assert } from '@/pages/DataStudio/function';
+import { DIALECT } from '@/services/constants';
+import { TableData } from '@/pages/DataStudioNew/Toolbar/Service/TableData';
+import { isSql } from '@/pages/DataStudioNew/utils';
+import { LineageNew } from '@/pages/DataStudioNew/Toolbar/Service/LineageNew';
 
 const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) => {
   const {
     showDesc,
     tabs,
-    action: {actionType, params}
+    action: { actionType, params }
   } = props;
   const [selectedKey, setSelectedKey] = useState<Key[]>([]);
   const [taskItems, setTaskItems] = useState<Record<string, TabsProps['items']>>({});
@@ -82,20 +82,20 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
       [DataStudioActionType.TASK_RUN_CHECK]: {
         key: actionType,
         label: '检查',
-        icon: <AuditOutlined/>,
-        children: <Explain data={params.data}/>
+        icon: <AuditOutlined />,
+        children: <Explain data={params.data} />
       },
       [DataStudioActionType.TASK_RUN_DAG]: {
         key: actionType,
         label: 'DAG',
-        icon: <ApartmentOutlined/>,
-        children: <FlinkGraph data={params.data}/>
+        icon: <ApartmentOutlined />,
+        children: <FlinkGraph data={params.data} />
       },
       [DataStudioActionType.TASK_RUN_LINEAGE]: {
         key: actionType,
         label: '血缘',
-        icon: <PartitionOutlined/>,
-        children: <Lineage data={params.data}/>
+        icon: <PartitionOutlined />,
+        children: <Lineage data={params.data} />
       }
     };
   }, [actionType, params?.data]);
@@ -121,7 +121,7 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
         setTaskItems((prevState) => {
           const item = prevState[params.taskId]!!;
           item.find((item) => item.key === actionType)!!.children = route.children;
-          return {...prevState};
+          return { ...prevState };
         });
       }
       setTabActiveKey((prevState) => ({
@@ -173,7 +173,7 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
                 (child) => child.key === dialect
               ) as TreeDataNode;
               if (!currentDialectTree) {
-                node.children!!.push({title: dialect, key: dialect, icon: icon, children: []});
+                node.children!!.push({ title: dialect, key: dialect, icon: icon, children: [] });
                 currentDialectTree = node.children!!.find(
                   (child) => child.key === dialect
                 ) as TreeDataNode;
@@ -198,31 +198,31 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
       const taskId = selectedKey[0] as number;
       const taskParams = tabs.find((tab) => tab.params.taskId === taskId)?.params;
       if (!taskParams) {
-        setSelectedKey([])
+        setSelectedKey([]);
         setTaskItems((prevState) => {
-          const newState = {...prevState};
+          const newState = { ...prevState };
           delete newState[taskId];
           return newState;
-        })
+        });
         setTabActiveKey((prevState) => {
-          const newState = {...prevState};
+          const newState = { ...prevState };
           delete newState[taskId];
           return newState;
-        })
+        });
       }
       const items: TabsProps['items'] = [
         {
           key: DataStudioActionType.TASK_RUN_SUBMIT,
           label: '输出',
-          icon: <CodeOutlined/>,
-          children: <Output taskId={taskId}/>
+          icon: <CodeOutlined />,
+          children: <Output taskId={taskId} />
         },
         {
           key: DataStudioActionType.TASK_RUN_DEBUG,
           label: '结果',
-          icon: <MonitorOutlined/>,
-          children: <Result taskId={taskId} action={props.action} dialect={taskParams?.dialect}/>
-        },
+          icon: <MonitorOutlined />,
+          children: <Result taskId={taskId} action={props.action} dialect={taskParams?.dialect} />
+        }
         // {
         //   key: "new-lineage",
         //   label: '新血缘',
@@ -234,16 +234,16 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
         items.push({
           key: 'history',
           label: '执行历史',
-          icon: <HistoryOutlined/>,
-          children: <ExecutionHistory taskId={taskId}/>
+          icon: <HistoryOutlined />,
+          children: <ExecutionHistory taskId={taskId} />
         });
       }
       if (assert(taskParams?.dialect, [DIALECT.FLINK_SQL], true, 'includes')) {
         items.push({
           key: 'tableData',
           label: '表数据',
-          icon: <TableOutlined/>,
-          children: <TableData statement={taskParams?.statement}/>
+          icon: <TableOutlined />,
+          children: <TableData statement={taskParams?.statement} />
         });
       }
 
@@ -259,19 +259,19 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
               [taskId]: activeKey
             }));
           }}
-          style={{height: '100%'}}
+          style={{ height: '100%' }}
         />
       );
     }
   };
   return (
     <PanelGroup direction={'horizontal'}>
-      <Panel defaultSize={20} style={{display: 'flex', flexDirection: 'column'}}>
+      <Panel defaultSize={20} style={{ display: 'flex', flexDirection: 'column' }}>
         <Flex justify={'right'}>
           <RunToolBarButton
             showDesc={showDesc}
             desc={'全部折叠'}
-            icon={<ShrinkOutlined/>}
+            icon={<ShrinkOutlined />}
             sleepTime={100}
             onClick={async () => {
               setExpandKeys([]);
@@ -280,7 +280,7 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
           <RunToolBarButton
             showDesc={showDesc}
             desc={'全部展开'}
-            icon={<ArrowsAltOutlined/>}
+            icon={<ArrowsAltOutlined />}
             sleepTime={100}
             onClick={async () => {
               expandAll();
@@ -294,7 +294,7 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
           onExpand={(expandedKeys) => {
             setExpandKeys(expandedKeys);
           }}
-          onSelect={(selectedKeys, {node}) => {
+          onSelect={(selectedKeys, { node }) => {
             node.isLeaf && setSelectedKey(selectedKeys);
           }}
           blockNode
@@ -303,8 +303,8 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
 
       {selectedKey && selectedKey.length > 0 && selectedKey[0] !== undefined && (
         <>
-          <CusPanelResizeHandle/>
-          <Panel style={{paddingInline: 10}}>
+          <CusPanelResizeHandle />
+          <Panel style={{ paddingInline: 10 }}>
             <KeepAlive cacheKey={'service:' + selectedKey[0]} when={() => false}>
               {renderContent()}
             </KeepAlive>
@@ -316,7 +316,7 @@ const Service = (props: { showDesc: boolean; tabs: CenterTab[]; action: any }) =
 };
 
 export default connect(
-  ({DataStudio}: { DataStudio: DataStudioState }) => ({
+  ({ DataStudio }: { DataStudio: DataStudioState }) => ({
     project: DataStudio.toolbar.project,
     action: DataStudio.action,
     showDesc: DataStudio.toolbar.showDesc,
