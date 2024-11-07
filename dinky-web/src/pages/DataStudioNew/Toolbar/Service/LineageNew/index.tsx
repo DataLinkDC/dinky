3ed9c6fafc9c2beab9219e322726432ef3c39a47
ZackYoung <zackyoungh@163.com>
2024-11-07 14:32:19 +0800

    优化血缘展示

--- dinky-web/src/pages/DataStudioNew/Toolbar/Service/LineageNew/index.tsx
+++ dinky-web/src/pages/DataStudioNew/Toolbar/Service/LineageNew/index.tsx
@@ -17,22 +17,15 @@
  *
  */
 
-import { Circle, Group, Path } from '@antv/g';
-import {
-  ExtensionCategory,
-  Graph,
-  PathArray,
-  RectCombo,
-  RectComboStyleProps,
-  register
-} from '@antv/g6';
-import { memo, useContext, useRef } from 'react';
-import { Flex } from 'antd';
-import { ReactNode } from '@antv/g6-extension-react';
-import { Graphin } from '@antv/graphin';
-import { DagreLayout, GridLayout } from '@antv/layout';
-import { LineageDetailInfo } from '@/types/DevOps/data';
-import { DataStudioContext } from '@/pages/DataStudioNew/DataStudioContext';
+import {Circle, Group, Path} from '@antv/g';
+import {ExtensionCategory, Graph, PathArray, RectCombo, RectComboStyleProps, register} from '@antv/g6';
+import {memo, useContext, useEffect, useRef} from 'react';
+import {Flex} from 'antd';
+import {ReactNode} from '@antv/g6-extension-react';
+import {Graphin} from '@antv/graphin';
+import {DagreLayout, GridLayout} from '@antv/layout';
+import {LineageDetailInfo} from '@/types/DevOps/data';
+import {DataStudioContext} from '@/pages/DataStudioNew/DataStudioContext';
 
 const collapse = (x: number, y: number, r: number) => {
   return [
@@ -63,7 +56,7 @@ class RectComboWithExtraButton extends RectCombo {
   }
 
   drawButton(attributes: Required<RectComboStyleProps>) {
-    const { collapsed } = attributes;
+    const {collapsed} = attributes;
     const [, height] = this.getKeySize(attributes);
     const btnR = 8;
     const y = -(height / 2 + btnR);
@@ -72,17 +65,17 @@ class RectComboWithExtraButton extends RectCombo {
     const hitArea = this.upsert(
       'hit-area',
       Circle,
-      { cy: y, r: 10, fill: '#fff', cursor: 'pointer' },
+      {cy: y, r: 10, fill: '#fff', cursor: 'pointer'},
       this
     );
-    this.upsert('button', Path, { stroke: '#3d81f7', d, cursor: 'pointer' }, hitArea!!);
+    this.upsert('button', Path, {stroke: '#3d81f7', d, cursor: 'pointer'}, hitArea!!);
   }
 
   onCreate() {
     this.shapeMap['hit-area'].addEventListener('click', () => {
       const id = this.id;
       const collapsed = !this.attributes.collapsed;
-      const { graph } = this.attributes.context!!;
+      const {graph} = this.attributes.context!!;
       if (collapsed) graph.collapseElement(id);
       else graph.expandElement(id);
     });
@@ -93,10 +86,23 @@ register(ExtensionCategory.COMBO, 'circle-combo-with-extra-button', RectComboWit
 
 register(ExtensionCategory.NODE, 'react', ReactNode);
 export const LineageNew = memo((props: { data: LineageDetailInfo }) => {
-  const { data } = props;
-  const { theme } = useContext(DataStudioContext);
+  const {data} = props;
+  const {theme} = useContext(DataStudioContext);
   const graphRef = useRef<Graph>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
+
+  useEffect(() => {
+    // 监控布局宽度高度变化，重新计算树的高度
+    const element = containerRef.current!!;
+    const observer = new ResizeObserver((entries) => {
+      if (entries?.length === 1) {
+        graphRef.current?.setSize(entries[0].contentRect.width, entries[0].contentRect.height);
+      }
+    });
+    observer.observe(element);
+    return () => observer.unobserve(element);
+  }, []);
   // 把data.tables 的id ,name转成map
   const tablesMap = data.tables.reduce(
     (acc, item) => {
@@ -106,126 +112,135 @@ export const LineageNew = memo((props: { data: LineageDetailInfo }) => {
     {} as Record<string, string>
   );
   return (
-    <Graphin
-      ref={graphRef}
-      style={{ overflow: 'hidden' }}
-      options={{
-        autoResize: true,
-        theme: theme === 'light' ? theme : 'dark',
-        data: {
-          nodes: data.tables.flatMap((item) =>
-            item.columns.map((column) => ({
-              id: item.id + column.name,
-              combo: item.id,
-              data: { name: column.name }
-            }))
-          ),
-          edges: data.relations.map((item) => ({
-            source: item.srcTableId + item.srcTableColName,
-            target: item.tgtTableId + item.tgtTableColName
-          })),
-          combos: data.tables.map((item) => ({ id: item.id }))
-        },
-        combo: {
-          type: 'circle-combo-with-extra-button',
-          style: {
-            labelText: (d) => tablesMap[d.id]
-          }
-        },
-        node: {
-          type: 'react',
-          style: {
-            size: [240, 20],
-            component: (data: { data: { name: string } }) => (
-              <Flex
-                justify={'center'}
-                align={'center'}
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  background: '#fff',
-                  borderRadius: 5,
-                  border: '1px solid gray'
-                }}
-                vertical
-              >
-                {data.data.name}
-              </Flex>
+    <div ref={containerRef} style={{width:'100%',height:'100%'}}>
+      <Graphin
+        ref={graphRef}
+        style={{overflow: 'hidden'}}
+        options={{
+          autoResize: true,
+          theme: theme === 'light' ? theme : 'dark',
+          data: {
+            nodes: data.tables.flatMap((item) =>
+              item.columns.map((column) => ({
+                id: item.id + column.name,
+                combo: item.id,
+                data: {name: column.name}
+              }))
             ),
-            port: true,
-            ports: [{ placement: 'right' }, { placement: 'left' }]
-          }
-        },
-        edge: {
-          type: 'cubic-horizontal',
-          style: {
-            labelBackground: true,
-            endArrow: true
-          }
-        },
-        layout: {
-          type: 'combo-combined',
-          // comboPadding: 40,
-          // nodeSize: 0,
-          // spacing: 0,
-          innerLayout: new GridLayout({ cols: 1, condense: true }),
-          outerLayout: new DagreLayout({
-            rankdir: 'LR',
-            edgeLabelSpace: false,
-            nodesep: 5,
-            ranksep: 50
-          })
-        },
-        behaviors: [
-          'drag-canvas',
-          'zoom-canvas',
-          {
-            type: 'hover-activate',
-            degree: 100 // 👈🏻 Activate relations.
-          }
-        ],
-        plugins: [
-          {
-            key: 'grid-line',
-            type: 'grid-line',
-            follow: false,
-            size: 40,
-            stroke: 'var(--border-color)'
+            edges: data.relations.map((item) => ({
+              source: item.srcTableId + item.srcTableColName,
+              target: item.tgtTableId + item.tgtTableColName
+            })),
+            combos: data.tables.map((item) => ({id: item.id}))
           },
-          {
-            type: 'toolbar',
-            position: 'right-top',
-            onClick: (item: string) => {
-              const graph = graphRef.current;
-              switch (item) {
-                // 放大
-                case 'zoom-in':
-                  graph?.zoomTo(graph?.getZoom() + 0.2);
-                  break;
-                case 'zoom-out':
-                  graph?.zoomBy(0.5);
-                  break;
-                case 'auto-fit':
-                  graph?.fitView();
-                  break;
-              }
-            },
-            getItems: () => {
-              return [
-                { id: 'zoom-in', value: 'zoom-in' },
-                { id: 'zoom-out', value: 'zoom-out' },
-                { id: 'auto-fit', value: 'auto-fit' }
-              ];
-            },
+          combo: {
+            type: 'circle-combo-with-extra-button',
             style: {
-              backgroundColor: 'var(--btn-background-color)'
+              labelText: (d) => tablesMap[d.id]
             }
           },
-          { key: 'background', type: 'background', background: 'var(--primary-color)' }
-        ],
-        transforms: ['process-parallel-edges'],
-        autoFit: 'view'
-      }}
-    />
+          node: {
+            type: 'react',
+            style: {
+              size: [240, 20],
+              component: (data: { data: { name: string } }) => (
+                <Flex
+                  justify={'center'}
+                  align={'center'}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    background: 'var(--main-background-color)',
+                    borderRadius: 5,
+                    border: '1px solid gray'
+                  }}
+                  vertical
+                >
+                  {data.data.name}
+                </Flex>
+              ),
+              port: true,
+              ports: [{placement: 'right'}, {placement: 'left'}]
+            }
+          },
+          edge: {
+            type: 'cubic-horizontal',
+            style: {
+              endArrow: true,
+              endArrowType:'vee',
+            }
+          },
+          layout: {
+            type: 'combo-combined',
+            innerLayout: new GridLayout({cols: 1, condense: true}),
+            outerLayout: new DagreLayout({
+              rankdir: 'LR',
+              edgeLabelSpace: false,
+              nodesep: 5,
+              ranksep: 50
+            })
+          },
+          behaviors: [
+            'focus-element',
+            'drag-canvas',
+            'zoom-canvas',
+            {
+              type: 'hover-activate',
+              enable: (event) => event.targetType === 'node',
+              degree: 1, // 👈🏻 Activate relations.
+              state: 'highlight',
+              inactiveState: 'dim',
+              onHover: (event) => {
+                event.view.setCursor('pointer');
+              },
+              onHoverEnd: (event) => {
+                event.view.setCursor('default');
+              },
+            }
+          ],
+          plugins: [
+            {
+              key: 'grid-line',
+              type: 'grid-line',
+              follow: false,
+              size: 40,
+              stroke: 'var(--border-color)'
+            },
+            {
+              type: 'toolbar',
+              position: 'right-top',
+              onClick: (item: string) => {
+                const graph = graphRef.current;
+                switch (item) {
+                  // 放大
+                  case 'zoom-in':
+                    graph?.zoomTo(graph?.getZoom() + 0.2);
+                    break;
+                  case 'zoom-out':
+                    graph?.zoomBy(0.5);
+                    break;
+                  case 'auto-fit':
+                    graph?.fitView();
+                    break;
+                }
+              },
+              getItems: () => {
+                return [
+                  {id: 'zoom-in', value: 'zoom-in'},
+                  {id: 'zoom-out', value: 'zoom-out'},
+                  {id: 'auto-fit', value: 'auto-fit'}
+                ];
+              },
+              style: {
+                backgroundColor: 'var(--btn-background-color)'
+              }
+            },
+            {key: 'background', type: 'background', background: 'var(--primary-color)'}
+          ],
+          transforms: ['process-parallel-edges'],
+          autoFit: 'view'
+        }}
+      />
+    </div>
   );
 });
