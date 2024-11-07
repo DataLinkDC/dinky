dbaa27b53b389375664bcea2da8813ab77ff7778
Zzm0809 <Zzm0809@users.noreply.github.com>
2024-11-07 08:25:54 +0000

    Spotless Apply

--- dinky-web/src/pages/DataStudioNew/CenterTabContent/SqlTask.tsx
+++ dinky-web/src/pages/DataStudioNew/CenterTabContent/SqlTask.tsx
@@ -580,12 +580,7 @@ export const SqlTask = memo((props: FlinkSqlProps & any) => {
               desc={l('menu.datastudio.lineage')}
               icon={<PartitionOutlined />}
               onClick={handleLineage}
-              isShow={assert(
-                currentState.dialect,
-                [DIALECT.FLINK_SQL],
-                true,
-                'includes'
-              )}
+              isShow={assert(currentState.dialect, [DIALECT.FLINK_SQL], true, 'includes')}
             />
 
             {assert(
