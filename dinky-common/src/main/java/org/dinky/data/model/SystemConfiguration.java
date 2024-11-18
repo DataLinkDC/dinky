849c13eba3e0ea84b89b7b66d181eac625a0332c
ZackYoung <zackyoungh@163.com>
2024-11-18 13:38:50 +0800

    noChangeList

--- dinky-common/src/main/java/org/dinky/data/model/SystemConfiguration.java
+++ dinky-common/src/main/java/org/dinky/data/model/SystemConfiguration.java
@@ -447,6 +447,7 @@ public class SystemConfiguration {
                     FileUtil.file(DirConstant.getTempRootDir(), "flink-job-archive")
                             .getAbsolutePath());
             config.put("historyserver.archive.fs.dir", FLINK_JOB_ARCHIVE);
+            config.put("historyserver.archive.clean-expired-jobs","true" );
         }
         return config;
     }
