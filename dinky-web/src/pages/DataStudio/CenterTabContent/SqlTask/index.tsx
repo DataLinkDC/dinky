fa5fce618190c62add0b1ebd97b94984557e3586
wenmo <32723967+aiwenmo@users.noreply.github.com>
2024-11-22 22:16:25 +0800

    Revert code

--- dinky-web/src/pages/DataStudio/CenterTabContent/SqlTask/index.tsx
+++ dinky-web/src/pages/DataStudio/CenterTabContent/SqlTask/index.tsx
@@ -30,10 +30,10 @@ import {
   CaretRightOutlined,
   ClearOutlined,
   CloseOutlined,
+  CloudDownloadOutlined,
   EnvironmentOutlined,
   FullscreenExitOutlined,
   FullscreenOutlined,
-  MergeCellsOutlined,
   PartitionOutlined,
   PauseOutlined,
   RocketOutlined,
@@ -726,7 +726,7 @@ export const SqlTask = memo((props: FlinkSqlProps & any) => {
             <Divider type={'vertical'} style={{ height: dividerHeight }} />
             <RunToolBarButton
               showDesc={showDesc}
-              disabled={isLockTask}
+              disabled={isLockTask || JOB_LIFE_CYCLE.PUBLISH == currentState.step}
               desc={l('shortcut.key.format')}
               icon={<ClearOutlined />}
               onClick={handleFormat}
@@ -754,7 +754,7 @@ export const SqlTask = memo((props: FlinkSqlProps & any) => {
               showDesc={showDesc}
               disabled={isLockTask}
               desc={l('button.offline')}
-              icon={<MergeCellsOutlined />}
+              icon={<CloudDownloadOutlined style={{ color: 'red' }} />}
               onClick={handleChangeJobLife}
             />
             <RunToolBarButton
