47f39883c61a027eb483cc86976c22bb7797916c
ZackYoung <zackyoungh@163.com>
2024-11-27 21:51:09 +0800

    fix_session_cannot_use_add_customjar

--- dinky-core/src/main/java/org/dinky/job/runner/JobDDLRunner.java
+++ dinky-core/src/main/java/org/dinky/job/runner/JobDDLRunner.java
@@ -144,26 +144,24 @@ public class JobDDLRunner extends AbstractJobRunner {
 
     private void executeAdd(String statement) {
         Set<File> allFilePath = AddJarSqlParseStrategy.getAllFilePath(statement);
-        allFilePath
-                .forEach(t -> jobManager.getUdfPathContextHolder().addOtherPlugins(t));
+        allFilePath.forEach(t -> jobManager.getUdfPathContextHolder().addOtherPlugins(t));
         (jobManager.getExecutor().getDinkyClassLoader())
                 .addURLs(URLUtils.getURLs(jobManager.getUdfPathContextHolder().getOtherPluginsFiles()));
     }
 
     private void executeAddFile(String statement) {
         Set<File> allFilePath = AddFileSqlParseStrategy.getAllFilePath(statement);
-        allFilePath
-                .forEach(t -> jobManager.getUdfPathContextHolder().addFile(t));
+        allFilePath.forEach(t -> jobManager.getUdfPathContextHolder().addFile(t));
         (jobManager.getExecutor().getDinkyClassLoader())
                 .addURLs(URLUtils.getURLs(jobManager.getUdfPathContextHolder().getFiles()));
-        jobManager.getExecutor().addJar(ArrayUtil.toArray(allFilePath,File.class));
+        jobManager.getExecutor().addJar(ArrayUtil.toArray(allFilePath, File.class));
     }
 
     private void executeAddJar(String statement) {
         Set<File> allFilePath = AddFileSqlParseStrategy.getAllFilePath(statement);
         Configuration combinationConfig = getCombinationConfig();
         FileSystem.initialize(combinationConfig, null);
-        jobManager.getExecutor().addJar(ArrayUtil.toArray(allFilePath,File.class));
+        jobManager.getExecutor().addJar(ArrayUtil.toArray(allFilePath, File.class));
         jobManager.getExecutor().executeSql(statement);
     }
 
