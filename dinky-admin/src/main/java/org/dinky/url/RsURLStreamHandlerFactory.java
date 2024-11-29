91e873a09be8c3f74fda135ffa490c61b8351df7
ZackYoung <zackyoungh@163.com>
2024-11-29 12:49:34 +0800

    optimzie_some_code

--- dinky-admin/src/main/java/org/dinky/url/RsURLStreamHandlerFactory.java
+++ dinky-admin/src/main/java/org/dinky/url/RsURLStreamHandlerFactory.java
@@ -33,18 +33,25 @@ import cn.hutool.core.util.StrUtil;
 
 @Profile("!test")
 public class RsURLStreamHandlerFactory implements URLStreamHandlerFactory {
-    private final List<String> notContains = Arrays.asList("jar", "file", "http", "https");
+    private static final String PREFIX = "sun.net.www.protocol";
 
     @Override
     public URLStreamHandler createURLStreamHandler(String protocol) {
-        if ("rs".equals(protocol)) {
+        if ("rs".equalsIgnoreCase(protocol)) {
             return new RsURLStreamHandler();
         }
-        for (String tempProtocol : notContains) {
-            if (tempProtocol.equals(StrUtil.sub(protocol, 0, tempProtocol.length()))) {
-                return null;
-            }
+        String name = PREFIX + "." + protocol + ".Handler";
+        try {
+            @SuppressWarnings("deprecation")
+            Object o = Class.forName(name).newInstance();
+            return (URLStreamHandler) o;
+        } catch (ClassNotFoundException x) {
+            // ignore
+        } catch (Exception e) {
+            // For compatibility, all Exceptions are ignored.
+            // any number of exceptions can get thrown here
         }
+
 
         try {
             Class.forName("org.apache.hadoop.fs.FsUrlStreamHandlerFactory");
