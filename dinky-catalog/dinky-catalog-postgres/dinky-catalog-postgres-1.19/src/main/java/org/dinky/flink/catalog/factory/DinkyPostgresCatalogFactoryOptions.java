b2fdb6911da855a3ee95c23945fef819649a388f
Zzm0809 <934230207@qq.com>
2024-12-04 16:34:27 +0800

    feat-support-pgsql-catalog

--- dinky-catalog/dinky-catalog-postgres/dinky-catalog-postgres-1.19/src/main/java/org/dinky/flink/catalog/factory/DinkyPostgresCatalogFactoryOptions.java
+++ dinky-catalog/dinky-catalog-postgres/dinky-catalog-postgres-1.19/src/main/java/org/dinky/flink/catalog/factory/DinkyPostgresCatalogFactoryOptions.java
@@ -19,7 +19,6 @@
 
 package org.dinky.flink.catalog.factory;
 
-import org.dinky.flink.catalog.DinkyPostgresCatalog;
 
 import org.apache.flink.annotation.Internal;
 import org.apache.flink.configuration.ConfigOption;
