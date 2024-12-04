7885704b974f84fb9a7bec87283df6d2e59871ae
Zzm0809 <934230207@qq.com>
2024-12-04 14:25:27 +0800

    feat-support-pgsql-catalog

--- dinky-admin/src/main/java/org/dinky/configure/DinkyCustomConfiguration.java
+++ dinky-admin/src/main/java/org/dinky/configure/DinkyCustomConfiguration.java
@@ -19,6 +19,8 @@
 
 package org.dinky.configure;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
