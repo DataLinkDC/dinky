84269387df95162b53388ef148dbbd42813c3985
MactavishCui <MactavishCui@163.com>
2024-12-24 00:26:38 +0800

    register center optimization

--- dinky-web/src/pages/RegCenter/Document/constans.ts
+++ dinky-web/src/pages/RegCenter/Document/constans.ts
@@ -17,6 +17,8 @@
  *
  */
 
+import { l } from "@/utils/intl";
+
 export const DOCUMENT_CATEGORY_ENUMS = {
   Variable: { text: 'Variable', value: 'Variable' },
   Module: { text: 'Module', value: 'Module' },
@@ -31,32 +33,32 @@ export const DOCUMENT_CATEGORY_ENUMS = {
  * document function type enum map
  */
 export const DOCUMENT_TYPE_ENUMS = {
-  SQL_TEMPLATE: { text: '代码片段/模板', value: 'SQL_TEMPLATE' },
-  FLINK_OPTIONS: { text: 'Flink参数', value: 'FLINK_OPTIONS' },
-  FUN_UDF: { text: '函数/UDF', value: 'FUN_UDF' },
-  OTHER: { text: '其他', value: 'OTHER' }
+  SQL_TEMPLATE: { text: l('rc.doc.type.codeSnippetOrTemplate'), value: 'SQL_TEMPLATE' },
+  FLINK_OPTIONS: { text: l( 'rc.doc.type.flinkParam'), value: 'FLINK_OPTIONS' },
+  FUN_UDF: { text: l( 'rc.doc.type.functionOrUDF'), value: 'FUN_UDF' },
+  OTHER: { text: l( 'rc.doc.type.other'), value: 'OTHER' }
 };
 
 export const DOCUMENT_FUNCTION_TYPE_ENUMS = {
-  COMPARE_FUNCTION: { text: '比较函数', value: 'COMPARE_FUNCTION' },
-  LOGICAL_FUNCTION: { text: '逻辑函数', value: 'LOGICAL_FUNCTION' },
-  ARITHMETIC_FUNCTIONS: { text: '算术函数', value: 'ARITHMETIC_FUNCTIONS' },
-  STRING_FUNCTIONS: { text: '字符串函数', value: 'STRING_FUNCTIONS' },
-  TIME_FUNCTION: { text: '时间函数', value: 'TIME_FUNCTION' },
-  CONDITIONAL_FUNCTION: { text: '条件函数', value: 'CONDITIONAL_FUNCTION' },
-  TYPE_CONVER_FUNCTION: { text: '类型转换函数功能', value: 'TYPE_CONVER_FUNCTION' },
-  COLLECTION_FUNCTION: { text: 'Collection 函数', value: 'COLLECTION_FUNCTION' },
+  COMPARE_FUNCTION: { text: l( 'rc.doc.function.type.compare'), value: 'COMPARE_FUNCTION' },
+  LOGICAL_FUNCTION: { text: l( 'rc.doc.function.type.logical'), value: 'LOGICAL_FUNCTION' },
+  ARITHMETIC_FUNCTIONS: { text: l( 'rc.doc.function.type.arithmetic'), value: 'ARITHMETIC_FUNCTIONS' },
+  STRING_FUNCTIONS: { text: l( 'rc.doc.function.type.string'), value: 'STRING_FUNCTIONS' },
+  TIME_FUNCTION: { text: l( 'rc.doc.function.type.time'), value: 'TIME_FUNCTION' },
+  CONDITIONAL_FUNCTION: { text: l( 'rc.doc.function.type.conditional'), value: 'CONDITIONAL_FUNCTION' },
+  TYPE_CONVER_FUNCTION: { text: l( 'rc.doc.function.type.typeConver'), value: 'TYPE_CONVER_FUNCTION' },
+  COLLECTION_FUNCTION: { text: l( 'rc.doc.function.type.collection'), value: 'COLLECTION_FUNCTION' },
   VALUE_CONSTRUCTION_FUNCTION: {
-    text: 'Value Construction函数',
+    text: l( 'rc.doc.function.type.valueConstruction'),
     value: 'VALUE_CONSTRUCTION_FUNCTION Construction函数'
   },
-  VALUE_ACCESS_FUNCTION: { text: 'Value Access函数', value: 'VALUE_ACCESS_FUNCTION' },
-  GROUP_FUNCTION: { text: '分组函数', value: 'GROUP_FUNCTION' },
-  HASH_FUNCTION: { text: 'hash函数', value: 'HASH_FUNCTION' },
-  AGGREGATE_FUNCTION: { text: '聚合函数', value: 'AGGREGATE_FUNCTION' },
-  COLUMN_FUNCTION: { text: '列函数', value: 'COLUMN_FUNCTION' },
-  TABLE_AGGREGATE_FUNCTION: { text: '表值聚合函数', value: 'TABLE_AGGREGATE_FUNCTION' },
-  OTHER_FUNCTION: { text: '其他函数', value: 'OTHER_FUNCTION' }
+  VALUE_ACCESS_FUNCTION: { text: l( 'rc.doc.function.type.valueAccess'), value: 'VALUE_ACCESS_FUNCTION' },
+  GROUP_FUNCTION: { text:l( 'rc.doc.function.type.group'), value: 'GROUP_FUNCTION' },
+  HASH_FUNCTION: { text: l( 'rc.doc.function.type.hash'), value: 'HASH_FUNCTION' },
+  AGGREGATE_FUNCTION: { text:l( 'rc.doc.function.type.aggregate'), value: 'AGGREGATE_FUNCTION' },
+  COLUMN_FUNCTION: { text: l( 'rc.doc.function.type.column'), value: 'COLUMN_FUNCTION' },
+  TABLE_AGGREGATE_FUNCTION: { text: l( 'rc.doc.function.type.tableAggregate'), value: 'TABLE_AGGREGATE_FUNCTION' },
+  OTHER_FUNCTION: { text: l( 'rc.doc.function.type.other'), value: 'OTHER_FUNCTION' }
 };
 
 /**
