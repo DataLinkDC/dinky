4e9ca6079e6bf435252f79b05d8cf8d2ba1c634e
zackyoungh <zackyoungh@users.noreply.github.com>
2024-12-26 06:57:49 +0000

    Spotless Apply

--- dinky-web/src/pages/RegCenter/Document/constans.ts
+++ dinky-web/src/pages/RegCenter/Document/constans.ts
@@ -17,7 +17,7 @@
  *
  */
 
-import { l } from "@/utils/intl";
+import { l } from '@/utils/intl';
 
 export const DOCUMENT_CATEGORY_ENUMS = {
   Variable: { text: 'Variable', value: 'Variable' },
@@ -34,31 +34,46 @@ export const DOCUMENT_CATEGORY_ENUMS = {
  */
 export const DOCUMENT_TYPE_ENUMS = {
   SQL_TEMPLATE: { text: l('rc.doc.type.codeSnippetOrTemplate'), value: 'SQL_TEMPLATE' },
-  FLINK_OPTIONS: { text: l( 'rc.doc.type.flinkParam'), value: 'FLINK_OPTIONS' },
-  FUN_UDF: { text: l( 'rc.doc.type.functionOrUDF'), value: 'FUN_UDF' },
-  OTHER: { text: l( 'rc.doc.type.other'), value: 'OTHER' }
+  FLINK_OPTIONS: { text: l('rc.doc.type.flinkParam'), value: 'FLINK_OPTIONS' },
+  FUN_UDF: { text: l('rc.doc.type.functionOrUDF'), value: 'FUN_UDF' },
+  OTHER: { text: l('rc.doc.type.other'), value: 'OTHER' }
 };
 
 export const DOCUMENT_FUNCTION_TYPE_ENUMS = {
-  COMPARE_FUNCTION: { text: l( 'rc.doc.function.type.compare'), value: 'COMPARE_FUNCTION' },
-  LOGICAL_FUNCTION: { text: l( 'rc.doc.function.type.logical'), value: 'LOGICAL_FUNCTION' },
-  ARITHMETIC_FUNCTIONS: { text: l( 'rc.doc.function.type.arithmetic'), value: 'ARITHMETIC_FUNCTIONS' },
-  STRING_FUNCTIONS: { text: l( 'rc.doc.function.type.string'), value: 'STRING_FUNCTIONS' },
-  TIME_FUNCTION: { text: l( 'rc.doc.function.type.time'), value: 'TIME_FUNCTION' },
-  CONDITIONAL_FUNCTION: { text: l( 'rc.doc.function.type.conditional'), value: 'CONDITIONAL_FUNCTION' },
-  TYPE_CONVER_FUNCTION: { text: l( 'rc.doc.function.type.typeConver'), value: 'TYPE_CONVER_FUNCTION' },
-  COLLECTION_FUNCTION: { text: l( 'rc.doc.function.type.collection'), value: 'COLLECTION_FUNCTION' },
+  COMPARE_FUNCTION: { text: l('rc.doc.function.type.compare'), value: 'COMPARE_FUNCTION' },
+  LOGICAL_FUNCTION: { text: l('rc.doc.function.type.logical'), value: 'LOGICAL_FUNCTION' },
+  ARITHMETIC_FUNCTIONS: {
+    text: l('rc.doc.function.type.arithmetic'),
+    value: 'ARITHMETIC_FUNCTIONS'
+  },
+  STRING_FUNCTIONS: { text: l('rc.doc.function.type.string'), value: 'STRING_FUNCTIONS' },
+  TIME_FUNCTION: { text: l('rc.doc.function.type.time'), value: 'TIME_FUNCTION' },
+  CONDITIONAL_FUNCTION: {
+    text: l('rc.doc.function.type.conditional'),
+    value: 'CONDITIONAL_FUNCTION'
+  },
+  TYPE_CONVER_FUNCTION: {
+    text: l('rc.doc.function.type.typeConver'),
+    value: 'TYPE_CONVER_FUNCTION'
+  },
+  COLLECTION_FUNCTION: { text: l('rc.doc.function.type.collection'), value: 'COLLECTION_FUNCTION' },
   VALUE_CONSTRUCTION_FUNCTION: {
-    text: l( 'rc.doc.function.type.valueConstruction'),
+    text: l('rc.doc.function.type.valueConstruction'),
     value: 'VALUE_CONSTRUCTION_FUNCTION Construction函数'
   },
-  VALUE_ACCESS_FUNCTION: { text: l( 'rc.doc.function.type.valueAccess'), value: 'VALUE_ACCESS_FUNCTION' },
-  GROUP_FUNCTION: { text:l( 'rc.doc.function.type.group'), value: 'GROUP_FUNCTION' },
-  HASH_FUNCTION: { text: l( 'rc.doc.function.type.hash'), value: 'HASH_FUNCTION' },
-  AGGREGATE_FUNCTION: { text:l( 'rc.doc.function.type.aggregate'), value: 'AGGREGATE_FUNCTION' },
-  COLUMN_FUNCTION: { text: l( 'rc.doc.function.type.column'), value: 'COLUMN_FUNCTION' },
-  TABLE_AGGREGATE_FUNCTION: { text: l( 'rc.doc.function.type.tableAggregate'), value: 'TABLE_AGGREGATE_FUNCTION' },
-  OTHER_FUNCTION: { text: l( 'rc.doc.function.type.other'), value: 'OTHER_FUNCTION' }
+  VALUE_ACCESS_FUNCTION: {
+    text: l('rc.doc.function.type.valueAccess'),
+    value: 'VALUE_ACCESS_FUNCTION'
+  },
+  GROUP_FUNCTION: { text: l('rc.doc.function.type.group'), value: 'GROUP_FUNCTION' },
+  HASH_FUNCTION: { text: l('rc.doc.function.type.hash'), value: 'HASH_FUNCTION' },
+  AGGREGATE_FUNCTION: { text: l('rc.doc.function.type.aggregate'), value: 'AGGREGATE_FUNCTION' },
+  COLUMN_FUNCTION: { text: l('rc.doc.function.type.column'), value: 'COLUMN_FUNCTION' },
+  TABLE_AGGREGATE_FUNCTION: {
+    text: l('rc.doc.function.type.tableAggregate'),
+    value: 'TABLE_AGGREGATE_FUNCTION'
+  },
+  OTHER_FUNCTION: { text: l('rc.doc.function.type.other'), value: 'OTHER_FUNCTION' }
 };
 
 /**
