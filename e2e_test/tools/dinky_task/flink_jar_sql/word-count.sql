586a4db3e62573604a637104f3437aebc6c376d2
wenmo <32723967+aiwenmo@users.noreply.github.com>
2025-03-13 22:05:31 +0800

    try local

--- e2e_test/tools/dinky_task/flink_jar_sql/word-count.sql
+++ e2e_test/tools/dinky_task/flink_jar_sql/word-count.sql
@@ -1,6 +1,6 @@
 set 'execution.checkpointing.interval' = '5s';
 EXECUTE JAR WITH (
-'uri'='file:/opt/flink/examples/streaming/WordCount.jar',
+'uri'='local:/opt/flink/examples/streaming/WordCount.jar',
 'main-class'='org.apache.flink.streaming.examples.wordcount.WordCount',
 'args'='',
 'allowNonRestoredState'='false'
