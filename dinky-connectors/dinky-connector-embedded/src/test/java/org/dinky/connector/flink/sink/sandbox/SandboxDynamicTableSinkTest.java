04434d3707ebe4853a9379d86779c3f56cac083a
Wink <809097465@qq.com>
2025-09-10 19:09:23 +0800

    upgrade flink 1.20.2

--- dinky-connectors/dinky-connector-embedded/src/test/java/org/dinky/connector/flink/sink/sandbox/SandboxDynamicTableSinkTest.java
+++ dinky-connectors/dinky-connector-embedded/src/test/java/org/dinky/connector/flink/sink/sandbox/SandboxDynamicTableSinkTest.java
@@ -38,7 +38,6 @@ import org.apache.flink.table.catalog.Column;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 public class SandboxDynamicTableSinkTest {
@@ -141,7 +140,6 @@ public class SandboxDynamicTableSinkTest {
         assertArrayEquals(new String[] {"[1]", "[2]", "[3]", "[4]", "[5]"}, actualSandboxData);
     }
 
-    @Ignore
     @Test
     public void testAutoCancel() {
         List<Column> columns = Arrays.asList(Column.physical("id", DataTypes.INT()));
