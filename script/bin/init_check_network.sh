2e354e4d3f693a0b6794e768f4c96c6317845e06
Lex Li <libeely@gmail.com>
2025-10-21 15:37:35 +0800

    Update init_check_network.sh

--- script/bin/init_check_network.sh
+++ script/bin/init_check_network.sh
@@ -6,7 +6,7 @@ echo -e "${YELLOW}Checking whether your network can connect to the Internet prop
 if ! ping -c 1 ${EXTERNAL_CONNECTIVITY_CHECK_URL} &> /dev/null; then
     echo -e "${RED}Your network cannot connect to the Internet using ping mode, please check whether your network environment is normal, and the program will try to use curl mode to detect the network connection again...${RESET}"
     if ! curl -I -s --connect-timeout 5 ${EXTERNAL_CONNECTIVITY_CHECK_URL} -w '%{http_code}' | tail -n1 | grep "200" &> /dev/null; then
-        echo -e "${RED}Your network cannot be connected to the Internet using curl mode, please check whether your network environment is normal。${RESET}"
+        echo -e "${RED}Your network cannot be connected to the Internet using curl mode, please check whether your network environment is normal.${RESET}"
         echo -e "${YELLOW}Note that in some network environments, firewalls or security policies may block ICMP requests (i.e., pings). If this happens, you can use curl to detect the network connection.${RESET}"
         exit 1
     else
