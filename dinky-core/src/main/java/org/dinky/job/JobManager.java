697ae1051ba828c77d6e911a071bc42589396e98
jarvis <jarvis@apache.org>
2025-10-24 14:42:32 +0800

    [fix] add end time for jar sql

--- dinky-core/src/main/java/org/dinky/job/JobManager.java
+++ dinky-core/src/main/java/org/dinky/job/JobManager.java
@@ -275,6 +275,7 @@ public class JobManager {
                 setCurrentSql(jobStatement.getStatement());
                 jobRunnerFactory.getJobRunner(jobStatement.getStatementType()).run(jobStatement);
             }
+            job.setEndTime(LocalDateTime.now());
             if (job.isFailed()) {
                 failed();
             } else {
