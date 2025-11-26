356fe8c40f35568eb3574405d18715f8bdad801b
lkj <605780332@qq.com>
2025-11-26 10:34:58 +0800

    [Optimization-4511] Optimize catalogue tree loading performance for large-scale catalogue structures (2000+)

--- dinky-admin/src/main/java/org/dinky/service/catalogue/impl/CatalogueServiceImpl.java
+++ dinky-admin/src/main/java/org/dinky/service/catalogue/impl/CatalogueServiceImpl.java
@@ -62,7 +62,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.LinkedHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
